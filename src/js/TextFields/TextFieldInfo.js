7e8e037f9e96ebaacf8c13205f8e900311b708a1
mlaursen <mlaursen03@gmail.com>
2016-01-01 22:03:51 -0500

    Added opacity transition to info text

--- src/js/TextFields/TextFieldInfo.js
+++ src/js/TextFields/TextFieldInfo.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 import classnames from 'classnames';
 
 export default ({ value, text, maxLength, isError, active, isHelpOnFocus }) => {
+  const isTextVisible = !!text && (!isHelpOnFocus || active);
   return (
-    <div className={classnames('md-text-field-info', { 'error': isError })}>
-      {(!isHelpOnFocus || active) && text}
+    <CSSTransitionGroup
+      component="div"
+      transitionName="opacity"
+      transitionEnterTimeout={150}
+      transitionLeaveTimeout={150}
+      className={classnames('md-text-field-info', {
+        'error': isError,
+        'count-only': !text || !(isTextVisible),
+      })}
+      >
+      {isTextVisible && <span key="text">{text}</span>}
       {maxLength &&
         <span className="md-text-field-count">
           {value.length + ' / ' + maxLength}
         </span>
       }
-    </div>
+    </CSSTransitionGroup>
   );
 };
