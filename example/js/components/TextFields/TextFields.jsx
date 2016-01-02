1660a2a9cda25c207de9d87462ca7ac59349bb39
mlaursen <mlaursen03@gmail.com>
2016-01-01 22:03:29 -0500

    Updated text field docs a bit

--- example/js/components/TextFields/TextFields.jsx
+++ example/js/components/TextFields/TextFields.jsx
@@ -17,9 +17,23 @@ export default class TextFields extends Component {
         imports={['TextField']}
         examples={[
           <TextField label="Title" />,
-          <TextField label="Title" helpText="I am help text that appears only on focus." helpOnFocus maxLength={20} />,
-          <TextField label="Title" errorText="This is an example of some error message. It should automatically wrap lines as well. It just keeps going and going and going." />,
+          <TextField
+            label="Title"
+            helpText="I am help text that appears only on focus."
+            helpOnFocus
+            maxLength={20}
+          />,
+          <TextField
+            label="Title"
+            errorText="This is an example of some error message. It should automatically wrap lines as well. It just keeps going and going and going."
+          />,
           <TextField label="Position" floatingLabel={false} helpText="I am a single line with help text." />,
+          <TextField
+            label="Phone"
+            floatingLabel={false}
+            icon={<FontIcon>phone</FontIcon>}
+            type="phone"
+          />,
           <TextField label="I am required" required />,
           <TextField label="I am disabled" disabled />,
           <TextField label="I am a single line disabled" floatingLabel={false} disabled />,
@@ -31,10 +45,11 @@ export default class TextFields extends Component {
             helpText="I am a multiline text field that has 2 rows."
           />,
           <TextField
-            label="Phone"
-            floatingLabel={false}
-            icon={<FontIcon>phone</FontIcon>}
-            type="phone"
+            label="Description"
+            placeholder="What are you thinking about today?"
+            rows={2}
+            maxRows={10}
+            maxLength={1000}
           />,
         ]}
         components={[{
