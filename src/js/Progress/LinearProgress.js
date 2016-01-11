cb141f8090ace56e40489daf6b0a608c5a230c8f
mlaursen <mlaursen03@gmail.com>
2016-01-10 23:33:44 -0500

    Added query indeterminate to determinate for linear progress

--- src/js/Progress/LinearProgress.js
+++ src/js/Progress/LinearProgress.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+import { isPropEnabled } from '../utils/PropUtils';
+
 export default class LinearProgress extends Component {
   constructor(props) {
     super(props);
@@ -12,21 +14,24 @@ export default class LinearProgress extends Component {
   static propTypes = {
     className: PropTypes.string,
     value: PropTypes.number,
+    query: PropTypes.bool,
   };
 
   render() {
-    const { className, value } = this.props;
+    const { className, value, ...props } = this.props;
     const isDeterminate = typeof value === 'number';
+
     let style;
     if(isDeterminate) {
       style = { width: `${value}%` };
     }
     return (
-      <div className={classnames('md-linear-progress-container', className)}>
+      <div className={classnames('md-linear-progress-container', className)} {...props}>
         <div
           className={classnames('md-linear-progress', {
             'determinate': isDeterminate,
             'indeterminate': !isDeterminate,
+            'query': isPropEnabled(props, 'query'),
           })}
           style={style}
         />
