2558b5eaa71e7b37b6acbd4f30de07e44b73ccc5
Mikkel Laursen <mlaursen03@gmail.com>
2016-01-14 13:15:26 -0500

    Added mobile scrolling to tabs

--- src/js/Tabs/Tabs.js
+++ src/js/Tabs/Tabs.js
@@ -13,6 +13,7 @@ export default class Tabs extends Component {
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
     this.state = {
       indicatorStyle: {},
+      touchDistance: 0,
     };
 
     if(typeof props.value === 'undefined') {
@@ -90,6 +91,45 @@ export default class Tabs extends Component {
     this.updateIndicator();
   }
 
+  handleTouchStart = ({ changedTouches }) => {
+    this.setState({ touchStart: changedTouches[0].pageX });
+  };
+
+  handleTouchMove = ({ changedTouches }) => {
+    const touchDistance = this.moveTabs(changedTouches[0], 20);
+    this.setState({
+      tabTransform: `translate3d(${touchDistance}px, 0, 0)`,
+    });
+  };
+
+  handleTouchEnd = ({ changedTouches }) => {
+    const touchDistance = this.moveTabs(changedTouches[0]);
+    this.setState({
+      touchDistance,
+      tabTransform: `translate3d(${touchDistance}px, 0, 0)`,
+    });
+  };
+
+  handleTabChange = ({ value, label, onChange }) => {
+    let params = { label };
+    if(typeof value !== 'undefined') {
+      params.value = value;
+    }
+
+    if(onChange) {
+      onChange(...params);
+    }
+
+    if(this.props.onChange) {
+      this.props.onChange(...params);
+    }
+
+    if(typeof this.props.value === 'undefined') {
+      this.setState({ value: value || label });
+    }
+  };
+
+
   getValue = (props = this.props, state = this.state) => {
     return (typeof props.value !== 'undefined' ? props : state).value;
   };
@@ -126,25 +166,6 @@ export default class Tabs extends Component {
     return stateValue === tabValue;
   };
 
-  handleTabChange = ({ value, label, onChange }) => {
-    let params = { label };
-    if(typeof value !== 'undefined') {
-      params.value = value;
-    }
-
-    if(onChange) {
-      onChange(...params);
-    }
-
-    if(this.props.onChange) {
-      this.props.onChange(...params);
-    }
-
-    if(typeof this.props.value === 'undefined') {
-      this.setState({ value: value || label });
-    }
-  };
-
   updateIndicator = () => {
     const { offsetWidth, offsetLeft } = this.refs.tabs.querySelector('.md-tab.active');
     this.setState({
@@ -155,8 +176,22 @@ export default class Tabs extends Component {
     });
   };
 
+  moveTabs = ({ pageX }, threshold = 0) => {
+    let distance = this.state.touchDistance + (pageX - this.state.touchStart);
+    const tabs = Array.prototype.slice.call(this.refs.tabs.querySelectorAll('.md-tab'));
+    const maxWidth = tabs.reduce((prev, curr) => prev + curr.clientWidth, 0) + threshold - this.refs.tabs.clientWidth - parseInt(this.props.tabsOffset);
+
+    if(distance > 0) {
+      distance = Math.min(distance, threshold);
+    } else {
+      distance = Math.max(distance, -maxWidth);
+    }
+
+    return distance;
+  };
+
   render() {
-    const { indicatorStyle, tabsContent } = this.state;
+    const { indicatorStyle, tabsContent, tabTransform } = this.state;
     const { className, children, tabsOffset, scrollIconLeft, scrollIconRight, ...props } = this.props;
     const { transitionName, transitionEnter, transitionEnterTimeout, transitionLeave, transitionLeaveTimeout, ...remainingProps } = props;
 
@@ -189,7 +224,7 @@ export default class Tabs extends Component {
             onTouchStart={this.handleTouchStart}
             onTouchMove={this.handleTouchMove}
             onTouchEnd={this.handleTouchEnd}
-            style={{ marginLeft: tabsOffset }}
+            style={{ marginLeft: tabsOffset, transform: tabTransform }}
             >
             {tabs}
             <span className="md-tab-indicator" style={indicatorStyle} />
