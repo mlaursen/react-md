ec3508eca4be22c434aab46d28f2723f6f86a882
Mikkel Laursen <mlaursen03@gmail.com>
2016-01-15 09:41:10 -0500

    Updated app with tabs to have paper and background color

--- docs/src/app/AppWithTabs/AppWithTabs.jsx
+++ docs/src/app/AppWithTabs/AppWithTabs.jsx
@@ -4,31 +4,34 @@ import AppBar, { ActionArea } from 'react-md/AppBar';
 import { IconButton } from 'react-md/Buttons';
 import { Tabs, Tab } from 'react-md/Tabs';
 import { numstr } from '../utils';
+import Paper from 'react-md/Paper';
 
 const AppWithTabs = ({ centered = false, scrollable = false, fixedWidth = false }) => {
   return (
-    <AppBar
-      primary
-      menuButton={<IconButton className="menu-btn">menu</IconButton>}
-      title="Page title"
-      actionsRight={(
-        <ActionArea>
-          <IconButton>search</IconButton>
-          <IconButton>more_vert</IconButton>
-        </ActionArea>
-      )}
-      >
-      <Tabs primary centered={centered} scrollable={scrollable} fixedWidth={fixedWidth}>
-        {Array.apply(null, new Array(scrollable ? 9 : 3)).map((_, i) => (
-        <Tab
-          key={i}
-          label={`Item ${numstr[i]}`}
-          >
-          <div style={{ minHeight: '80px' }} />
-        </Tab>
-        ))}
-      </Tabs>
-    </AppBar>
+    <Paper>
+      <AppBar
+        primary
+        menuButton={<IconButton className="menu-btn">menu</IconButton>}
+        title="Page title"
+        actionsRight={(
+          <ActionArea>
+            <IconButton>search</IconButton>
+            <IconButton>more_vert</IconButton>
+          </ActionArea>
+        )}
+        >
+        <Tabs primary centered={centered} scrollable={scrollable} fixedWidth={fixedWidth}>
+          {Array.apply(null, new Array(scrollable ? 9 : 3)).map((_, i) => (
+          <Tab
+            key={i}
+            label={`Item ${numstr[i]}`}
+            >
+            <div style={{ minHeight: '80px', background: '#fafafa' }} />
+          </Tab>
+          ))}
+        </Tabs>
+      </AppBar>
+    </Paper>
   );
 };
 
