2bf35f7362678b47e3d6a112a14f104f9f046621
Mikkel Laursen <mlaursen03@gmail.com>
2016-02-25 10:52:27 -0500

    Removed the weird picker container from datepicker. Really need a new way for this:

--- src/js/DatePickers/DatePickerContainer.js
+++ src/js/DatePickers/DatePickerContainer.js
@@ -1,17 +1,31 @@
 import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import TransitionGroup from 'react-addons-transition-group';
+import classnames from 'classnames';
 
 import FontIcon from '../FontIcons';
-import { PickerContainer } from '../Pickers';
 import DatePicker from './DatePicker';
 
-import { DateTimeFormat } from '../utils';
+import { ESC } from '../constants/keyCodes';
+import { addDate, subtractDate, DateTimeFormat } from '../utils';
+import TextField from '../TextFields';
+import Dialog from '../Dialogs';
+import Height from '../Transitions';
 
 export default class DatePickerContainer extends Component {
   constructor(props) {
     super(props);
 
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+
+    const date = props.defaultValue ? new Date(props.defaultValue) : new Date();
+    this.state = {
+      isOpen: props.initiallyOpen,
+      value: props.defaultValue,
+      calendarDate: date,
+      calendarTempDate: date,
+      calendarMode: props.initialCalendarMode,
+    };
   }
 
   static propTypes = {
@@ -38,7 +52,6 @@ export default class DatePickerContainer extends Component {
     minDate: PropTypes.instanceOf(Date),
     maxDate: PropTypes.instanceOf(Date),
     autoOk: PropTypes.bool,
-    type: PropTypes.oneOf(['date', 'time']),
     initialYearsDisplayed: PropTypes.number,
     inline: PropTypes.bool,
     displayMode: PropTypes.oneOf(['landscape', 'portrait']),
@@ -60,9 +73,175 @@ export default class DatePickerContainer extends Component {
     cancelPrimary: true,
   };
 
+  componentWillUpdate(nextProps, nextState) {
+    if(this.state.isOpen && !nextState.isOpen) {
+      if(nextProps.inline) {
+        window.removeEventListener('click', this.closeOnOutside);
+      }
+
+      window.removeEventListener('keydown', this.closeOnEsc);
+    } else if(!this.state.isOpen && nextState.isOpen) {
+      if(nextProps.inline) {
+        window.addEventListener('click', this.closeOnOutside);
+      }
+
+      window.addEventListener('keydown', this.closeOnEsc);
+    }
+  }
+
+  componentWillUnmount() {
+    if(this.state.isOpen) {
+      if(this.props.inline) {
+        window.removeEventListener('click', this.closeOnOutside);
+      }
+
+      window.removeEventListener('keydown', this.closeOnEsc);
+    }
+  }
+
+  closeOnEsc = (e) => {
+    if((e.which || e.keyCode) === ESC) {
+      this.handleCancelClick();
+    }
+  };
+
+  closeOnOutside = (e) => {
+    const { container } = this.refs;
+    let target = e.target;
+    while(target.parentNode) {
+      if(target === container) { return; }
+      target = target.parentNode;
+    }
+
+    this.close();
+  };
+
+  toggleOpen = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  close = () => {
+    this.setState({ isOpen: false });
+  };
+
+  handleOkClick = (e) => {
+    const { DateTimeFormat, locales, onChange } = this.props;
+    const value = DateTimeFormat(locales).format(this.state.calendarTempDate);
+    if(typeof this.props.value !== 'undefined' && onChange) {
+      onChange(value, new Date(this.state.calendarTempDate), e);
+    }
+
+    this.setState({ value, isOpen: false });
+  };
+
+  handleCancelClick = () => {
+    this.setState({ calendarTempDate: this.state.calendarDate, isOpen: false });
+  };
+
+  changeCalendarMode = (calendarMode) => {
+    if(this.state.calendarMode === calendarMode) { return; }
+
+    this.setState({ calendarMode });
+  };
+
+  previousMonth = () => {
+    const calendarDate = subtractDate(this.state.calendarDate, 1, 'M');
+    this.setState({ calendarDate });
+  };
+
+  nextMonth = () => {
+    const calendarDate = addDate(this.state.calendarDate, 1, 'M');
+    this.setState({ calendarDate });
+  };
+
+  setCalendarTempDate = (calendarTempDate) => {
+    const { autoOk, DateTimeFormat, locales, onChange } = this.props;
+
+    if(autoOk) {
+      const value = DateTimeFormat(locales).format(calendarTempDate);
+      if(onChange && typeof this.props.value !== 'undefined') {
+        onChange(value, new Date(calendarTempDate));
+      }
+
+      this.setState({
+        isOpen: false,
+        value,
+        calendarTempDate,
+      });
+    } else {
+      this.setState({ calendarTempDate });
+    }
+  };
+
+  setCalendarTempYear = (year) => {
+    const { calendarTempDate, calendarDate } = this.state;
+    if(calendarTempDate.getFullYear() === year) { return; }
+
+    const { minDate, maxDate } = this.props;
+    let nextDate = new Date(calendarDate.setFullYear(year));
+    let nextTemp = new Date(calendarTempDate.setFullYear(year));
+
+    if(minDate && nextTemp < minDate) {
+      nextDate = new Date(minDate);
+      nextTemp = new Date(minDate);
+    }
+
+    if(maxDate && nextTemp > maxDate) {
+      nextDate = new Date(maxDate);
+      nextTemp = new Date(maxDate);
+    }
+
+    this.setState({
+      calendarDate: nextDate,
+      calendarTempDate: nextTemp,
+    });
+  };
+
+
   render() {
+    const { label, floatingLabel, value, onChange, icon, inline, displayMode, ...props } = this.props;
+    const { isOpen, ...state } = this.state;
+    const pickerProps = {
+      ...state,
+      ...props,
+      className: classnames('md-picker', displayMode, { inline, 'with-icon': inline && icon }),
+      onCancelClick: this.handleCancelClick,
+      onOkClick: this.handleOkClick,
+      changeCalendarMode: this.changeCalendarMode,
+      onPreviousClick: this.previousMonth,
+      onNextClick: this.nextMonth,
+      onCalendarDateClick: this.setCalendarTempDate,
+      onCalendarYearClick: this.setCalendarTempYear,
+    };
+
+    let textFieldValue = typeof value === 'undefined' ? state.value : value;
+    if(isOpen && inline) {
+      textFieldValue = new props.DateTimeFormat(props.locale).format(state.calendarTempDate);
+    }
+
     return (
-      <PickerContainer {...this.props} type="date" component={DatePicker} />
+      <div className="md-picker-container" ref="container">
+        <TextField
+          icon={icon}
+          onClick={this.toggleOpen}
+          label={label}
+          floatingLabel={floatingLabel}
+          value={textFieldValue}
+          onChange={onChange}
+        />
+        {inline ?
+          <TransitionGroup>
+            {isOpen &&
+              <Height transitionEnterTimeout={150} transitionLeaveTimeout={150}>
+                <DatePicker {...pickerProps} />
+              </Height>
+            }
+          </TransitionGroup> :
+          <Dialog isOpen={isOpen} close={this.close}>
+            {isOpen && <DatePicker {...pickerProps} />}
+          </Dialog>
+        }
+      </div>
     );
   }
 }
