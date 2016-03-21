e6379ec1d0746f92d3683fb2774a2ba700f8de59
Mikkel Laursen <mlaursen03@gmail.com>
2016-03-21 14:24:17 -0400

    Updated tabs to have ink

--- src/js/Tabs/Tab.js
+++ src/js/Tabs/Tab.js
@@ -1,45 +1,57 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
+import Ink from '../Inks';
 
-const Tab = ({ className, icon, label, label2, checked, value, onChange, ...props }) => {
-  return (
-    <div
-      className={classnames('md-tab', className, { 'active': checked })}
-      {...props}
-    >
-      <label
-        className={classnames('md-tab-label', {
-          'multiline': !!label && !!label2,
-          'with-icon': !!label && !!icon,
-        })}
-      >
-        {icon}
-        {label && <div>{label}</div>}
-        {label2 && <div>{label2}</div>}
-        <input
-          type="radio"
-          className="md-tab-control"
-          checked={checked}
-          value={value}
-          onChange={onChange}
-        />
-      </label>
-    </div>
-  );
-};
+export default class Tab extends Component {
+  constructor(props) {
+    super(props);
 
-Tab.propTypes = {
-  className: PropTypes.string,
-  children: PropTypes.node.isRequired,
-  icon: PropTypes.node,
-  label: PropTypes.string,
-  label2: PropTypes.string,
-  checked: PropTypes.bool,
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
-  onChange: PropTypes.func,
-};
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
 
-export default Tab;
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    icon: PropTypes.node,
+    label: PropTypes.string,
+    label2: PropTypes.string,
+    checked: PropTypes.bool,
+    value: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+    onChange: PropTypes.func,
+  };
+
+  render() {
+    const { className, icon, label, label2, checked, value, onChange, ...props } = this.props;
+
+    return (
+      <Ink>
+        <div
+          className={classnames('md-tab', className, { 'active': checked })}
+          {...props}
+        >
+          <label
+            className={classnames('md-tab-label', {
+              'multiline': !!label && !!label2,
+              'with-icon': !!label && !!icon,
+            })}
+          >
+            {icon}
+            {label && <div>{label}</div>}
+            {label2 && <div>{label2}</div>}
+            <input
+              type="radio"
+              className="md-tab-control"
+              checked={checked}
+              value={value}
+              onChange={onChange}
+            />
+          </label>
+        </div>
+      </Ink>
+    );
+  }
+}
