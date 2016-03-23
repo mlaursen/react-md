ccb70567e9aa97040f5bac588f2618d55deb68a7
Mikkel Laursen <mlaursen03@gmail.com>
2016-03-23 14:56:12 -0400

    Added className and containerClassName to toolbar

--- src/js/Toolbars/Toolbar.js
+++ src/js/Toolbars/Toolbar.js
@@ -13,6 +13,7 @@ export default class Toolbar extends Component {
 
   static propTypes = {
     className: PropTypes.string,
+    containerClassName: PropTypes.string,
     primary: PropTypes.bool,
     actionLeft: PropTypes.node,
     title: PropTypes.string,
@@ -43,7 +44,7 @@ export default class Toolbar extends Component {
   }
 
   render() {
-    const { actionLeft, title, actionsRight, children, fixed, primary, ...props } = this.props;
+    const { actionLeft, title, actionsRight, children, fixed, primary, className, containerClassName, ...props } = this.props;
     const { tabsOffset } = this.state;
     const childrenAsHeader = !!children && !actionLeft && !title && !actionsRight;
 
@@ -66,10 +67,10 @@ export default class Toolbar extends Component {
       });
     }
     return (
-      <div className={classnames('md-toolbar-container', { fixed })}>
+      <div className={classnames('md-toolbar-container', containerClassName, { fixed })}>
         <header
           {...props}
-          className={classnames('md-toolbar', { 'md-primary': primary })}
+          className={classnames('md-toolbar', className, { 'md-primary': primary })}
         >
           {header}
         </header>
