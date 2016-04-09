4e12175a630e137c78e8dc055d7ee0c086124376
mlaursen <mlaursen03@gmail.com>
2016-04-09 14:57:16 -0400

    Added Large Edit Dialog Column
    Added the first parts of the large edit dialog.

--- src/js/DataTables/EditDialogColumn.js
+++ src/js/DataTables/EditDialogColumn.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+import { FlatButton } from '../Buttons';
 import TableColumn from './TableColumn';
 import TextField from '../TextFields';
 import { ENTER, TAB } from '../constants/keyCodes';
@@ -65,10 +66,36 @@ export default class EditDialogColumn extends Component {
      * An optional onKeyDown function to call.
      */
     onKeyDown: PropTypes.func,
+
+    large: (props, propName, component) => {
+      try {
+        PropTypes.bool(props, propName, component);
+      } catch(e) {
+        return e;
+      }
+
+      if(!props.large) { return; }
+
+      try {
+        PropTypes.string.isRequired(props, 'title', component);
+      } catch(e) {
+        return e;
+      }
+    },
+
+    title: PropTypes.string,
+    onOkClick: PropTypes.func,
+    okLabel: PropTypes.string.isRequired,
+    onCancelClick: PropTypes.func,
+    cancelLabel: PropTypes.string.isRequired,
   };
 
   static defaultProps = {
     transitionDuration: 150,
+    okLabel: 'Save',
+    cancelLabel: 'Cancel',
+    onOkClick: () => {},
+    onCancelClick: () => {},
   };
 
   componentWillUpdate(nextProps, nextState) {
@@ -105,11 +132,27 @@ export default class EditDialogColumn extends Component {
   };
 
   handleKeyDown = (e) => {
-    this.props.onKeyDown && this.props.onKeyDown(e);
+    const { onKeyDown } = this.props;
+    onKeyDown && onKeyDown(e);
 
-    if([ENTER, TAB].indexOf(e.which || e.keyCode) !== -1) {
-      this.setState({ active: false });
+    const key = e.which || e.keyCode;
+    if(key === ENTER) {
+      this.save();
+    } else if(key === TAB) {
+      e.preventDefault();
     }
+  };
+
+  save = (e) => {
+    this.props.onOkClick && this.props.onOkClick(this.refs.textField.getValue(), e);
+
+    this.setState({ active: false });
+  };
+
+  handleCancelClick = (e) => {
+    this.props.onCancelClick(e);
+
+    this.setState({ active: false });
   };
 
   render() {
@@ -118,8 +161,28 @@ export default class EditDialogColumn extends Component {
       columnClassName,
       className,
       maxLength,
+      title,
+      onOkClick,
+      okLabel,
+      onCancelClick,
+      cancelLabel,
+      large,
       ...props,
     } = this.props;
+
+    let actions, largeTitle;
+    if(large && active) {
+      actions = (
+        <footer className="md-dialog-footer">
+          <FlatButton label={cancelLabel} onClick={this.handleCancelClick} primary={true} />
+          <FlatButton label={okLabel} onClick={this.save} primary={true} />
+        </footer>
+      );
+
+      largeTitle = (
+        <h3 className="md-title">{title}</h3>
+      );
+    }
 
     return (
       <TableColumn className={classnames(columnClassName, 'prevent-grow md-edit-dialog-column')} ref="column">
@@ -127,8 +190,10 @@ export default class EditDialogColumn extends Component {
           className={classnames('md-edit-dialog', className, {
             animating,
             active,
+            large,
           })}
         >
+          {largeTitle}
           <TextField
             {...props}
             ref="textField"
@@ -137,6 +202,7 @@ export default class EditDialogColumn extends Component {
             onFocus={this.handleFocus}
             maxLength={active ? maxLength : null}
           />
+          {actions}
         </div>
       </TableColumn>
     );
