1a12c33c8b60999468246885e06d8735ee3fe4c2
mlaursen <mlaursen03@gmail.com>
2016-04-10 11:35:54 -0400

    Forgot to remove TableData component that doesn't exist anymore

--- src/js/DataTables/index.js
+++ src/js/DataTables/index.js
@@ -2,6 +2,5 @@ export DataTable from './DataTable';
 export TableHeader from './TableHeader';
 export TableBody from './TableBody';
 export TableRow from './TableRow';
-export TableData from './TableData';
 export TableColumn from './TableColumn';
 export EditDialogColumn from './EditDialogColumn';
