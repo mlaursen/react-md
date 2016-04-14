3b47b910c0b4c650718f07e61c73297f40381ea2
mlaursen <mlaursen03@gmail.com>
2016-04-14 17:59:35 -0400

    Added Docgen for Avatars
    This should handle the Avatar component for #30.

--- src/js/Avatars/Avatar.js
+++ src/js/Avatars/Avatar.js
@@ -2,6 +2,13 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+/**
+ * The avatar component is used to convert a `FontIcon`, an image, or
+ * a letter into an avatar.
+ *
+ * Any other props given to the Avatar component such as event listeners
+ * or styles will also be applied.
+ */
 export default class Avatar extends Component {
   constructor(props) {
     super(props);
@@ -10,13 +17,48 @@ export default class Avatar extends Component {
   }
 
   static propTypes = {
+    /**
+     * An optional className to apply to the avatar.
+     */
     className: PropTypes.string,
+
+    /**
+     * An optional image source to use for the avatar.
+     */
     src: PropTypes.string,
+
+    /**
+     * An optional image alt to use for the avatar if it is
+     * an image.
+     */
     alt: PropTypes.string,
+
+    /**
+     * An optional `FontIcon` to convert into an avatar.
+     */
     icon: PropTypes.node,
+
+    /**
+     * An optional letter to display in the avatar.
+     */
     children: PropTypes.string,
+
+    /**
+     * A boolean if a random color should be applied to the avatar.
+     * This will be one of the `suffixes`.
+     */
     random: PropTypes.bool,
+
+    /**
+     * A list of available suffixes to use when generating a random
+     * color for the avatar.
+     */
     suffixes: PropTypes.arrayOf(PropTypes.string),
+
+    /**
+     * The suffix to use for a color. This can be any value but
+     * *should* be one of the available `suffixes`.
+     */
     suffix: PropTypes.string,
   };
 
