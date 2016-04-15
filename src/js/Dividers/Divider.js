b63388b8be4431efe8f8f1329653b3372598def1
Mikkel Laursen <mlaursen03@gmail.com>
2016-04-15 11:47:01 -0400

    Enhancement/divider tests (#62)
    * Added Divider Tests for #43
    
    Added the initial divider tests. Had to update the Divider
    component to not be a pure function because jest does not pick that
    up at this time.
    
    * Added react-docgen for Dividers and #20

--- src/js/Dividers/Divider.js
+++ src/js/Dividers/Divider.js
@@ -1,18 +1,45 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
-const Divider = ({ className, inset, vertical, ...props}) => (
-  <hr
-    role="divider"
-    className={classnames('md-divider', className, { inset, vertical })}
-    {...props}
-  />
-);
+/**
+ * The divider component will pass all other props such as style or
+ * event listeners on to the component.
+ */
+export default class Divider extends Component {
+  constructor(props) {
+    super(props);
 
-Divider.propTypes = {
-  className: PropTypes.string,
-  inset: PropTypes.bool,
-  vertical: PropTypes.bool,
-};
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
 
-export default Divider;
+  static propTypes = {
+    /**
+     * An optional className to apply to the divider.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Boolean if this divider should be inset relative to it's container
+     * component. This means that if it is in a `List` with `Avatar`, it
+     * will start the divider  to the left of the main text in the list.
+     */
+    inset: PropTypes.bool,
+
+    /**
+     * Boolean if the divider should be vertical instead of horizontal.
+     */
+    vertical: PropTypes.bool,
+  };
+
+  render() {
+    const { className, inset, vertical, ...props } = this.props;
+    const dividerProps = {
+      role: 'divider',
+      className: classnames('md-divider', className, { inset, vertical }),
+      ...props,
+    };
+
+    return React.createElement(vertical ? 'div' : 'hr', dividerProps);
+  }
+}
