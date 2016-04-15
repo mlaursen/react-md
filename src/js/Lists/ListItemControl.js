55a7c70d884e456158b5a2b3588f7e1142ac74f4
Mikkel Laursen <mlaursen03@gmail.com>
2016-04-15 15:44:03 -0400

    Enhancement/#20 list docgen (#64)
    * Updated ListItem's className and Styles
    
    Updated the className style props to apply to the main li component in ListItem.
    Created a tileClassName and tileStyle to apply to the .md-list-tile in the List Item.
    
    * Added Lists react-docgen

--- src/js/Lists/ListItemControl.js
+++ src/js/Lists/ListItemControl.js
@@ -4,6 +4,11 @@ import classnames from 'classnames';
 
 import ListItemText from './ListItemText';
 
+/**
+ * A `ListItemControl` component has either a `primaryAction` or a `secondaryAction`.
+ * The `primaryAction` can **only** be a `Checkbox`. A `secondaryAction` can either be
+ * a `Checkbox`, `Switch`, or a `Reorder` icon.
+ */
 export default class ListItemControl extends Component {
   constructor(props) {
     super(props);
@@ -12,21 +17,54 @@ export default class ListItemControl extends Component {
   }
 
   static propTypes = {
+    /**
+     * An optional className to apply to the list item.
+     */
     className: PropTypes.string,
-    primaryText: PropTypes.string.isRequired,
-    secondaryText: PropTypes.node,
-    primaryAction: PropTypes.element,
-    secondaryAction: PropTypes.element,
-    threeLines: PropTypes.bool,
-    validation: (props, propName, componentName) => {
-      if(!props.primaryAction && !props.secondaryAction) {
-        return new Error(`Missing required prop 'primaryAction' or 'secondaryAction' for the component '${componentName}'.`);
-      }
 
-      if(props.primaryAction && props.secondaryAction) {
-        return new Error(`You can not have a 'primaryAction' and a 'secondaryAction' prop for the component '${componentName}'.`);
+    /**
+     * The primary text to display in the item.
+     */
+    primaryText: PropTypes.string.isRequired,
+
+    /**
+     * An optional secondary text to display below the `primaryText`. This can
+     * be an additional 1 or 2 lines. The text will automatically be ellipsed
+     * if it spans more than one line. If the prop `threeLines` is set to true,
+     * then the text will automatically be ellipsed if it spans more than two lines.
+     */
+    secondaryText: PropTypes.node,
+
+    /**
+     * Boolean if the `ListItem` should allow three lines of text including
+     * the `primaryText`.
+     */
+    threeLines: PropTypes.bool,
+
+    /**
+     * The primary action element for the `ListItemControl`. This should be a `Checkbox`
+     * component.
+     *
+     * The custom validation will warn you about missing both a `primaryAction` and
+     * a `secondaryAction` or if you have both a `primaryAction` and a `secondaryAction`.
+     */
+    primaryAction: (props, propName, component) => {
+      const primaryAction = props[propName];
+      const secondaryAction = props.secondaryAction;
+      if(primaryAction && !secondaryAction) {
+        return PropTypes.element(props, propName, component);
+      } else if(!primaryAction && !secondaryAction) {
+        return new Error(`Missing required prop 'primaryAction' or 'secondaryAction' for the component '${component}'.`);
+      } else if(primaryAction && secondaryAction) {
+        return new Error(`You can not have a 'primaryAction' and a 'secondaryAction' prop for the component '${component}'.`);
       }
     },
+
+    /**
+     * A secondary action element. This should be either a `Checkbox`, `Switch`, or a
+     * `Reorder` icon.
+     */
+    secondaryAction: PropTypes.element,
   };
 
   static defaultProps = {
