c1c95dc2f5caf837e20210cfbb87ca3caccab89d
mlaursen <mlaursen03@gmail.com>
2016-04-14 20:08:19 -0400

    Added Docgen for RaisedButtons
    Added the docgen for RaisedButtons for #20

--- src/js/Buttons/RaisedButton.js
+++ src/js/Buttons/RaisedButton.js
@@ -4,6 +4,10 @@ import classnames from 'classnames';
 
 import Button from './Button';
 
+/**
+ * Any other props such as style or event listeners will also
+ * be applied to the button.
+ */
 export default class RaisedButton extends Component {
   constructor(props) {
     super(props);
@@ -12,13 +16,74 @@ export default class RaisedButton extends Component {
   }
 
   static propTypes = {
+    /**
+     * The label to display in the button.
+     */
+    label: PropTypes.string.isRequired,
+
+    /**
+     * An optional className to apply to the button.
+     */
     className: PropTypes.string,
-    icon: PropTypes.string,
+
+    /**
+     * Boolean if the icon should be displayed before the label.
+     */
     iconBefore: PropTypes.bool,
+
+    /**
+     * A `FontIcon` to display in the button. It can be placed before
+     * or after the label.
+     */
+    children: PropTypes.node,
+
+    /**
+     * The button type.
+     */
+    type: PropTypes.string,
+
+    /**
+     * Boolean if the button should be styled with the primary color.
+     */
+    primary: PropTypes.bool,
+
+    /**
+     * Boolean if the button should be styled with the secondary color.
+     */
+    secondary: PropTypes.bool,
+
+    /**
+     * Boolean if the button is disabled.
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * An optional href to convert the button into a link button.
+     */
+    href: PropTypes.string,
+
+    /**
+     * An optional function to call when the button is clicked.
+     */
+    onClick: PropTypes.func,
+
+    /**
+     * An optional label to use if you would like a tooltip to display
+     * on hover or touch hold.
+     */
+    tooltipLabel: PropTypes.string,
+
+    /**
+     * The position that the tooltip should be displayed relative to
+     * the button.
+     */
+    tooltipPosition: PropTypes.oneOf(['left', 'right', 'bottom', 'left']),
   };
 
   static defaultProps = {
+    type: 'button',
     iconBefore: true,
+    tooltipPosition: 'bottom',
   };
 
   render() {
