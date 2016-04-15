5df7f53c9d8d8d6b13a8a4e953f3b956beeee705
Mikkel Laursen <mlaursen03@gmail.com>
2016-04-15 11:45:27 -0400

    Added react-docgen for Dividers and #20

--- src/js/Dividers/Divider.js
+++ src/js/Dividers/Divider.js
@@ -2,6 +2,10 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+/**
+ * The divider component will pass all other props such as style or
+ * event listeners on to the component.
+ */
 export default class Divider extends Component {
   constructor(props) {
     super(props);
@@ -10,8 +14,21 @@ export default class Divider extends Component {
   }
 
   static propTypes = {
+    /**
+     * An optional className to apply to the divider.
+     */
     className: PropTypes.string,
+
+    /**
+     * Boolean if this divider should be inset relative to it's container
+     * component. This means that if it is in a `List` with `Avatar`, it
+     * will start the divider  to the left of the main text in the list.
+     */
     inset: PropTypes.bool,
+
+    /**
+     * Boolean if the divider should be vertical instead of horizontal.
+     */
     vertical: PropTypes.bool,
   };
 
