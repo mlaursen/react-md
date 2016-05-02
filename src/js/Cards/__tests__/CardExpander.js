676399148bf7447727257eb156f0808839f8534f
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-02 12:22:33 -0400

    Added tests for CardExpander component

---
+++ src/js/Cards/__tests__/CardExpander.js
@@ -0,0 +1,56 @@
+/*eslint-env jest*/
+jest.unmock('../CardExpander');
+jest.unmock('../Card');
+jest.unmock('../CardActions');
+jest.unmock('../../Buttons');
+jest.unmock('../../Buttons/IconButton');
+jest.unmock('../../FontIcons');
+jest.unmock('../../FontIcons/FontIcon');
+jest.unmock('../../Inks');
+jest.unmock('../../Inks/injectInk');
+jest.unmock('../../Tooltips');
+jest.unmock('../../Tooltips/injectTooltip');
+
+import React from 'react';
+import {
+  Simulate,
+  renderIntoDocument,
+  findRenderedDOMComponentWithClass,
+} from 'react-addons-test-utils';
+
+import Card from '../Card';
+import CardActions from '../CardActions';
+
+describe('CardExpander', () => {
+  it('allows for a customizable expander icon', () => {
+    let card = renderIntoDocument(<Card><CardActions isExpander={true} /></Card>);
+    let icon = findRenderedDOMComponentWithClass(card, 'md-icon');
+    expect(icon.classList.contains('material-icons')).toBe(true);
+    expect(icon.textContent).toBe('keyboard_arrow_down');
+
+    card = renderIntoDocument(
+      <Card iconClassName="fa fa-github" iconChildren={null}>
+        <CardActions isExpander={true} />
+      </Card>
+    );
+
+    icon = findRenderedDOMComponentWithClass(card, 'md-icon');
+    expect(icon.classList.contains('fa')).toBe(true);
+    expect(icon.classList.contains('fa-github')).toBe(true);
+    expect(icon.textContent).toBe('');
+  });
+
+  it('allows for an optional onExpanderClick function', () => {
+    const onClick = jest.genMockFunction();
+    const card = renderIntoDocument(
+      <Card onExpanderClick={onClick}>
+        <CardActions isExpander={true} />
+      </Card>
+    );
+
+    const expander = findRenderedDOMComponentWithClass(card, 'md-icon-btn');
+    Simulate.click(expander);
+
+    expect(onClick.mock.calls.length).toBe(1);
+  });
+});
