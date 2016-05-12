1163775f0057b9a16dbb7ad2957b7c08dd65158c
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-12 14:38:32 -0400

    Enhancement/subheader tests (#84)
    * Updated Subheader to remove menu and menuComponent since it was undocumented and no idea what the purpose of it was.
    
    * Added initial subheader tests
    
    * Added Subheader docgen for #20

--- src/js/Subheaders/Subheader.js
+++ src/js/Subheaders/Subheader.js
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+/**
+ * The `Subheader` component is generally used inside of lists or menus.
+ */
 export default class Subheader extends Component {
   constructor(props) {
     super(props);
@@ -10,21 +13,43 @@ export default class Subheader extends Component {
   }
 
   static propTypes = {
-    className: PropTypes.string,
-    primary: PropTypes.bool,
-    inset: PropTypes.bool,
-    menu: PropTypes.bool,
-    menuComponent: PropTypes.string,
+    /**
+     * The text to display as a subheader.
+     */
     primaryText: PropTypes.string.isRequired,
+
+    /**
+     * The component to render the Subheader as.
+     */
+    component: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.func,
+    ]).isRequired,
+
+    /**
+     * An optional className to apply to the subheader.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Boolean if the subheader should be styled with the primary color.
+     */
+    primary: PropTypes.bool,
+
+    /**
+     * Boolean if the Subheader is inset in the list. This will add additional
+     * spacing to align the subheader.
+     */
+    inset: PropTypes.bool,
   };
 
   static defaultProps = {
-    menuComponent: 'h5',
+    component: 'li',
   };
 
   render() {
-    const { menu, menuComponent, inset, primary, primaryText, className, ...props } = this.props;
-    return React.createElement(menu ? menuComponent : 'li', {
+    const { component, inset, primary, primaryText, className, ...props } = this.props;
+    return React.createElement(component, {
       ...props,
       className: classnames('md-subheader', className, {
         inset,
