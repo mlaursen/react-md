2387d7ce3c4aa143894aeac7d1bfb51dda7ace4b
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-12 14:13:54 -0400

    Added Subheader docgen for #20

--- src/js/Subheaders/Subheader.js
+++ src/js/Subheaders/Subheader.js
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+/**
+ * The `Subheader` component is generally used inside of lists or menus.
+ */
 export default class Subheader extends Component {
   constructor(props) {
     super(props);
@@ -10,14 +13,34 @@ export default class Subheader extends Component {
   }
 
   static propTypes = {
+    /**
+     * The text to display as a subheader.
+     */
+    primaryText: PropTypes.string.isRequired,
+
+    /**
+     * The component to render the Subheader as.
+     */
     component: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.func,
     ]).isRequired,
+
+    /**
+     * An optional className to apply to the subheader.
+     */
     className: PropTypes.string,
+
+    /**
+     * Boolean if the subheader should be styled with the primary color.
+     */
     primary: PropTypes.bool,
+
+    /**
+     * Boolean if the Subheader is inset in the list. This will add additional
+     * spacing to align the subheader.
+     */
     inset: PropTypes.bool,
-    primaryText: PropTypes.string.isRequired,
   };
 
   static defaultProps = {
