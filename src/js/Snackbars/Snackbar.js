7d35ae7deba5fbbbad5f9f77d8165e6ed4d52c1e
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-13 09:50:37 -0400

    Added docgen for snackbars and #20

--- src/js/Snackbars/Snackbar.js
+++ src/js/Snackbars/Snackbar.js
@@ -6,6 +6,10 @@ import classnames from 'classnames';
 
 import Toast from './Toast';
 
+/**
+ * A snackbar takes a queue of toasts and displays them to the user one after another.
+ * They can be auto dismissed, or require user interaction to close the toast.
+ */
 export default class Snackbar extends Component {
   constructor(props) {
     super(props);
@@ -15,10 +19,33 @@ export default class Snackbar extends Component {
   }
 
   static propTypes = {
+    /**
+     * An optional className to apply to the active toast.
+     */
     className: PropTypes.string,
+
+    /**
+     * The moving queue of toasts to display.
+     *
+     * ##### Shape Description
+     */
     toasts: PropTypes.arrayOf(PropTypes.shape({
+      /**
+       * The text to display in the toast.
+       */
       text: PropTypes.string.isRequired,
+
+      /**
+       * An optional key for the toast.
+       */
       key: PropTypes.any,
+
+      /**
+       * An optional action to take. If this value is a string, a `FlatButton`
+       * will be created with a `label` of this value and the `onClick` function
+       * will be the `dismiss` function. If this is an object, all values will be
+       * applied to the `FlatButton`.
+       */
       action: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.shape({
@@ -26,15 +53,47 @@ export default class Snackbar extends Component {
           label: PropTypes.string.isRequired,
         }),
       ]),
+
+      /**
+       * An optional function to call when the toast appears.
+       */
       onAppear: PropTypes.func,
     })).isRequired,
+
+    /**
+     * Boolean if the toast should automatically hide after the given timeout.
+     */
     autohide: PropTypes.bool,
+
+    /**
+     * The auto hide timeout.
+     */
     autohideTimeout: PropTypes.number,
+
+    /**
+     * A function to call that will dismiss the active toast.
+     */
     dismiss: PropTypes.func.isRequired,
-    fabTimeout: PropTypes.number,
+
+    /**
+     * The transition name to use for the snackbar.
+     */
     transitionName: PropTypes.string.isRequired,
+
+    /**
+     * The transition enter timeout for the snackbar.
+     */
     transitionEnterTimeout: PropTypes.number.isRequired,
+
+    /**
+     * The transition leave timeout for the snackbar.
+     */
     transitionLeaveTimeout: PropTypes.number.isRequired,
+
+    /**
+     * An optional Floating Action Button element that should be moved
+     * when a toast appears/disappears.
+     */
     fab: PropTypes.object,
   };
 
@@ -44,11 +103,10 @@ export default class Snackbar extends Component {
     transitionName: 'snackbar',
     transitionEnterTimeout: 450,
     transitionLeaveTimeout: 450,
-    fabTimeout: 450,
     toasts: [],
   };
 
-  componentWillReceiveProps({ toasts, dismiss, autohide, autohideTimeout, fab, fabTimeout }) {
+  componentWillReceiveProps({ toasts, dismiss, autohide, autohideTimeout, fab, transitionEnterTimeout }) {
     if(this.props.toasts.length === toasts.length) { return; }
     const [toast] = toasts;
     toast && toast.onAppear && toast.onAppear();
@@ -61,7 +119,7 @@ export default class Snackbar extends Component {
       if(toast) {
         this.fabTimeout = setTimeout(() => {
           fab.classList.add(`snackbar${this.state.multiline ? '-multiline' : ''}-adjust`);
-        }, this.props.toasts.length > 1 ? fabTimeout : 0);
+        }, this.props.toasts.length > 1 ? transitionEnterTimeout : 0);
       }
     }
 
