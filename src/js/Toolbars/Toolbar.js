b01a62ba036e9b650ececf6627e6d2166f2c604e
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-13 13:14:53 -0400

    Enhancement/toolbar tests (#86)
    * Added initial toolbar tests for #60. Didn't do much since I play on changing it later
    
    * Added docgen for toolbars and #20
    
    * Fixed spelling error of portait -> portrait
    
    * Fixed overflow in swipeable views so each view is incharge of scrollbar

--- src/js/Toolbars/Toolbar.js
+++ src/js/Toolbars/Toolbar.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import classnames from 'classnames';
 
+/**
+ * A toolbar is a container that has an optional title and action areas.
+ *
+ * If you do not want to use the `NavigationDrawer` component, you can get
+ * almost the same effect by combining the `Toolbar` and `Sidebar` components.
+ */
 export default class Toolbar extends Component {
   constructor(props) {
     super(props);
@@ -12,13 +18,58 @@ export default class Toolbar extends Component {
   }
 
   static propTypes = {
+    /**
+     * An optional style to apply to the toobar header.
+     */
+    style: PropTypes.object,
+
+    /**
+     * An optional style to apply to the toolbar container.
+     */
+    containerStyle: PropTypes.object,
+
+    /**
+     * An optional className to apply to the header.
+     */
     className: PropTypes.string,
+
+    /**
+     * An optional className to apply to the toolbar container.
+     */
     containerClassName: PropTypes.string,
+
+    /**
+     * Boolean if the toolbar should be styled with the primary color.
+     */
     primary: PropTypes.bool,
-    actionLeft: PropTypes.node,
+
+    /**
+     * An element to render to the left of the toolbar's title. This is normally a
+     * Hamburger menu button or some sort of button.
+     */
+    actionLeft: PropTypes.element,
+
+    /**
+     * A title to include in the toolbar.
+     */
     title: PropTypes.string,
+
+    /**
+     * Any children to display underneath the toolbar title. This will
+     * normally be the `Tabs` component.
+     */
     children: PropTypes.node,
+
+    /**
+     * Any elements or nodes to display to the right of the toolbar title.
+     */
     actionsRight: PropTypes.node,
+
+    /**
+     * Boolean if the toolbar is fixed to the top of the page. If this is set to true,
+     * box-shadow will be added and any relative components/DOM nodes will have
+     * additional spacing applied to them.
+     */
     fixed: PropTypes.bool,
   };
 
@@ -44,7 +95,18 @@ export default class Toolbar extends Component {
   }
 
   render() {
-    const { actionLeft, title, actionsRight, children, fixed, primary, className, containerClassName, ...props } = this.props;
+    const {
+      actionLeft,
+      title,
+      actionsRight,
+      children,
+      fixed,
+      primary,
+      className,
+      containerStyle,
+      containerClassName,
+      ...props,
+    } = this.props;
     const { tabsOffset } = this.state;
     const childrenAsHeader = !!children && !actionLeft && !title && !actionsRight;
 
@@ -67,7 +129,7 @@ export default class Toolbar extends Component {
       });
     }
     return (
-      <div className={classnames('md-toolbar-container', containerClassName, { fixed })}>
+      <div className={classnames('md-toolbar-container', containerClassName, { fixed })} style={containerStyle}>
         <header
           {...props}
           className={classnames('md-toolbar', className, { 'md-primary': primary })}
