0cfea371bd459dde1975f31b9fb9c3ca975a3c1c
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-16 08:26:53 -0400

    Added tests for PickerControl and PickerFooter. Also converted them to non-pure React Components

--- src/js/Pickers/PickerFooter.js
+++ src/js/Pickers/PickerFooter.js
@@ -1,32 +1,56 @@
-import React, { PropTypes } from 'react';
-import { FlatButton } from '../Buttons';
+import React, { Component, PropTypes } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import classnames from 'classnames';
 
-const PickerFooter = ({ okLabel, okPrimary, onOkClick, cancelLabel, cancelPrimary, onCancelClick }) => {
-  return (
-    <footer className="md-dialog-footer">
-      <FlatButton
-        primary={cancelPrimary}
-        secondary={!cancelPrimary}
-        label={cancelLabel}
-        onClick={onCancelClick}
-      />
-      <FlatButton
-        primary={okPrimary}
-        secondary={!okPrimary}
-        label={okLabel}
-        onClick={onOkClick}
-      />
-    </footer>
-  );
-};
+import FlatButton from '../Buttons/FlatButton';
 
-PickerFooter.propTypes = {
-  okLabel: PropTypes.string.isRequired,
-  okPrimary: PropTypes.bool.isRequired,
-  onOkClick: PropTypes.func.isRequired,
-  cancelLabel: PropTypes.string.isRequired,
-  cancelPrimary: PropTypes.bool.isRequired,
-  onCancelClick: PropTypes.func.isRequired,
-};
+/**
+ * The `PickerFooter` component is a dialog footer for the `Picker` components.
+ * It renders an ok and cancel button.
+ */
+export default class PickerFooter extends Component {
+  constructor(props) {
+    super(props);
 
-export default PickerFooter;
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  static propTypes = {
+    className: PropTypes.string,
+    okLabel: PropTypes.string.isRequired,
+    okPrimary: PropTypes.bool.isRequired,
+    onOkClick: PropTypes.func.isRequired,
+    cancelLabel: PropTypes.string.isRequired,
+    cancelPrimary: PropTypes.bool.isRequired,
+    onCancelClick: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const {
+      className,
+      okLabel,
+      okPrimary,
+      onOkClick,
+      cancelLabel,
+      cancelPrimary,
+      onCancelClick,
+    } = this.props;
+
+    return (
+      <footer className={classnames('md-dialog-footer', className)}>
+        <FlatButton
+          primary={cancelPrimary}
+          secondary={!cancelPrimary}
+          label={cancelLabel}
+          onClick={onCancelClick}
+        />
+        <FlatButton
+          primary={okPrimary}
+          secondary={!okPrimary}
+          label={okLabel}
+          onClick={onOkClick}
+        />
+      </footer>
+    );
+  }
+}
