57de5abda9b6d52cf492b2fc388f5f7b7244a36b
Mikkel Laursen <mlaursen03@gmail.com>
2016-05-25 15:02:01 -0400

    Added ClockFace tests for TimePicker

--- src/js/Pickers/ClockFace.js
+++ src/js/Pickers/ClockFace.js
@@ -2,114 +2,113 @@ import React, { Component, PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 import { LEFT_MOUSE } from '../constants/keyCodes';
-import { getTouchOffset, isPointInCircle } from '../utils';
+import { getTouchOffset, isPointInCircle, isTouchDevice } from '../utils';
 import ClockTime from './ClockTime';
 import ClockHand from './ClockHand';
 
+/**
+ * The `ClockFace` component is used for rendering all the clock's times
+ * and the clock hand.
+ */
 export default class ClockFace extends Component {
   constructor(props) {
     super(props);
 
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-    this.state = { radius: 136 };
+    this.state = { radius: 136, moving: false };
   }
 
   static propTypes = {
+    /**
+     * The current time for the clock.
+     */
     time: PropTypes.number.isRequired,
+
+    /**
+     * Boolean if the clock is on the minutes view.
+     */
     minutes: PropTypes.bool.isRequired,
+
+    /**
+     * A function to call when a new time is selected. It gives the
+     * new time value. If it is 12 o'clock, 0 will be given.
+     */
     onClick: PropTypes.func.isRequired,
+
+    /**
+     * An optional time period string. This should be either AM or PM
+     * if the locale uses them.
+     */
     timePeriod: PropTypes.string,
   };
 
   componentDidMount() {
-    this.updateRadius();
+    this.init();
   }
 
-  updateRadius = () => {
-    const { offsetWidth } = this.refs.clockFace;
+  init = () => {
+    const radius = this.refs.clockFace.offsetWidth / 2;
+    const touch = isTouchDevice();
 
-    this.setState({ radius: offsetWidth / 2 });
+    this.setState({ radius, touch });
   };
 
   handleMouseDown = (e) => {
-    if(e.button !== LEFT_MOUSE || e.ctrlKey) { return; }
-    this.refs.clockFace.addEventListener('mousemove', this.handleMouseMove);
+    if(e.button !== LEFT_MOUSE || e.ctrlKey || this.state.touch) { return; }
+    this.setState({ moving: true });
   };
 
   handleMouseMove = (e) => {
+    if(!this.state.moving || this.state.touch) { return; }
     e.preventDefault();
-    let { offsetX, offsetY } = e;
-    if(typeof offsetX === 'undefined' || typeof offsetY === 'undefined') {
-      const offset = getTouchOffset(e);
-
-      offsetX = offset.offsetX;
-      offsetY = offset.offsetY;
-    }
-
-    this.calcNewTime(offsetX, offsetY);
+    this.calcNewTime(e);
   };
 
   handleMouseUp = (e) => {
-    if(e.button !== LEFT_MOUSE || e.ctrlKey) { return; }
-    this.refs.clockFace.removeEventListener('mousemove', this.handleMouseMove);
-    let { offsetX, offsetY } = e;
-    if(typeof offsetX === 'undefined' || typeof offsetY === 'undefined') {
-      const offset = getTouchOffset(e);
+    if(e.button !== LEFT_MOUSE || e.ctrlKey || this.state.touch) { return; }
+    this.calcNewTime(e);
+    this.setState({ moving: false });
+  };
 
-      offsetX = offset.offsetX;
-      offsetY = offset.offsetY;
-    }
-
-    this.calcNewTime(offsetX, offsetY);
+  handleTouchStart = () => {
+    this.setState({ moving: true });
   };
 
   handleTouchMove = (e) => {
+    if(!this.state.moving) { return; }
     e.preventDefault();
-    let { offsetX, offsetY } = e.changedTouches[0];
-    if(typeof offsetX === 'undefined' || typeof offsetY === 'undefined') {
-      const offset = getTouchOffset(e);
-
-      offsetX = offset.offsetX;
-      offsetY = offset.offsetY;
-    }
-
-    this.calcNewTime(offsetX, offsetY);
+    this.calcNewTime(e);
   };
 
   handleTouchEnd = (e) => {
-    let { offsetX, offsetY } = e.changedTouches[0];
-    if(typeof offsetX === 'undefined' || typeof offsetY === 'undefined') {
-      const offset = getTouchOffset(e);
-
-      offsetX = offset.offsetX;
-      offsetY = offset.offsetY;
-    }
-
-    this.calcNewTime(offsetX, offsetY);
+    this.calcNewTime(e);
+    this.setState({ moving: false });
   };
 
-  calcNewTime = (x, y) => {
+  calcNewTime = (e) => {
+    const { offsetX, offsetY } = getTouchOffset(e);
     const { radius } = this.state;
-    const sectors = this.props.minutes ? 60 : 12;
+    const { minutes, timePeriod, onClick } = this.props;
+    const sectors = minutes ? 60 : 12;
     const sectorSize = 360 / sectors;
-    const atan = Math.atan2(y - radius, x - radius);
+    const atan = Math.atan2(offsetY - radius, offsetX - radius);
     const degrees = atan * (180 / Math.PI);
     let time = Math.round(degrees / sectorSize);
 
     // time will be a negative number if it is the top half of the circle
-    time += (this.props.minutes ? 15 : 3);
+    time += (minutes ? 15 : 3);
     if(time < 0) {
       time += sectors;
     }
 
-    if(!this.props.timePeriod) {
-      const isInCircle = isPointInCircle(radius, radius, radius - 48, x, y);
+    if(!timePeriod && !minutes) {
+      const isInCircle = isPointInCircle(radius, radius, radius - 48, offsetX, offsetY);
       if((isInCircle && time !== 0) || (!isInCircle && time === 0)) {
         time += 12;
       }
     }
 
-    this.props.onClick(time);
+    onClick(time);
   };
 
   render() {
@@ -134,14 +133,17 @@ export default class ClockFace extends Component {
         />
       );
     });
+
     return (
       <div
         className="md-clock-face"
         ref="clockFace"
         onMouseDown={this.handleMouseDown}
+        onMouseMove={this.handleMouseMove}
         onMouseUp={this.handleMouseUp}
+        onTouchStart={this.handleTouchStart}
         onTouchMove={this.handleTouchMove}
-        onTouchEnd={this.handleTouchStart}
+        onTouchEnd={this.handleTouchEnd}
       >
         {times}
         <ClockHand time={time} coords={radius} minutes={minutes} />
---
+++ src/js/Pickers/__tests__/ClockFace.js
@@ -0,0 +1,190 @@
+/*eslint-env jest*/
+jest.unmock('../ClockFace');
+
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import {
+  Simulate,
+  renderIntoDocument,
+//  findRenderedComponentWithType,
+  scryRenderedComponentsWithType,
+//  scryRenderedDOMComponentsWithTag,
+} from 'react-addons-test-utils';
+
+import { LEFT_MOUSE, RIGHT_MOUSE } from '../../constants/keyCodes';
+import ClockFace from '../ClockFace';
+import ClockTime from '../ClockTime';
+import ClockHand from '../ClockHand';
+
+describe('ClockFace', () => {
+  it('renders a 12 hour clock if there is a time period prop and is not displayed in minutes', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="AM" />
+    );
+
+    const times = scryRenderedComponentsWithType(face, ClockTime);
+    expect(times.length).toBe(12);
+  });
+
+  it('renders a 24 hour clock if there is not a time period prop and is not displayed in minutes', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} />
+    );
+
+    const times = scryRenderedComponentsWithType(face, ClockTime);
+    expect(times.length).toBe(24);
+  });
+
+  it('renders minutes in 5 minute increments starting from 0', () => {
+    const onClick = jest.fn();
+    let face = renderIntoDocument(
+      <ClockFace time={3} minutes={true} onClick={onClick} />
+    );
+
+    let times = scryRenderedComponentsWithType(face, ClockTime);
+    expect(times.length).toBe(60 / 5);
+
+    face = renderIntoDocument(
+      <ClockFace time={3} minutes={true} onClick={onClick} timePeriod="AM" />
+    );
+
+    times = scryRenderedComponentsWithType(face, ClockTime);
+    expect(times.length).toBe(60 / 5);
+  });
+
+  it('sets the prop active for the correct time', () => {
+    const onClick = jest.fn();
+    let face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} />
+    );
+
+    let times = scryRenderedComponentsWithType(face, ClockTime);
+    expect(times[2].props.active).toBe(true);
+
+    face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="AM" />
+    );
+
+    times = scryRenderedComponentsWithType(face, ClockTime);
+    expect(times[2].props.active).toBe(true);
+  });
+
+  it('renders a clock hand component', () => {
+    const props = {
+      onClick: jest.fn(),
+      time: 3,
+      minutes: true,
+    };
+
+    const face = renderIntoDocument(<ClockFace {...props} />);
+    face.setState({ radius: 136 });
+
+    const hands = scryRenderedComponentsWithType(face, ClockHand);
+    expect(hands.length).toBe(1);
+    const [hand] = hands;
+    expect(hand.props.time).toBe(props.time);
+    expect(hand.props.coords).toBe(136);
+    expect(hand.props.minutes).toBe(props.minutes);
+  });
+
+  it('calls the on click prop with the selected time on mouse up', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="A" />
+    );
+    face.setState({ radius: 136 });
+
+    const faceNode = findDOMNode(face);
+    // Click at 12 o'clock
+    Simulate.mouseUp(faceNode, { button: LEFT_MOUSE, offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(1);
+    expect(onClick.mock.calls[0][0]).toBe(0);
+
+    // Click at 3 o'clock
+    Simulate.mouseUp(faceNode, { button: LEFT_MOUSE, offsetX: 272, offsetY: 136 });
+    expect(onClick.mock.calls.length).toBe(2);
+    expect(onClick.mock.calls[1][0]).toBe(3);
+  });
+
+  it('calls the onClick prop with the selected time on mousemove until the mouse up event', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="A" />
+    );
+    face.setState({ radius: 136 });
+
+    const faceNode = findDOMNode(face);
+
+    Simulate.mouseMove(faceNode, { offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(0);
+
+    Simulate.mouseDown(faceNode, { button: LEFT_MOUSE });
+    Simulate.mouseMove(faceNode, { offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(1);
+
+    Simulate.mouseMove(faceNode, { offsetX: 120, offsetY: 20 });
+    expect(onClick.mock.calls.length).toBe(2);
+
+    Simulate.mouseUp(faceNode, { button: LEFT_MOUSE, offsetX: 125, offsetY: 25 });
+    expect(onClick.mock.calls.length).toBe(3);
+
+    Simulate.mouseMove(faceNode, { offsetX: 130, offsetY: 30 });
+    expect(onClick.mock.calls.length).toBe(3);
+  });
+
+  it('does not start listening to mousemove events if the control key was pressed on mousedown', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="A" />
+    );
+    face.setState({ radius: 136 });
+
+    const faceNode = findDOMNode(face);
+
+    Simulate.mouseDown(faceNode, { button: LEFT_MOUSE, ctrlKey: true });
+    Simulate.mouseMove(faceNode, { offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(0);
+  });
+
+  it('does not start listening to mousemove events if the right mouse key was pressed on mousedown', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="A" />
+    );
+    face.setState({ radius: 136 });
+
+    const faceNode = findDOMNode(face);
+
+    Simulate.mouseDown(faceNode, { button: RIGHT_MOUSE });
+    Simulate.mouseMove(faceNode, { offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(0);
+  });
+
+  it('calls the onClick prop with the selected time on touchmove until the touchend event', () => {
+    const onClick = jest.fn();
+    const face = renderIntoDocument(
+      <ClockFace time={3} minutes={false} onClick={onClick} timePeriod="A" />
+    );
+    face.setState({ radius: 136 });
+
+    const faceNode = findDOMNode(face);
+
+    Simulate.touchMove(faceNode, { offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(0);
+
+    Simulate.touchStart(faceNode);
+    Simulate.touchMove(faceNode, { offsetX: 136, offsetY: 15 });
+    expect(onClick.mock.calls.length).toBe(1);
+
+    Simulate.touchMove(faceNode, { offsetX: 120, offsetY: 20 });
+    expect(onClick.mock.calls.length).toBe(2);
+
+    Simulate.touchEnd(faceNode, { offsetX: 125, offsetY: 25 });
+    expect(onClick.mock.calls.length).toBe(3);
+
+    Simulate.touchMove(faceNode, { offsetX: 130, offsetY: 30 });
+    expect(onClick.mock.calls.length).toBe(3);
+  });
+});
