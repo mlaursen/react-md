9dcfcee54a4fbdcab633e92f4bb099019aa1a97f
Mikkel Laursen <mlaursen03@gmail.com>
2016-06-01 13:20:10 -0400

    Feature/bottom navigations (#94)
    * Added BottomNav component and Tests
    
    Added a component for rendering the bottom navigation nav item and added tests.
    
    * Fixed some sassdoc pluralization problems
    
    * Added the BottomNavigation Component
    
    Added the BottomNavigation component tests, source, and styling.
    
    This includes the mixins for styling and updating the main scss source to include those files.

---
+++ src/js/BottomNavigations/__tests__/BottomNavigation.js
@@ -0,0 +1,141 @@
+/*eslint-env jest*/
+jest.unmock('../BottomNavigation');
+
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import {
+  renderIntoDocument,
+  scryRenderedComponentsWithType,
+  scryRenderedDOMComponentsWithTag,
+  findRenderedDOMComponentWithClass,
+} from 'react-addons-test-utils';
+
+import BottomNavigation from '../BottomNavigation';
+import BottomNav from '../BottomNav';
+
+describe('BottomNavigation', () => {
+  it('merges className and style', () => {
+    const style = { display: 'block' };
+    const className = 'test';
+    const containerStyle = { background: 'red' };
+    const containerClassName = 'test-container';
+    const nav = renderIntoDocument(
+      <BottomNavigation
+        style={style}
+        className={className}
+        containerStyle={containerStyle}
+        containerClassName={containerClassName}
+        actions={[{ label: 'a' }, { label: 'b' }, { label: 'c' }]}
+      />
+    );
+
+    const navNode = findDOMNode(nav);
+    expect(navNode.style.background).toBe(containerStyle.background);
+    expect(navNode.className).toContain(containerClassName);
+
+    const bottomNav = findRenderedDOMComponentWithClass(nav, 'md-bottom-navigation');
+    expect(bottomNav.className).toContain(className);
+    expect(bottomNav.style.display).toBe(style.display);
+  });
+
+  it('can be a controlled component', () => {
+    const props = {
+      active: 'A',
+      actions: [{
+        label: 'A',
+      }, {
+        label: 'B',
+      }, {
+        label: 'C',
+      }],
+      onChange: jest.fn(),
+    };
+
+    const nav = renderIntoDocument(<BottomNavigation {...props} />);
+
+    nav.handleNavChange('B');
+    expect(props.onChange.mock.calls.length).toBe(1);
+    expect(props.onChange.mock.calls[0][0]).toBe('B');
+  });
+
+  it('generates a BottomNav component for each action', () => {
+    let actions = [{
+      label: 'A',
+    }, {
+      label: 'B',
+    }, {
+      label: 'C',
+    }];
+
+    let nav = renderIntoDocument(<BottomNavigation actions={actions} />);
+    let navs = scryRenderedComponentsWithType(nav, BottomNav);
+
+    expect(navs.length).toBe(actions.length);
+
+    actions = actions.concat([{ label: 'D' }, { label: 'E' }]);
+    nav = renderIntoDocument(<BottomNavigation actions={actions} />);
+    navs = scryRenderedComponentsWithType(nav, BottomNav);
+
+    expect(navs.length).toBe(actions.length);
+  });
+
+  it('only displays the bottom navigation when visible', () => {
+    const actions = [{ label: 'A' }, { label: 'B' }, { label: 'C' }];
+    const nav = renderIntoDocument(<BottomNavigation actions={actions} />);
+
+    let navs = scryRenderedDOMComponentsWithTag(nav, 'footer');
+    expect(navs.length).toBe(1);
+
+    nav.setState({ visible: false });
+    jest.runAllTimers();
+
+    navs = scryRenderedDOMComponentsWithTag(nav, 'footer');
+    expect(navs.length).toBe(0);
+  });
+
+  it('updates the state when the onTouchStart is called', () => {
+    const actions = [{ label: 'A' }, { label: 'B' }, { label: 'C' }];
+    const nav = renderIntoDocument(<BottomNavigation actions={actions} />);
+
+    nav.handleTouchStart({ changedTouches: [{ pageY: 32 }]});
+    expect(nav.state.scrolling).toBe(true);
+    expect(nav.state.pageY).toBe(32);
+  });
+
+  it('updates the state when the onTouchMove is called only if already scrolling', () => {
+    const actions = [{ label: 'A' }, { label: 'B' }, { label: 'C' }];
+    const nav = renderIntoDocument(<BottomNavigation actions={actions} />);
+
+    const startEvent = { changedTouches: [{ pageY: 32 }] };
+    // scrolling downwards
+    const moveEvent = { changedTouches: [{ pageY: 20 }] };
+    expect(nav.state.visible).toBe(true);
+
+    nav.handleTouchMove(moveEvent);
+
+    expect(nav.state.visible).toBe(true);
+    expect(nav.state.scrolling).toBe(false);
+    expect(nav.state.pageY).toBe(null);
+
+    nav.handleTouchStart(startEvent);
+    expect(nav.state.visible).toBe(true);
+    expect(nav.state.scrolling).toBe(true);
+    expect(nav.state.pageY).toBe(32);
+
+    nav.handleTouchMove(moveEvent);
+    expect(nav.state.visible).toBe(false);
+    expect(nav.state.pageY).toBe(20);
+    expect(nav.state.scrolling).toBe(true);
+  });
+
+  it('resets the scrolling state onTouchEnd', () => {
+    const actions = [{ label: 'A' }, { label: 'B' }, { label: 'C' }];
+    const nav = renderIntoDocument(<BottomNavigation actions={actions} />);
+
+    nav.handleTouchStart({ changedTouches: [{ pageY: 32 }] });
+    nav.handleTouchEnd();
+    expect(nav.state.visible).toBe(true);
+    expect(nav.state.pageY).toBe(null);
+    expect(nav.state.scrolling).toBe(false);
+  });
+});
