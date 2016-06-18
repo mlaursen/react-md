60ac3548cc751462c9b9bcb054b0751af9a2629c
Mikkel Laursen <mlaursen03@gmail.com>
2016-06-18 12:47:02 -0400

    Implemented a single file UMD Build (#114)
    * Implemented a single file UMD Build
    
    Added a single-file UMD build so that this can be used from the browser.
    
    * Updated compiled scss to go to dist since it makes more since and hopefully people aren't even using this giant file

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+/*eslint-env node*/
+const webpack = require('webpack');
+
+function makeConfig() {
+  return {
+    entry: './src/js/index.js',
+
+    externals: {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+      'react-addons-pure-render-mixin': 'var React.addons.PureRenderMixin',
+      'react-addons-css-transition-group': 'var React.addons.CSSTransitionGroup',
+      'react-addons-transition-group': 'var React.addons.TransitionGroup',
+    },
+
+    module: {
+      loaders: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel' }],
+    },
+
+    output: {
+      publicPath: '/',
+      path: './dist',
+      library: 'ReactMD',
+      libraryTarget: 'umd',
+    },
+
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+    ],
+
+    resolve: {
+      extensions: ['', '.js'],
+    },
+  };
+}
+
+const devConfig = makeConfig();
+devConfig.output.filename = 'react-md.js';
+devConfig.devtool = 'source-map';
+devConfig.plugins = devConfig.plugins.concat([
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('development'),
+    },
+  }),
+]);
+
+const prodConfig = makeConfig();
+prodConfig.output.filename = 'react-md.min.js';
+prodConfig.plugins = prodConfig.plugins.concat([
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production'),
+    },
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: { warnings: false },
+    output: { comments: false },
+  }),
+]);
+
+module.exports = [devConfig, prodConfig];
