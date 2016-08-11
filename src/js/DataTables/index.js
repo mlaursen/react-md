45d9cd2cccb3e75ec5625b3aa0e79d57becd2395
Mikkel Laursen <mlaursen03@gmail.com>
2016-08-11 11:52:07 -0600

    Added the TablePagination Component
    Added a component for helping paginate tables.

--- src/js/DataTables/index.js
+++ src/js/DataTables/index.js
@@ -4,3 +4,4 @@ export TableBody from './TableBody';
 export TableRow from './TableRow';
 export TableColumn from './TableColumn';
 export EditDialogColumn from './EditDialogColumn';
+export TablePagination from './TablePagination';
