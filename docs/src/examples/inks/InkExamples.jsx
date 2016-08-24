83b2280095cd7870729c995c223c9a05fd1c83c0
Mikkel Laursen <mlaursen03@gmail.com>
2016-08-24 12:53:13 -0600

    Extracted the InkExamples Button to it's own file.

--- docs/src/examples/inks/InkExamples.jsx
+++ docs/src/examples/inks/InkExamples.jsx
@@ -1,37 +1,6 @@
-import React, { PureComponent, PropTypes } from 'react';
+import React from 'react';
 
-import injectInk from 'react-md/lib/Inks';
-
-@injectInk
-class TerriblyInaccessibleFakeButton extends PureComponent {
-  static propTypes = {
-    children: PropTypes.node,
-    disabled: PropTypes.bool,
-
-    // Injected from injectInk
-    ink: PropTypes.node,
-    // It is required to pass all these event listeners onto your component to
-    // get the full ink effect
-    onMouseDown: PropTypes.func,
-    onMouseUp: PropTypes.func,
-    onMouseLeave: PropTypes.func,
-    onKeyUp: PropTypes.func,
-    onBlur: PropTypes.func,
-    onTouchStart: PropTypes.func,
-    onTouchEnd: PropTypes.func,
-    onTouchCancel: PropTypes.func,
-  };
-
-  render() {
-    const { ink, children, ...props } = this.props;
-    return (
-      <div className={`terribly-inaccessible-fake-button${this.props.disabled ? ' disabled' : ''}`} {...props}>
-        {ink}
-        {children}
-      </div>
-    );
-  }
-}
+import TerriblyInaccessibleFakeButton from './TerriblyInaccessibleFakeButton';
 
 const InkExamples = () => (
   <div>
