03d6490a2b5554d6cd738145bd9e507c1b263ffb
Mikkel Laursen <mlaursen03@gmail.com>
2016-08-25 17:23:03 -0600

    Updated documentation website a bit for Sliders

--- docs/src/examples/sliders/index.js
+++ docs/src/examples/sliders/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 import SimpleContinuousExamples from './SimpleContinuousExamples';
 import SimpleContinuousExamplesRaw from '!!raw!./SimpleContinuousExamples';
+import IconExamples from './IconExamples';
+import IconExamplesRaw from '!!raw!./IconExamples';
+import EditableExample from './EditableExample';
+import EditableExampleRaw from '!!raw!./EditableExample';
+import EditableExampleSCSS from '!!raw!./_editable-example.scss';
 
 export default [{
   title: 'Simple Continuous Example',
   code: SimpleContinuousExamplesRaw,
   children: <SimpleContinuousExamples />,
+}, {
+  title: 'With Icon Examples',
+  code: IconExamplesRaw,
+  children: <IconExamples />,
+}, {
+  title: 'Editable Example',
+  code: `
+/* EditableExample.jsx */
+${EditableExampleRaw}
+\`\`\`
+
+\`\`\`scss
+/* _editable-example.scss */
+${EditableExampleSCSS}
+`,
+  children: <EditableExample />,
 }];
