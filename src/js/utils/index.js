8c259d3becc4bb6b5de2e12e5d25d89511f04480
Mikkel Laursen <mlaursen03@gmail.com>
2016-08-27 14:40:50 -0600

    More Accurate Sliders Round 2
    Updated the logic behind sliders to be a bit more accurate and allowing
    fractional steps from 0 - 1. Also updated the prop validation to hopefully
    give usefull debugging messages why things look weird.

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,5 +1,19 @@
 /* eslint-disable no-param-reassign */
 
+/**
+ * Gets the current field for a component that can the field
+ * as either uncontrolled or controlled.
+ *
+ * @param {Object} props - the props object.
+ * @param {Object} state = the state object.
+ * @param {string=} field - the field to extract a value from. Defaults to 'value'.
+ *
+ * @return the field's value.
+ */
+export function getField(props, state, field = 'value') {
+  return typeof props[field] !== 'undefined' ? props[field] : state[field];
+}
+
 export function setOverflow(enabled, selector) {
   const el = selector ? document.querySelector(selector) : document.body;
   if (enabled) {
