3b2df3bf8393165e8ac95c115a626791ee0b2c72
Mikkel Laursen <mlaursen03@gmail.com>
2016-09-14 11:35:48 -0600

    Fixed the Mocks for the PropTypes

--- src/js/utils/__mocks__/PropTypes.js
+++
@@ -1,6 +0,0 @@
-/* eslint-env jest */
-import { PropTypes } from 'react';
-
-export const controlled = jest.fn((funcName, validator) => validator);
-export const minNumber = jest.fn(() => PropTypes.number);
-export const maxNumber = jest.fn(() => PropTypes.number);
