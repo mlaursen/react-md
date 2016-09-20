39faebb8021989f9900bb8921924607816e396a4
Mikkel Laursen <mlaursen03@gmail.com>
2016-09-20 12:29:37 -0600

    Updated papers to not automatically raise on hover for zDepth=0

--- docs/src/examples/papers/PaperExamples.jsx
+++ docs/src/examples/papers/PaperExamples.jsx
@@ -3,7 +3,7 @@ import Paper from 'react-md/lib/Papers';
 
 const PaperExamples = () => {
   const papers = Array.apply(null, new Array(6)).map((_, i) => {
-    return <Paper key={i} zDepth={i} className="paper-example">zDepth = {i}</Paper>;
+    return <Paper key={i} zDepth={i} raiseOnHover={i === 0} className="paper-example">zDepth = {i}</Paper>;
   });
 
   return (
