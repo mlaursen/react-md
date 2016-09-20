8a161b887bcda4189d60d13d120e4f3c7617cf54
Mikkel Laursen <mlaursen03@gmail.com>
2016-09-20 12:39:19 -0600

    Updated Menus for a Context Menu position and stuff.

--- docs/src/examples/menus/index.js
+++ docs/src/examples/menus/index.js
@@ -6,6 +6,9 @@ import MenuExamplesRaw from '!!raw!./MenuExamples';
 import MenuButtonExamples from './MenuButtonExamples';
 import MenuButtonExamplesRaw from '!!raw!./MenuButtonExamples';
 
+import ContextMenuExample from './ContextMenuExample';
+import ContextMenuExampleRaw from '!!raw!./ContextMenuExample';
+
 import './_menus.scss';
 
 export default [{
@@ -16,4 +19,12 @@ export default [{
   title: 'Menu Button Examples',
   code: MenuButtonExamplesRaw,
   children: <MenuButtonExamples />,
+}, {
+  title: 'Context Menu Example',
+  description: `
+> This is an example replacing the default context menu with the \`Menu\` component. The
+items will be visible if you right click anywhere in the Lorem Ipsum text below.
+`,
+  code: ContextMenuExampleRaw,
+  children: <ContextMenuExample />,
 }];
