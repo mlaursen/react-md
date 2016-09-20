88a66fa83dc91f491e891eb7dc61d4a5eb08b665
Mikkel Laursen <mlaursen03@gmail.com>
2016-09-20 13:25:49 -0600

    Added missing styles for IconSeparator examples

--- docs/src/examples/helpers/icon-separator/index.js
+++ docs/src/examples/helpers/icon-separator/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 import SimpleExamples from './SimpleExamples';
 import SimpleExamplesRaw from '!!raw!./SimpleExamples';
+import IconSCSS from '!!raw!./_icon-separators.scss';
 
 export default [{
   title: 'Simple Examples',
-  code: SimpleExamplesRaw,
+  code: `
+/* SimpleExamples.jsx */
+${SimpleExamplesRaw}
+
+\`\`\`
+
+\`\`\`scss
+/* _icon-separators.scss */
+${IconSCSS}
+`,
   children: <SimpleExamples />,
 }];
