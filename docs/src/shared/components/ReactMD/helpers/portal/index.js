78d40222496eceaa524aedf2393407bc5937d1e4
Mikkel Laursen <mlaursen03@gmail.com>
2016-12-14 15:30:38 -0700

    General Keyboard Accessibility fixes for documentation website

--- docs/src/shared/components/ReactMD/helpers/portal/index.js
+++ docs/src/shared/components/ReactMD/helpers/portal/index.js
@@ -5,6 +5,10 @@ import SimpleExampleRaw from '!!raw!./SimpleExample';
 
 export default [{
   title: 'Simple Example',
+  description: `
+Clicking on the button in this example will create a temporary overlay. Keyboard users can press the
+enter key once open to close the overlay. All other users can just click or touch the overlay.
+`,
   code: SimpleExampleRaw,
   children: <SimpleExample />,
 }];
