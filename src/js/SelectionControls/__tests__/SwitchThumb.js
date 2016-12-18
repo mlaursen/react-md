435a91f52c992c0d461e3455ddd290da7e4f4135
Mikkel Laursen <mlaursen03@gmail.com>
2016-12-17 17:11:14 -0700

    Fixed double onChange firing for Switch #182
    The switch's thumb and the switch's track need the onClick handler so
    that either the track can be clicked or the thumb can be _clicked_ by
    pressing the enter key. However, if a user actually touches or clicks
    the thumb itself, the click event propigates from the thumb to the track
    as well, so two onChange events are emitted.
    
    This commit prevents that click propigation from the switch's thumb.

--- src/js/SelectionControls/__tests__/SwitchThumb.js
+++ src/js/SelectionControls/__tests__/SwitchThumb.js
@@ -11,15 +11,16 @@ import {
 import SwitchThumb from '../SwitchThumb';
 import AccessibleFakeInkedButton from '../../Helpers/AccessibleFakeInkedButton';
 
+const noop = () => {};
 describe('SwitchThumb', () => {
   it('renders an AccessibleFakeInkedButton', () => {
-    const thumb = renderIntoDocument(<SwitchThumb />);
+    const thumb = renderIntoDocument(<SwitchThumb onClick={noop} />);
     const btns = scryRenderedComponentsWithType(thumb, AccessibleFakeInkedButton);
     expect(btns.length).toBe(1);
   });
 
   it('renders an AccessibleFakeInkedButton with the correct classNames', () => {
-    const props = { disabled: false, checked: false };
+    const props = { disabled: false, checked: false, onClick: noop };
     let thumb = renderIntoDocument(<SwitchThumb {...props} />);
     let btn = findRenderedComponentWithType(thumb, AccessibleFakeInkedButton);
     expect(btn.props.className).toContain('md-switch-thumb');
