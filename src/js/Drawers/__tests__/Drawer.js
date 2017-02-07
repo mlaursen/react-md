b215f0bdb28c30eedebb0037af7f0be6f09d5f86
Mikkel Laursen <mlaursen03@gmail.com>
2017-02-06 22:35:09 -0700

    Added some better tests for the static methods on the Drawer

--- src/js/Drawers/__tests__/Drawer.js
+++ src/js/Drawers/__tests__/Drawer.js
@@ -34,4 +34,65 @@ describe('Drawer', () => {
     expect(drawer.context.renderNode).toBe(renderNode);
     expect(dialogContainer.context.renderNode).toBe(renderNode);
   });
+
+  describe('matchesMedia', () => {
+    const MATCH_MEDIA = window.matchMedia;
+    let matchMedia;
+    beforeEach(() => {
+      matchMedia = jest.fn(() => ({ matches: false }));
+      window.matchMedia = matchMedia;
+    });
+
+    it('should call the window.matchMedia with a min width', () => {
+      Drawer.matchesMedia(320);
+      expect(matchMedia.mock.calls.length).toBe(1);
+      expect(matchMedia.mock.calls[0][0]).toBe('screen and (min-width: 320px)');
+
+      Drawer.matchesMedia(800);
+      expect(matchMedia.mock.calls.length).toBe(2);
+      expect(matchMedia.mock.calls[1][0]).toBe('screen and (min-width: 800px)');
+    });
+
+    it('should call window.matchMedia with the min and max width', () => {
+      Drawer.matchesMedia(320, 800);
+      expect(matchMedia.mock.calls.length).toBe(1);
+      expect(matchMedia.mock.calls[0][0]).toBe('screen and (min-width: 320px) and (max-width: 800px)');
+    });
+
+    afterAll(() => {
+      window.matchMedia = MATCH_MEDIA;
+    });
+  });
+
+  describe('getCurrentMedia', () => {
+    const { mobileMinWidth, tabletMinWidth, desktopMinWidth } = Drawer.defaultProps;
+    const MATCH_MEDIA = window.matchMedia;
+    it('should return the mobile drawer type when the media matches mobile', () => {
+      window.matchMedia = jest.fn(query => ({
+        matches: !!query.match(`min-width: ${mobileMinWidth}`),
+      }));
+      const expected = { mobile: true, tablet: false, desktop: false, type: Drawer.defaultProps.mobileType };
+      expect(Drawer.getCurrentMedia()).toEqual(expected);
+    });
+
+    it('should return the tablet drawer type when the media matches tablet', () => {
+      window.matchMedia = jest.fn(query => ({
+        matches: !!query.match(`min-width: ${tabletMinWidth}`),
+      }));
+      const expected = { mobile: false, tablet: true, desktop: false, type: Drawer.defaultProps.tabletType };
+      expect(Drawer.getCurrentMedia()).toEqual(expected);
+    });
+
+    it('should return the desktop drawer type when the media matches desktop', () => {
+      window.matchMedia = jest.fn(query => ({
+        matches: !!query.match(`min-width: ${desktopMinWidth}`),
+      }));
+      const expected = { mobile: false, tablet: false, desktop: true, type: Drawer.defaultProps.desktopType };
+      expect(Drawer.getCurrentMedia()).toEqual(expected);
+    });
+
+    afterAll(() => {
+      window.matchMedia = MATCH_MEDIA;
+    });
+  });
 });
