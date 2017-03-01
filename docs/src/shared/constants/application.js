4446b703d04f7fe814162b82a43fc4198d966423
Mikkel Laursen <mlaursen03@gmail.com>
2017-03-01 14:29:05 -0700

    Documenttion url building fix

--- docs/src/shared/constants/application.js
+++ docs/src/shared/constants/application.js
@@ -7,13 +7,15 @@ const {
   protocol: apiProtocol,
 } = require('../../../../api/src/server/config');
 
+const buildURI = (protocol, host, port, path) => `${protocol}://${host}${__DEV__ && port ? `:${port}` : ''}${path}`;
+
 export const VERSION = version;
 export const GITHUB_URL = bugs.url.replace('/issues', '');
 export const THEME_STORAGE_KEY = 'save-theme';
 export const CUSTOM_THEME_CLASS_NAME = 'custom-theme';
 export const FOOD_DATA_URL = 'https://data.cityofchicago.org/api/views/4ijn-s7e5/rows.json?accessType=DOWNLOAD';
 
-export const API_URL = `${apiProtocol}://${apiHost}${apiPort ? `:${apiPort}` : ''}${apiPath}`;
+export const API_URL = buildURI(apiProtocol, apiHost, apiPort, apiPath);
 
-export const BASE_URL = `${protocol}://${host}${port ? `:${port}` : ''}${path}`;
+export const BASE_URL = buildURI(protocol, host, port, path);
 export const PROXY_URL = `${BASE_URL}/proxy?url=`;
