9cab6084568db47c56f91e3493c30b68f2c6e7fa
Mikkel Laursen <mlaursen03@gmail.com>
2017-03-31 08:29:26 -0600

    Fixed the Google Docs dropdown menu

--- docs/src/shared/components/ReactMD/menus/GoogleDocsClone/DropDownMenu.jsx
+++ docs/src/shared/components/ReactMD/menus/GoogleDocsClone/DropDownMenu.jsx
@@ -1,13 +1,12 @@
 import React, { PureComponent, PropTypes } from 'react';
 import cn from 'classnames';
-import Menu from 'react-md/lib/Menus/Menu';
+import DropdownMenu from 'react-md/lib/Menus/DropdownMenu';
 import AccessibleFakeInkedButton from 'react-md/lib/Helpers/AccessibleFakeInkedButton';
-import mapToListParts from 'react-md/lib/utils/mapToListParts';
 
 import toClassName from 'utils/StringUtils/toClassName';
 const anchor = {
-  x: Menu.HorizontalAnchors.INNER_LEFT,
-  y: Menu.VerticalAnchors.BOTTOM,
+  x: DropdownMenu.HorizontalAnchors.INNER_LEFT,
+  y: DropdownMenu.VerticalAnchors.BOTTOM,
 };
 
 export default class DropDownMenu extends PureComponent {
@@ -24,12 +23,8 @@ export default class DropDownMenu extends PureComponent {
     this.state = { mouseover: false, visible: false };
   }
 
-  _toggle = () => {
-    this.setState({ visible: true });
-  };
-
-  _close = () => {
-    this.setState({ visible: false });
+  _handleVisibilityChange = (visible) => {
+    this.setState({ visible });
   };
 
   _handleMouseOver = () => {
@@ -44,36 +39,31 @@ export default class DropDownMenu extends PureComponent {
     const { text, items, ...props } = this.props;
     const { mouseover, visible } = this.state;
 
-    const toggle = (
-      <AccessibleFakeInkedButton
-        className={cn('google-docs-dd-menu', {
-          'md-btn--hover': mouseover,
-          'md-paper md-paper--2': visible,
-        })}
-        onClick={this._toggle}
-        onMouseOver={this._handleMouseOver}
-        onMouseLeave={this._handleMouseLeave}
-        tabbedClassName="md-btn--hover"
-      >
-        {text}
-      </AccessibleFakeInkedButton>
-    );
-
     return (
-      <Menu
+      <DropdownMenu
         {...props}
         id={`doc-control-${toClassName(text)}`}
         listClassName="google-docs-list"
-        anchor={anchor}
+        belowAnchor={anchor}
         cascading
         visible={visible}
-        toggle={toggle}
-        onClose={this._close}
-        position={Menu.Positions.BELOW}
+        onVisibilityChange={this._handleVisibilityChange}
+        position={DropdownMenu.Positions.BELOW}
         listHeightRestricted={false}
+        menuItems={items}
       >
-        {items.map(mapToListParts)}
-      </Menu>
+        <AccessibleFakeInkedButton
+          className={cn('google-docs-dd-menu', {
+            'md-btn--hover': mouseover,
+            'md-paper md-paper--2': visible,
+          })}
+          onMouseOver={this._handleMouseOver}
+          onMouseLeave={this._handleMouseLeave}
+          tabbedClassName="md-btn--hover"
+        >
+          {text}
+        </AccessibleFakeInkedButton>
+      </DropdownMenu>
     );
   }
 }
