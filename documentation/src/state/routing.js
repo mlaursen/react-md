1f9a691a6cefd017878036b4e2c15bc2e48626f1
Mikkel Laursen <mlaursen03@gmail.com>
2017-06-03 20:28:06 -0600

    Updated documentaiton for the routing reducer

--- documentation/src/state/routing.js
+++ documentation/src/state/routing.js
@@ -1,5 +1,12 @@
 export const LOCATION_CHANGE = 'LOCATION_CHANGE';
 
+/**
+ * This is a simple plug-in replacement for react-router-redux until it supports
+ * react-router v4.
+ *
+ * @param {Object} location - the next location object on route change.
+ * @return {Object} the action
+ */
 export function updateLocation(location) {
   return { type: LOCATION_CHANGE, payload: { location } };
 }
