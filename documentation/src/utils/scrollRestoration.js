3018cdaaec3895617d0884272d7edeb79c8fd81e
Mikkel Laursen <mlaursen03@gmail.com>
2017-06-23 21:46:19 -0600

    Updated scrollRestoration to also export a scrollTo function

--- documentation/src/utils/scrollRestoration.js
+++ documentation/src/utils/scrollRestoration.js
@@ -1,10 +1,28 @@
+import smoothScroll from 'smooth-scroll';
+
+/**
+ * A utility function to scroll to a single element. This will use the smooth-scroll
+ * animation when scrolling.
+ */
+export function scrollTo(el) {
+  if (!el) {
+    window.scrollTo(0, 0);
+  } else {
+    const { top: elTop } = el.getBoundingClientRect();
+    const { top: bodyTop } = (document.body || document.documentElement).getBoundingClientRect();
+    const position = Math.abs(elTop - bodyTop);
+    smoothScroll.animateScroll(position);
+  }
+}
+
+/**
+ * This is used each time the browser's location changes. It will attempt to either scroll to the top
+ * of the page, or the current hash.
+ */
 export default function scrollRestoration() {
   if (!__CLIENT__) {
     return;
   }
-
-  /* eslint-disable global-require */
-  const animateScroll = require('smooth-scroll').animateScroll;
 
   const { hash } = window.location;
   let el = null;
@@ -12,12 +30,5 @@ export default function scrollRestoration() {
     el = document.querySelector(hash);
   }
 
-  if (!el) {
-    window.scrollTo(0, 0);
-  } else {
-    const { top: elTop } = el.getBoundingClientRect();
-    const { top: bodyTop } = (document.body || document.documentElement).getBoundingClientRect();
-    const position = Math.abs(elTop - bodyTop);
-    animateScroll(position);
-  }
+  scrollTo(el);
 }
