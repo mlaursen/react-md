5fe1f2d6927da7bcdb2f96879016bf6c2d0b2409
Mikkel Laursen <mlaursen03@gmail.com>
2017-06-23 22:56:24 -0600

    Added a proxy endpoint for github requests

--- documentation/src/server/api/index.js
+++ documentation/src/server/api/index.js
@@ -5,6 +5,7 @@ import { middleware as cache } from 'apicache';
 import docgens from './docgens';
 import sassdocs from './sassdocs';
 import search from './search';
+import github from './github';
 
 const CACHE_DURATION = '10 days';
 const apiRouter = express.Router();
@@ -16,5 +17,6 @@ apiRouter.use(bodyParser.json());
 apiRouter.use('/docgens', docgens);
 apiRouter.use('/sassdocs', sassdocs);
 apiRouter.use('/search', search);
+apiRouter.use('/github', github);
 
 export default apiRouter;
--- documentation/src/utils/api.js
+++ documentation/src/utils/api.js
@@ -6,7 +6,6 @@ import {
   DOCGENS_ENDPOINT,
   SASSDOCS_ENDPOINT,
   GITHUB_API_ENDPOINT,
-  // VERSION,
 } from 'constants/application';
 
 export default function fetch(endpoint) {
@@ -21,39 +20,76 @@ export default function fetch(endpoint) {
   });
 }
 
+/**
+ * This is a simple wrapper to search for prop types, SassDoc, or components
+ * on the documentation website.
+ *
+ * @param {Object} searchParams - An object for the search params.
+ * @param {String=} searchParams.href - An optional href to search for. This is usually
+ *    set when querying the next result list.
+ * @param {String=} searchParams.query - The query string to be searched for.
+ * @param {number=} searchParams.start - The optional start index to search from.
+ * @param {String=''} server - the server to query from.
+ * @return {Promise} a promise to the fetch api call.
+ */
 export function search({ href, query, start }, server = '') {
-  const endpoint = href || `${server}${API_ENDPOINT}${SEARCH_ENDPOINT}?q=${query}&start=${start}`;
+  const endpoint = href || `${server}${API_ENDPOINT}${SEARCH_ENDPOINT}?q=${encodeURIComponent(query)}&start=${start}`;
   return fetch(endpoint);
 }
 
+/**
+ * Gets docgen from a specific endpoint.
+ *
+ * @param {String} endpoint - The endpoint to query for.
+ * @param {String=''} server - An optional server to query from.
+ * @return {Promise} a promise to the fetch api call.
+ */
 export function fetchDocgen(endpoint, server = '') {
   return fetch(`${server}${API_ENDPOINT}${DOCGENS_ENDPOINT}/${endpoint}`);
 }
 
+/**
+ * Gets SassDoc from a specific endpoint.
+ *
+ * @param {String} endpoint - The endpoint to query for.
+ * @param {String=''} server - An optional server to query from.
+ * @return {Promise} a promise to the fetch api call.
+ */
 export function fetchSassdoc(endpoint, server = '') {
   return fetch(`${server}${API_ENDPOINT}${SASSDOCS_ENDPOINT}/${endpoint}`);
 }
 
+/**
+ * A utility function to fetch data from github. When triggered from the browser,
+ * it will point to the local server for proxying to github. When triggered from
+ * the server, it will directly call the github api.
+ *
+ * The main reason for this is that GitHub likes to have custom User-Agent specified
+ * for each app, and modifying the User-Agent is not fully supported in browser yet
+ * since it was recently changed to not be read-only.
+ *
+ * @param {String} endpoint - The github api endpoint to make a request to.
+ * @param {Object={}} options - Any additional options to provide to the request.
+ * @return {Promise} a promise to the fetch api call.
+ */
 export function fetchGithub(endpoint, options = {}) {
-  const headers = new Headers({
-    ...options.headers,
-    Accept: 'application/vnd.github.v3+json',
-    // 'User-Agent': `react-md-documentation/${VERSION} mlaursen`,
-  });
-  return global.fetch(`${GITHUB_API_ENDPOINT}${endpoint}`, {
-    ...options,
-    headers,
-  });
+  const prefix = __CLIENT__ ? '/api/github' : GITHUB_API_ENDPOINT;
+  return global.fetch(`${prefix}${endpoint}`, options);
 }
 
+/**
+ * A utility function for getting the current rate limits after a github api call.
+ * This _technically_ isn't an api call itself so maybe it shouldn't be in this file,
+ * but I don't feel like finding a better place to put it.
+ *
+ * @param {Object} response - The response from a fetch request.
+ * @param {Map} response.headers - The headers to extract the rate limits from.
+ * @return {Object} an object containing the limit for a github endpoint, the remaining
+ *    number of calls for the endpoint, and a time for when the rate limits are reset.
+ */
 export function getGithubRateLimits({ headers }) {
   const limit = headers.get('X-RateLimit-Limit');
   const remaining = headers.get('X-RateLimit-Remaining');
   const reset = new Date(headers.get('X-RateLimit-Reset') * 1000);
   return { limit, remaining, reset };
-}
-
-export async function fetchGithubRateLimits() {
-  const response = await fetchGithub('/users/mlaursen');
-  return getGithubRateLimits(response);
 }
