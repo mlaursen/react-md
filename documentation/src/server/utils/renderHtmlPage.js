da18cc851caf5939ce89bcc754920e20b6460155
Mikkel Laursen <mlaursen03@gmail.com>
2017-07-07 09:27:17 -0600

    Added some additional meta tags

--- documentation/src/server/utils/renderHtmlPage.js
+++ documentation/src/server/utils/renderHtmlPage.js
@@ -1,5 +1,6 @@
 import Helmet from 'react-helmet';
 import serialize from 'serialize-javascript';
+import { DEFAULT_DESCRIPTION } from 'state/helmet/meta';
 
 let manifest;
 try {
@@ -19,15 +20,53 @@ window.webpackManifest = ${JSON.stringify(manifestJSON)}
   manifest = '';
 }
 
+const META = [{
+  name: 'viewport',
+  content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+}, {
+  'http-equiv': 'X-UA-Compatible',
+  content: 'IE=edge',
+}, {
+  property: 'og:title',
+  content: 'react-md - Accessible React Material Design Components',
+}, {
+  property: 'og:url',
+  content: 'https://react-md.mlaursen.com',
+}, {
+  property: 'og:type',
+  content: 'website',
+}, {
+  property: 'og:description',
+  content: DEFAULT_DESCRIPTION,
+}, {
+  property: 'og:image',
+  content: '/react-md.png',
+}, {
+  name: 'twitter:site',
+  content: 'react-md',
+}, {
+  name: 'twitter:creator',
+  content: 'Mikkel Laursen',
+}, {
+  name: 'twitter:title',
+  content: 'react-md - Accessible React Material Design Components',
+}, {
+  name: 'twitter.image',
+  content: '/react-md.png',
+}].reduce((allMeta, meta) => `${allMeta}<meta${Object.keys(meta).reduce((s, key) => `${s} ${key}="${meta[key]}"`, '')}>`, '');
+
 export default function renderHtmlPage(store, bundles = [], html = '') {
   const head = Helmet.renderStatic();
   const assets = global.webpackIsomorphicTools.assets();
 
   let page = `<!DOCTYPE html><html ${head.htmlAttributes.toString()}>`;
-
-  page += '<head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">';
-  page += '<meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">';
-  page += head.base.toString() + head.title.toString() + head.meta.toString() + head.link.toString();
+  page += '<head>';
+  page += head.base.toString();
+  page += head.title.toString();
+  page += head.meta.toString();
+  page += META;
+  page += head.link.toString();
+  page += '<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">';
 
   const styleKeys = Object.keys(assets.styles);
   if (__DEV__ && !styleKeys.length) {
