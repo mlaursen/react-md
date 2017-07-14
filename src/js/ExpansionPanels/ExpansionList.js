db6923e35c5ada43c2610d4ddcc8e682ffdc1ad0
Mikkel Laursen <mlaursen03@gmail.com>
2017-07-14 15:12:09 -0600

    Updated ExpansionPanel & Tabs with ResizeObserver
    Fixed the ExpansionPanel and Tabs components to update column widths
    when styles or loaded or the page resizes.
    
    This fix also allows for better mobile support by truncating columns
    that are too big.
    
    Closes #448

--- src/js/ExpansionPanels/ExpansionList.js
+++ src/js/ExpansionPanels/ExpansionList.js
@@ -1,10 +1,11 @@
-import { PureComponent, createElement, cloneElement, Children } from 'react';
+import React, { PureComponent, cloneElement, Children } from 'react';
 import PropTypes from 'prop-types';
 import { findDOMNode } from 'react-dom';
 import cn from 'classnames';
 
 import { TAB } from '../constants/keyCodes';
 import handleWindowClickListeners from '../utils/EventUtils/handleWindowClickListeners';
+import ResizeObserver from '../Helpers/ResizeObserver';
 
 /**
  * The `ExpansionList` component is a wrapper for the `ExpansionPanel` that helps
@@ -42,16 +43,19 @@ export default class ExpansionList extends PureComponent {
       PropTypes.string,
       PropTypes.func,
     ]).isRequired,
+
+    recalculateThreshold: PropTypes.number.isRequired,
   };
 
   static defaultProps = {
     component: 'ul',
+    recalculateThreshold: 80,
   };
 
   constructor(props) {
     super(props);
 
-    this.state = { columnWidths: [], focusedIndex: -1 };
+    this.state = { columnWidths: [], focusedIndex: -1, overflow: false };
 
     this._setContainer = this._setContainer.bind(this);
     this._removeFocus = this._removeFocus.bind(this);
@@ -87,6 +91,7 @@ export default class ExpansionList extends PureComponent {
       this._container = findDOMNode(container);
       window.addEventListener('keyup', this._determineTabFocus);
 
+      this._width = this._container.offsetWidth;
       this._calcColumnWidths();
     }
   }
@@ -102,48 +107,104 @@ export default class ExpansionList extends PureComponent {
     this.setState({ focusedIndex: -1 });
   }
 
+  _isOverflown = (widths) => {
+    if (!this._container) {
+      return 0;
+    }
+
+    const panel = this._container.querySelector('.md-panel-header');
+    if (!panel) {
+      return 0;
+    }
+
+    const collapser = this._container.querySelector('.md-expansion-panel__collapser');
+    const collapserWidth = collapser ? collapser.offsetWidth : 0;
+    const styles = window.getComputedStyle(panel);
+    const maxWidth = panel.offsetWidth
+      - parseFloat(styles.paddingLeft)
+      - parseFloat(styles.paddingRight)
+      - collapserWidth;
+
+    const totalWidth = widths.reduce((total, w) => total + w, 0);
+    return totalWidth > maxWidth;
+  };
+
   /**
    * Since this should really be rendered as a table, need to calculate the max width for each _column_
    * on the panel's header and apply that as a min width for the other panels.
    */
-  _calcColumnWidths() {
+  _getColumnWidths = () => {
     if (!this._container) {
-      return;
+      return this.state.columnWidths;
     }
 
-    const columnWidths = Array.prototype.slice.call(this._container.querySelectorAll('.md-panel-header'))
+    return [].slice.call(this._container.querySelectorAll('.md-panel-header'))
       .reduce((maxes, row) => {
         const columns = row.querySelectorAll('.md-panel-column');
         for (let i = 0; i < columns.length; i++) {
+          const col = columns[i];
+          // Need to reset the widths if it has already been calculated to get a more accurate measurement.
+          const { width, minWidth } = col.style;
+          col.style.width = 'auto';
+          col.style.minWidth = 'auto';
+
           // Only need to include the offsetWidth of the column because the child will really
           // determine the width of the column. Since it has already been defined at this point,
           // no additional work needs to be done.
-          maxes[i] = Math.max(
-            columns[i].offsetWidth,
-            maxes[i] || 0
-          );
+          maxes[i] = Math.max(col.offsetWidth, maxes[i] || 0);
+          col.style.width = width;
+          col.style.minWidth = minWidth;
         }
 
         return maxes;
       }, [0]);
+  };
 
-    this.setState({ columnWidths });
+  _calcColumnWidths = () => {
+    const { columnWidths } = this.state;
+    const nextWidths = this._getColumnWidths();
+    const overflown = this._isOverflown(nextWidths);
+    if (this.state.overflown !== overflown
+      || columnWidths.length !== nextWidths.length
+      || nextWidths.some((w, i) => w !== columnWidths[i])
+    ) {
+      this.setState({ columnWidths: nextWidths, overflown });
+    }
+  };
+
+  _handleResize = ({ width }) => {
+    const { recalculateThreshold } = this.props;
+    if (this._width !== width && (Math.abs(width - this._width) >= recalculateThreshold)) {
+      this._width = width;
+      this._calcColumnWidths();
+    }
   }
 
   render() {
-    const { columnWidths, focusedIndex } = this.state;
-    const { children, className, component, ...props } = this.props;
+    const { columnWidths, focusedIndex, overflown } = this.state;
+    const {
+      children,
+      className,
+      component: Component,
+      recalculateThreshold, // eslint-disable-line no-unused-vars
+      ...props
+    } = this.props;
 
-    return createElement(component, {
-      ...props,
-      ref: this._setContainer,
-      className: cn('md-expansion-panel-list', className),
-    }, Children.map(children, (child, i) => (
-      cloneElement(child, {
-        key: child.key || i,
-        columnWidths,
-        focused: focusedIndex === i,
-      })
-    )));
+    const panels = Children.map(children, (child, i) => cloneElement(child, {
+      key: child.key || i,
+      overflown,
+      columnWidths,
+      focused: focusedIndex === i,
+    }));
+    return (
+      <Component
+        {...props}
+        ref={this._setContainer}
+        className={cn('md-expansion-panel-list', className)}
+      >
+        <ResizeObserver watchWidth onResize={this._handleResize} />
+        {panels}
+      </Component>
+    );
   }
 }
