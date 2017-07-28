162b4105c12d34a18d04fc1580593cc4d9fea1ac
Mikkel Laursen <mlaursen03@gmail.com>
2017-07-28 09:46:23 -0600

    Added some dev defaults for the server

--- docs/src/server/index.js
+++ docs/src/server/index.js
@@ -99,7 +99,21 @@ app.get('*', cookieParser(), async (req, res) => {
   }
 });
 
-const port = process.env.NODE_PORT;
+let port = process.env.NODE_PORT;
+if (!port) {
+  const message = 'The `NODE_PORT` environment variable has not been set. This can be done by copying '
+    + 'the `.env.example` to `.env` in the docs root directory and updating the `NODE_PORT` or set an '
+    + 'environment variable on your machine.';
+
+  if (__DEV__) {
+    winston.info('Defaulting `NODE_PORT` to `8080` for dev mode.');
+    winston.info('It is recommended to copy the `.env.example` file to `.env` and set the `NODE_PORT` there.');
+    port = 8080;
+  } else {
+    throw new Error(message);
+  }
+}
+
 app.listen(port, (err) => {
   if (err) {
     throw err;
