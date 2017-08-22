6e5d0665734607a261e5ca9eb20d4a5bbc17428b
Mikkel Laursen <mlaursen03@gmail.com>
2017-08-22 15:47:40 +0200

    Finished the menu examples for tables

--- docs/src/components/Components/DataTables/WithMenus/code.js
+++ docs/src/components/Components/DataTables/WithMenus/code.js
@@ -1,1 +1,23 @@
-export default '';
+import WithMenus from '!!raw-loader!./index.jsx';
+import KebabMenu from '!!raw-loader!./KebabMenu';
+import SomeCustomMenu from '!!raw-loader!./SomeCustomMenu';
+import styles from '!!raw-loader!./_styles.scss';
+
+export default `/* WithMenus.jsx */
+${WithMenus}
+\`\`\`
+
+\`\`\`jsx
+/* KebabMenu.jsx */
+${KebabMenu}
+\`\`\`
+
+\`\`\`jsx
+/* SomeCustomMenu.jsx */
+${SomeCustomMenu}
+\`\`\`
+
+\`\`\`scss
+/* _styles.scss */
+${styles}
+`;
--- docs/src/components/Components/DataTables/WithMenus/index.jsx
+++ docs/src/components/Components/DataTables/WithMenus/index.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
+import Avatar from 'react-md/lib/Avatars';
 import DataTable from 'react-md/lib/DataTables/DataTable';
 import TableHeader from 'react-md/lib/DataTables/TableHeader';
 import TableRow from 'react-md/lib/DataTables/TableRow';
 import TableBody from 'react-md/lib/DataTables/TableBody';
 import TableColumn from 'react-md/lib/DataTables/TableColumn';
 
+import books from 'constants/sampleData/books';
+
 import './_styles.scss';
 import KebabMenu from './KebabMenu';
+import SomeCustomMenu from './SomeCustomMenu';
+
+const { suffixes } = Avatar.defaultProps;
 
 const WithMenus = () => (
   <DataTable baseId="menu-table">
     <TableHeader>
       <TableRow>
-        <TableColumn grow>Column 1</TableColumn>
-        <TableColumn>Column 2</TableColumn>
-        <TableColumn>Column 3</TableColumn>
-        <TableColumn>Column 4</TableColumn>
+        <TableColumn grow>Title</TableColumn>
+        <TableColumn>Author</TableColumn>
+        <TableColumn />
         <TableColumn />
       </TableRow>
     </TableHeader>
     <TableBody>
-      <TableRow>
-        <TableColumn>Column 1</TableColumn>
-        <TableColumn>Column 2</TableColumn>
-        <TableColumn>Column 3</TableColumn>
-        <TableColumn>Column 4</TableColumn>
-        <KebabMenu />
-      </TableRow>
+      {books.map(({ title, author }, i) => (
+        <TableRow key={`${title}-${author}`.replace(/\s+/g, '-')}>
+          <TableColumn>{title}</TableColumn>
+          <TableColumn>{author}</TableColumn>
+          <SomeCustomMenu id={i} suffix={suffixes[i]} letter={title.charAt(0)} />
+          <KebabMenu />
+        </TableRow>
+      ))}
     </TableBody>
   </DataTable>
 );
