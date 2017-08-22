e18307d36cf7decdc06e70bb32093908345967db
Mikkel Laursen <mlaursen03@gmail.com>
2017-08-22 20:49:08 +0200

    Fixed SelectionControl Deprecation
    Stopped using the uncheckedCheckboxIconChildren and
    checkedCheckboxIconChildren props for the GoogleDocsClone example.

--- docs/src/components/Components/Menus/GoogleDocsClone/StarDocument.jsx
+++ docs/src/components/Components/Menus/GoogleDocsClone/StarDocument.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import injectTooltip from 'react-md/lib/Tooltips/injectTooltip';
 import SelectionControl from 'react-md/lib/SelectionControls/SelectionControl';
+import FontIcon from 'react-md/lib/FontIcons';
 
 const Checkbox = injectTooltip(SelectionControl);
 
@@ -14,8 +15,8 @@ const StarDocument = () => (
     tooltipDelay={300}
     aria-label="Star the document"
     className="md-btn--toolbar"
-    checkedCheckboxIconChildren="star"
-    uncheckedCheckboxIconChildren="star_border"
+    checkedCheckboxIcon={<FontIcon>star</FontIcon>}
+    uncheckedCheckboxIcon={<FontIcon>star_border</FontIcon>}
   />
 );
 export default StarDocument;
