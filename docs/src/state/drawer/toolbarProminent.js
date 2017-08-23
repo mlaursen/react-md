aa2db5c0baa20faea02410d55f9b13731a1988bc
Mikkel Laursen <mlaursen03@gmail.com>
2017-08-23 08:37:31 +0200

    Async Loading Updates
    Updated the documentation site so that there is a spinner whenever an
    async route is loading now. It ended up being a bit slower than
    expected when these were being loaded so the spinner helps show
    something is happening.
    
    Also moved the drawer state parts into separate files and added
    accessibility for the loading content.

---
+++ docs/src/state/drawer/toolbarProminent.js
@@ -0,0 +1,16 @@
+import { LOCATION_CHANGE, NOT_FOUND } from 'state/routing';
+
+function isProminent({ pathname }) {
+  return !pathname.match(/minimizing/) && !!pathname.match(/components|customization/);
+}
+
+export default function toolbarProminent(state = false, action) {
+  switch (action.type) {
+    case LOCATION_CHANGE:
+      return isProminent(action.payload.location);
+    case NOT_FOUND:
+      return false;
+    default:
+      return state;
+  }
+}
