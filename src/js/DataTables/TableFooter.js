fa5e93a9427ac54c627cbebb9a0d1235c92029fd
Mikkel Laursen <mlaursen03@gmail.com>
2017-09-07 19:16:13 -0600

    Added and documented TableFooter
    The TableFooter is now correctly exported for UMD and documented the
    props.

--- src/js/DataTables/TableFooter.js
+++ src/js/DataTables/TableFooter.js
@@ -2,9 +2,25 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
+/**
+ * This is just a simple <tfoot> component.
+ */
 export default class TableFooter extends PureComponent {
   static propTypes = {
+    /**
+     * An optional style to apply.
+     */
+    style: PropTypes.object,
+
+    /**
+     * An optional className to apply.
+     */
     className: PropTypes.string,
+
+    /**
+     * The children to display. This should really be one or a list of `TableRow`
+     * components.
+     */
     children: PropTypes.node,
   };
 
