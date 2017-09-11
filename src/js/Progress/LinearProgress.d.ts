b731f5d06290d564095cecacb4d23ba5438e1744
Alexander Olsson <noseglid@users.noreply.github.com>
2017-09-11 11:42:10 +0200

    Correct type

--- src/js/Progress/LinearProgress.d.ts
+++ src/js/Progress/LinearProgress.d.ts
@@ -1,7 +1,7 @@
 import { BaseProgressProps } from './index';
 
 export interface LinearProgressProps extends BaseProgressProps {
-  query?: bool;
+  query?: boolean;
 }
 
 declare const LinearProgress: React.ComponentClass<LinearProgressProps>;
