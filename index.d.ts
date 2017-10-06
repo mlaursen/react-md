a47ef8f8f5ddd02803e7966cdd9d4f279ba01284
Mikkel Laursen <mlaursen03@gmail.com>
2017-10-05 19:40:44 -0600

    Added bem Typescript definition

--- index.d.ts
+++ index.d.ts
@@ -99,3 +99,4 @@ export {
 export { default as TextField, TextFieldProps, TextFieldTypes, TextFieldLineDirections } from './lib/TextFields';
 export { default as Toolbar, ToolbarProps } from './lib/Toolbars';
 export { default as injectTooltip, InjectedTooltipProps, TooltippedComponent, TooltippedProps } from './lib/Tooltips';
+export { default as bem } from './lib/utils/bem';
--- src/js/utils/bem.js
+++ src/js/utils/bem.js
@@ -4,12 +4,25 @@ import cn from 'classnames';
 /**
  * A utility function to apply BEM class names to an element.
  *
- * @param {String...} blocks - 1 to many blocks to use. These names will be joined
+ * ### Examples:
+ * ```js
+ * bem('class') === 'class'
+ * bem('block', 'element') === 'block__element'
+ * bem('block', 'element', 'sub-element') === 'block__element__sub-element'
+ * bem('block', { 'mod-1': true, 'mod-2': false }) === 'block block--mod-1'
+ * bem('block', 'element', { 'mod-1': false, 'mod-2': true }) === 'block__element block__element--mod-2'
+ * bem('block', 'element', {
+ *   'mod-1': false,
+ *   'mod-2': true,
+ * }, 'other', 'class-names') === 'block__element block__element--mod-2 other class-names'
+ * ```
+ *
+ * @param {...String} blocks - 1 to many blocks to use. These names will be joined
  *    with underscores.
  * @param {Object=} modifiers - Any conditional modifiers to apply to the blocks. Each
  *    key in this object will be applied as a `--suffix` to the blocks ONLY when
  *    their value is true-ish.
- * @param {String...} others - Any additional class names to apply.
+ * @param {...String} others - Any additional class names to apply.
  * @return {String} the bem-formatted className string.
  */
 export default function bem(...args) {
