f5bd164361e390ca13ab4678c189e190d44f2643
Mikkel Laursen <mlaursen03@gmail.com>
2017-10-06 07:43:15 -0600

    Updated tooltip examples for react 16
    THis should close #563

---
+++ docs/src/components/Components/Tooltips/CustomExamples/code.js
@@ -0,0 +1,17 @@
+import CustomExamples from '!!raw-loader!./index.jsx';
+import TooltipFontIcon from '!!raw-loader!./TooltipFontIcon.jsx';
+import TooltipLink from '!!raw-loader!./TooltipLink.jsx';
+
+export default `/* CustomExamples.jsx */
+${CustomExamples}
+\`\`\`
+
+\`\`\`jsx
+/* TooltipFontIcon.jsx */
+${TooltipFontIcon}
+\`\`\`
+
+\`\`\`jsx
+/* TooltipLink.jsx */
+${TooltipLink}
+`;
