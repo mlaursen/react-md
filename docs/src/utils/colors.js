6cfb71cf2007feaa626d4102250fa4fd5c38e367
Mikkel Laursen <mlaursen03@gmail.com>
2018-01-28 13:49:41 -0700

    Fixed the hexToRgb function

--- docs/src/utils/colors.js
+++ docs/src/utils/colors.js
@@ -4,10 +4,13 @@ export function hexToRgb(hexString) {
   hexString = hexString.replace(/^#?([A-Fa-f\d])([A-Fa-f\d])([A-Fa-f\d])$/, '#$1$1$2$2$3$3');
 
   const matches = hexString.match(/^#?([A-Fa-f\d]{2})([A-Fa-f\d]{2})([A-Fa-f\d]{2})/);
-  if (!matches || matches.length <= 5) {
+  if (!matches || matches.length <= 3) {
     return '0,0,0';
   }
 
-  const [, r, g, b] = matches;
+  let [, r, g, b] = matches;
+  r = parseInt(r, 16);
+  g = parseInt(g, 16);
+  b = parseInt(b, 16);
   return `${r},${g},${b}`;
 }
