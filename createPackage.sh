aa659873a2fa7463eeb8a6ff9b61d0aaff1b9fcc
Mikkel Laursen <mlaursen03@gmail.com>
2018-05-17 19:22:06 -0600

    Updated createPackage for additional styling info

--- createPackage.sh
+++ createPackage.sh
@@ -79,6 +79,44 @@ END
 README_STYLE_TEMPLATE=""
 if [[ $INCLUDE_STYLES = true ]]; then
   README_STYLE_TEMPLATE=$(cat <<-END
+#### Updating Sass to include `node_modules`
+If you want to include the SCSS styles for `@react-md/tooltip`, you will need to update your Sass compiler to include the `node_modules` in the paths as well as add [autoprefixer](https://github.com/postcss/autoprefixer) to handle multiple browser compatibility.
+
+> If you are using [create-react-app](https://github.com/facebook/create-react-app), the autoprefixer is already included.
+
+#### webpack
+```diff
+ {
+   test: /\.scss$/,
+   use: [{
+     loader: 'style-loader',
+     options: { sourceMap: true },
+   }, {
+     loader: 'css-loader',
+     options: { sourceMap: true, importLoaders: 2 },
+   }, {
+     loader: 'postcss',
+     options: { sourceMap: true },
+   }, {
+     loader: 'sass-loader',
+     options: {
+       sourceMap: true,
++      includePaths: [
++        './node_modules', // or whatever relative path it is to node_modules
++      ],
+     },
+   }],
+ }
+```
+
+#### create-react-app and node-sass-chokidar
+```diff
+   "scripts": {
++    "build-css": "node-sass-chokidar --include-path ./node_modules src/ -o src/",
++     "watch-css": "npm run build-csss && npm run build-css -- --watch --recursive"
+   }
+```
+
 ### Styles
 Including all the base styles can be done by either importing the styles file from the \`dist\` folder or importing the helpers file and using the mixin \`react-md-$PACKAGE_NAME\`:
 
@@ -111,6 +149,8 @@ fi
 README_TEMPLATE=$(cat <<-END
 # @react-md/$PACKAGE_NAME
 $DESCRIPTION
+
+This source code of this package can be found at: https://github.com/mlaursen/react-md/tree/release/2.0.x/packages/$PACKAGE_NAME
 
 ## Installation
 \`\`\`sh
