c9ed813c13c6a6527dde3872b757b74fc52553e1
Mikkel Laursen <mlaursen03@gmail.com>
2018-06-15 16:01:17 -0600

    Updated App Footer to remove a contact me link

--- docs/src/components/App/Footer/index.jsx
+++ docs/src/components/App/Footer/index.jsx
@@ -6,14 +6,12 @@ import { Grid } from 'react-md';
 import './_styles.scss';
 
 import QuickNav from './QuickNav';
-import Contact from './Contact';
 import Contribute from './Contribute';
 import Version from './Version';
 
 const Footer = ({ className, ...props }) => (
   <Grid {...props} id="main-footer" component="footer" className={cn('footer', className)}>
     <QuickNav />
-    <Contact />
     <Contribute />
     <Version />
   </Grid>
