b33b43616d287bd79a4526b1628ce539159e8001
Mikkel Laursen <mlaursen03@gmail.com>
2018-07-12 16:00:31 +0200

    Added some helpful event utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,2 +1,8 @@
 export { default as positionRelativeTo } from "./positionRelativeTo";
 export * from "./positionRelativeTo";
+
+export { default as throttleEvent } from "./throttleEvent";
+export * from "./throttleEvent";
+
+export { default as isPassiveEventsSupported } from "./isPassiveEventsSupported";
+export * from "./touches";
---
+++ packages/utils/src/isPassiveEventsSupported.ts
@@ -0,0 +1,24 @@
+const noop = () => undefined;
+let cached = false;
+
+/**
+ * Checks if the browser supports passive events. This shouldn't really be used
+ * outside of this file, but you can always check again if needed.
+ */
+export function supports() {
+  let isPassiveEventsSupported = false;
+  const opts = Object.defineProperty({}, "passive", {
+    get() {
+      isPassiveEventsSupported = true;
+    },
+  });
+
+  window.addEventListener("testSupportsPassive", noop, opts);
+  window.removeEventListener("testSupportsPassive", noop, opts);
+  cached = isPassiveEventsSupported;
+  return isPassiveEventsSupported;
+}
+
+supports();
+
+export default cached;
