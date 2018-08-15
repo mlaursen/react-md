c82b5cd757c45afad35df837ae518f033dbcd7db
Mikkel Laursen <mlaursen03@gmail.com>
2018-08-15 09:41:52 -0600

    Updated Transition to export react-transition-group types

---
+++ packages/transition/src/types.d.ts
@@ -0,0 +1,99 @@
+// these were copied from @types/react-transition-group and renamed to be prefixed with Transition
+// since they currently aren't exported
+export type TransitionEnterHandler = (node: HTMLElement, isAppearing: boolean) => void;
+export type TransitionExitHandler = (node: HTMLElement) => void;
+
+export type TransitionTimeout = number | { enter?: number; exit?: number };
+
+// this was copied over just for simplicity and not requiring adding react-transition-group for this
+export interface ICSSTransitionClassNames {
+  appear?: string;
+  appearActive?: string;
+  enter?: string;
+  enterActive?: string;
+  enterDone?: string;
+  exit?: string;
+  exitActive?: string;
+  exitDone?: string;
+}
+
+export type CSSTransitionClassNames = string | ICSSTransitionClassNames;
+
+export interface ITransitionProps {
+  /**
+   * The transition duration for the overlay. This should not be changed unless you manually change the
+   * `$rmd-overlay-transition-duration` scss variable.
+   *
+   * @docgen
+   */
+  timeout?: TransitionTimeout;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group. By default, the overlay will
+   * not be rendered in the DOM until the `visible` prop is `true` but this can be changed by setting this
+   * prop to `false`.
+   *
+   * @docgen
+   */
+  mountOnEnter?: boolean;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group. By default, the overlay will
+   * be removed from the DOM when the `visible` prop is `false` but this can be changed by setting this
+   * prop to `false`.
+   *
+   * @docgen
+   */
+  unmountOnExit?: boolean;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group.
+   *
+   * @docgen
+   */
+  onEnter?: TransitionEnterHandler;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group.
+   *
+   * @docgen
+   */
+  onEntering?: TransitionEnterHandler;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group.
+   *
+   * @docgen
+   */
+  onEntered?: TransitionEnterHandler;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group.
+   *
+   * @docgen
+   */
+  onExit?: TransitionExitHandler;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group.
+   *
+   * @docgen
+   */
+  onExiting?: TransitionExitHandler;
+
+  /**
+   * Pass-down prop to the `Transition` component from react-transition-group.
+   *
+   * @docgen
+   */
+  onExited?: TransitionExitHandler;
+}
+
+export interface ICSSTransitionProps extends ITransitionProps {
+  /**
+   * The class names to use during the different parts of the animation.
+   *
+   * @docgen
+   */
+  classNames?: CSSTransitionClassNames;
+}
