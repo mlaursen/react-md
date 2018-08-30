751adc4ca425ff56987cf75623e1691f7cf29d94
Mikkel Laursen <mlaursen03@gmail.com>
2018-08-30 13:42:40 -0600

    Updated createPackage script to install dependencies

--- scripts/createPackage.js
+++ scripts/createPackage.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
+const execSync = require('child_process').execSync;
 
 const createPackageJson = require('./createPackageJson');
 const createReadme = require('./createReadme');
@@ -57,6 +58,36 @@ module.exports = function createPackage(config) {
     ...Object.keys(filesToCreate).map(fileName => createFile(fileName, filesToCreate[fileName])),
     ...Object.keys(filesToCopy).map(fileName => fs.copy(filesToCopy[fileName], fileName)),
   ]).then(() => {
+    const dependencies = [];
+    const devDependencies = ['cpx', 'doc-generator', 'rimraf'];
+    if (styles) {
+      devDependencies.push('sass-lint');
+    }
+
+    if (typescript) {
+      devDependencies.push('jest', 'ts-jest', 'tslint', 'typescript');
+    }
+
+    if (typescript && styles) {
+      dependencies.push('classnames');
+      devDependencies.push('npm-run-all');
+    } else if (customTypes) {
+      devDependencies.push('npm-run-all');
+    }
+
+    if (dependencies.length) {
+      console.log('Installing dependencies...');
+      console.log(` - ${dependencies.join('\n - ')}`);
+      execSync(`lerna exec --scope @react-md/${name} -- npm i -S ${dependencies.join(' ')}`);
+    }
+
+    console.log('Installing devDependencies...');
+    console.log(` - ${devDependencies.join('\n - ')}`);
+    execSync(`lerna exec --scope @react-md/${name} -- npm i -D ${devDependencies.join(' ')}`);
+
+    console.log('Running bootstrap for safe measures...');
+    execSync('lerna bootstrap');
+
     console.log('Done!');
   });
 };
