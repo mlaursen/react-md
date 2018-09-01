74964919b2b9d240ea8094e6277d20b6d432cffa
Mikkel Laursen <mlaursen03@gmail.com>
2018-09-01 13:52:53 -0600

    Updated typography with examples

--- packages/documentation/src/components/packages/Typography/Examples/Examples.tsx
+++ packages/documentation/src/components/packages/Typography/Examples/Examples.tsx
@@ -2,7 +2,15 @@ import * as React from "react";
 
 import { default as ExamplesPage, ExampleList } from "components/ExamplesPage";
 
-const examples: ExampleList = [];
+import AllTextTypes from "./AllTextTypes";
+
+const examples: ExampleList = [
+  {
+    title: "All Text Types",
+    children: <AllTextTypes />,
+    exampleGroup: false,
+  },
+];
 
 const Examples = () => <ExamplesPage title="Typography" examples={examples} />;
 
