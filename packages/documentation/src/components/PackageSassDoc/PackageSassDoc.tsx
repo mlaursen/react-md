ad11f03caa8c778179e3950d0fb095d8b9698abb
Mikkel Laursen <mlaursen03@gmail.com>
2018-09-08 20:13:20 -0600

    Added some more sassdoc stuff

--- packages/documentation/src/components/PackageSassDoc/PackageSassDoc.tsx
+++ packages/documentation/src/components/PackageSassDoc/PackageSassDoc.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
-import { Text } from "@react-md/typography";
+import { Button } from "@react-md/button";
+import { SearchSVGIcon } from "@react-md/material-icons";
+
 import { IFlattenedSassDoc } from "types/sassdoc";
 
 import "./package-sassdoc.scss";
 import SassDoc from "./SassDoc";
+import Search from "./Search";
 
 export interface IPackageSassDocProps {
   name: string;
@@ -12,15 +15,15 @@ export interface IPackageSassDocProps {
 
 export default class PackageSassDoc extends React.Component<IPackageSassDocProps> {
   public render() {
-    const {
-      name,
-      sassdoc: { variables, functions, mixins },
-    } = this.props;
+    const { name, sassdoc } = this.props;
+    const { variables, functions, mixins } = sassdoc;
+
     return (
       <div className="package-sassdoc">
         <SassDoc name="Variables" list={variables} />
         <SassDoc name="Functions" list={functions} />
         <SassDoc name="Mixins" list={mixins} />
+        <Search sassdoc={sassdoc} />
       </div>
     );
   }
