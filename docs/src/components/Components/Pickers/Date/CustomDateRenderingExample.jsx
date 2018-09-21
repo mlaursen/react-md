9ea50682ccbca8964a2b4e071093894b4b6b34af
Mikkel Laursen <mlaursen03@gmail.com>
2018-09-20 20:20:14 -0600

    Updated custom DatePicker renderer example for mobile

--- docs/src/components/Components/Pickers/Date/CustomDateRenderingExample.jsx
+++ docs/src/components/Components/Pickers/Date/CustomDateRenderingExample.jsx
@@ -1,13 +1,36 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+import { connect } from 'react-redux';
 import { DatePicker } from 'react-md';
+
 import './CustomDateRenderingExample.scss';
 
+@connect(state => ({ mobile: state.media.mobile }))
 export default class CustomDateRenderingExample extends PureComponent {
+  static propTypes = {
+    mobile: PropTypes.bool.isRequired,
+  };
 
-  componentWillMount() {
+  constructor(props) {
+    super(props);
+
+    this.state = { visible: !props.mobile };
     const today = new Date();
     const eventDay = today.getDate() > 15 ? today.getDate() - 2 : today.getDate() + 2;
     this.eventDate = new Date(today.getFullYear(), today.getMonth(), eventDay);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { mobile } = nextProps;
+    const { visible } = this.state;
+    // make sure the date picker is visible on desktop and tablets and hidden when switching to
+    // a mobile screen size
+    if (mobile === this.props.mobile || visible !== mobile) {
+      return;
+    }
+
+    this.setState({ visible: !mobile });
   }
 
   getDateClassName = (date) => {
@@ -32,20 +55,28 @@ export default class CustomDateRenderingExample extends PureComponent {
     return <span>{day}</span>;
   };
 
+  handleVisibilityChange = (visible) => {
+    this.setState({ visible });
+  };
+
   render() {
+    const { mobile } = this.props;
+    const { visible } = this.state;
+
     return (
       <DatePicker
-        className="inline-fixed-date-picker"
         id="custom-date-rendering-date-picker"
-        pickerFooterClassName="hidden-footer"
+        className="inline-fixed-date-picker"
+        pickerFooterClassName={cn({ 'hidden-footer': !mobile })}
+        textFieldStyle={!mobile ? { display: 'none' } : {}}
         displayMode="portrait"
         label="Select a date"
-        textFieldStyle={{ display: 'none' }}
-        defaultVisible
-        inline
+        visible={visible}
+        inline={!mobile}
         closeYearOnSelect
         getDateClassName={this.getDateClassName}
         dateRenderer={this.dateRenderer}
+        onVisibilityChange={this.handleVisibilityChange}
       />
     );
   }
