4d81cab1c23f519a315d2e04ea30172aac50c362
Mikkel Laursen <mlaursen03@gmail.com>
2019-02-04 17:51:54 -0700

    Added sassdoc to states theme variables

--- packages/states/src/scssVariables.ts
+++ packages/states/src/scssVariables.ts
@@ -8,4 +8,13 @@ export default {
   "rmd-states-focus-opacity": "0.24",
   "rmd-states-pressed-opacity": "0.32",
   "rmd-states-selected-opacity": "0.16",
+  "rmd-states-theme-values": {
+    "background-color": "#000",
+    "hover-opacity": "0.08",
+    "focus-opacity": "0.24",
+    "pressed-opacity": "0.32",
+    "selected-opacity": "0.16",
+    "ripple-background-color": "rgba(0, 0, 0, 0.08)",
+    "ripple-opacity": "1",
+  },
 };
