f9f0ac23660d43e912eab0e3dc2643168fcccf11
Mikkel Laursen <mlaursen03@gmail.com>
2019-02-05 19:43:24 -0700

    Added more changes for appbars

--- packages/examples/src/components/Header/ToggleTheme.tsx
+++ packages/examples/src/components/Header/ToggleTheme.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { FunctionComponent, useState, useEffect, useRef } from "react";
 import cn from "classnames";
 import { AppBarAction } from "@react-md/app-bar";
 import { LightbulbOutlineSVGIcon } from "@react-md/material-icons";
@@ -6,32 +6,18 @@ import { Tooltipped } from "@react-md/tooltip";
 
 import "./toggle-theme.scss";
 
-const THEMES = {
-  dark: {
-    "theme-background": "",
-    "theme-surface": "",
-    "theme-on-surface": "",
-    "theme-text-primary-on-background": "",
-    "theme-text-secondary-on-background": "",
-    "theme-text-hint-on-background": "",
-    "theme-text-disabled-on-background": "",
-    "theme-text-icon-on-background": "",
-    "divider-background-color": "",
-  },
-  light: {
-    "theme-background": "#fafafa",
-    "theme-surface": "#fff",
-    "theme-on-surface": "#000",
-    "theme-text-primary-on-background":
-      "var(--rmd-theme-text-primary-on-light)",
-    "theme-text-secondary-on-background":
-      "var(--rmd-theme-text-secondary-on-light)",
-    "theme-text-hint-on-background": "var(--rmd-theme-text-hint-on-light)",
-    "theme-text-disabled-on-background":
-      "var(--rmd-theme-text-disabled-on-light)",
-    "theme-text-icon-on-background": "var(--rmd-theme-text-icon-on-light)",
-    "divider-background-color": "var(--rmd-divider-background-color-on-light)",
-  },
+const LIGHT_THEMES = {
+  "theme-background": "#fafafa",
+  "theme-surface": "#fff",
+  "theme-on-surface": "#000",
+  "theme-text-primary-on-background": "var(--rmd-theme-text-primary-on-light)",
+  "theme-text-secondary-on-background":
+    "var(--rmd-theme-text-secondary-on-light)",
+  "theme-text-hint-on-background": "var(--rmd-theme-text-hint-on-light)",
+  "theme-text-disabled-on-background":
+    "var(--rmd-theme-text-disabled-on-light)",
+  "theme-text-icon-on-background": "var(--rmd-theme-text-icon-on-light)",
+  "divider-background-color": "var(--rmd-divider-background-color-on-light)",
 };
 
 const ToggleTheme: FunctionComponent = () => {
@@ -39,13 +25,36 @@ const ToggleTheme: FunctionComponent = () => {
     () => window.localStorage.getItem("isLight") === "true"
   );
 
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    const root = document.documentElement;
+    root.classList.add("toggle-theme-transition");
+
+    const timeout = window.setTimeout(() => {
+      root.classList.remove("toggle-theme-transition");
+    }, 150);
+    return () => {
+      window.clearTimeout(timeout);
+      root.classList.remove("toggle-theme-transition");
+    };
+  }, [isLight]);
+
   useEffect(() => {
     const root = document.documentElement;
     const key = isLight ? "light" : "dark";
 
-    Object.entries(THEMES[key]).forEach(([key, value]) => {
-      root.style.setProperty(`--rmd-${key}`, value);
+    Object.entries(LIGHT_THEMES).forEach(([key, value]) => {
+      // my app is pre-built for dark theme, so just reset all values when
+      // dark
+      root.style.setProperty(`--rmd-${key}`, isLight ? value : "");
     });
+
     window.localStorage.setItem("isLight", isLight.toString());
   }, [isLight]);
 
