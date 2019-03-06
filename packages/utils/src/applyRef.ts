ceba19eb0bbabf1dfb26f54105f26c6ea04544e5
Mikkel Laursen <mlaursen03@gmail.com>
2019-03-06 07:57:47 -0700

    Moved the applyRef util to @react-md/utils

--- packages/utils/src/applyRef.ts
+++ packages/utils/src/applyRef.ts
@@ -1,6 +1,17 @@
 import { Ref } from "react";
 
+/**
+ * A small utility function that allows me to apply a passed in
+ * ref along with my own custom ref logic.
+ *
+ * @param instance - The DOM Node instance
+ * @param ref - The prop ref
+ */
 export default function applyRef<E>(instance: E | null, ref?: Ref<E>) {
+  if (!ref) {
+    return;
+  }
+
   if (typeof ref === "function") {
     ref(instance);
   } else if (typeof ref === "object") {
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,5 +6,6 @@ export * from "./omit";
 export * from "./hooks";
 export * from "./positioning";
 export { default as unitToNumber } from "./unitToNumber";
+export { default as applyRef } from "./applyRef";
 
 export * from "./types.d";
