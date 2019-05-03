cf1291d389d314a6ee466c68871a2bb2524e48b0
Mikkel Laursen <mlaursen03@gmail.com>
2019-05-02 23:47:12 -0600

    new tooltip logic

---
+++ packages/tooltip/src_old/index.ts
@@ -0,0 +1,8 @@
+export { default as Tooltip } from "./Tooltip";
+export * from "./Tooltip";
+
+export { default as Tooltipped } from "./Tooltipped";
+export * from "./Tooltipped";
+
+export * from "./hooks";
+export * from "./types.d";
