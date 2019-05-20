8fba3b5b6cbff9df5408658a4a2eda4c6d1dbaf4
Mikkel Laursen <mlaursen03@gmail.com>
2019-05-19 18:50:16 -0600

    Updated scss variables

--- packages/card/src/scssVariables.ts
+++ packages/card/src/scssVariables.ts
@@ -1,9 +1,8 @@
 /** this is an auto-generated file from @react-md/dev-utils */
 export default {
   "rmd-card-background-color": "var(--rmd-theme-surface, #fff)",
-  "rmd-card-color": "var(--rmd-theme-text-primary-on-light, #212121)",
-  "rmd-card-secondary-color":
-    "var(--rmd-theme-text-secondary-on-light, #757575)",
+  "rmd-card-color": "var(--rmd-theme-on-surface, #000)",
+  "rmd-card-secondary-color": "var(--rmd-theme-text-primary-on-light, #212121)",
   "rmd-card-elevation": 2,
   "rmd-card-base-elevation": 1,
   "rmd-card-raised-elevation": 8,
@@ -16,7 +15,7 @@ export default {
   "rmd-card-actions-padding": "0.5rem",
   "rmd-card-theme-values": {
     "background-color": "var(--rmd-theme-surface, #fff)",
-    color: "var(--rmd-theme-text-primary-on-light, #212121)",
-    "secondary-color": "var(--rmd-theme-text-secondary-on-light, #757575)",
+    color: "var(--rmd-theme-on-surface, #000)",
+    "secondary-color": "var(--rmd-theme-text-primary-on-light, #212121)",
   },
 };
