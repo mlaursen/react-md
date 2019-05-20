642207f886bc6b6d7c50204497ee9ad3226aeec1
Mikkel Laursen <mlaursen03@gmail.com>
2019-05-20 16:20:39 -0600

    Removed old cards

--- old/src/js/Cards/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default, default as Card, CardProps } from './Card';
-export { default as CardActions, CardActionsProps } from './CardActions';
-export { default as CardText, CardTextProps } from './CardText';
-export { default as CardTitle, CardTitleProps } from './CardTitle';
--- old/src/js/Cards/index.js
+++
@@ -1,9 +0,0 @@
-import Card from './Card';
-export default Card;
-
-export Card from './Card';
-export CardTitle from './CardTitle';
-export CardMedia from './CardMedia';
-export CardActions from './CardActions';
-export CardText from './CardText';
-export CardActionOverlay from './CardActionOverlay';
