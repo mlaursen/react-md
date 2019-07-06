c51ebe76c91e2aa40c8ff20d1ddd05ad8efb7dba
Mikkel Laursen <mlaursen03@gmail.com>
2019-07-06 10:30:51 -0600

    Removed text field/textarea stuff for wip

--- packages/documentation/components/Demos/Form/TextAreaExamples.md
+++
@@ -1,2 +0,0 @@
-Textareas are mostly like text fields except they can not render icons inline
-with the textarea.
--- packages/documentation/components/Demos/Form/index.tsx
+++ packages/documentation/components/Demos/Form/index.tsx
@@ -12,15 +12,6 @@ import customCheckboxes from "./CustomCheckboxes.md";
 import SwitchExamples from "./SwitchExamples";
 import switchExamples from "./SwitchExamples.md";
 
-import TextFieldExamples from "./TextFieldExamples";
-import textFieldExamples from "./TextFieldExamples.md";
-
-import TextAreaExamples from "./TextAreaExamples";
-import textAreaExamples from "./TextAreaExamples.md";
-
-import SimpleValidation from "./SimpleValidation";
-import simpleValidation from "./SimpleValidation.md";
-
 const demos = [
   {
     name: "Checkbox and Radio Examples",
@@ -36,21 +27,6 @@ const demos = [
     name: "Switch Examples",
     description: switchExamples,
     children: <SwitchExamples />,
-  },
-  {
-    name: "Text Field Examples",
-    description: textFieldExamples,
-    children: <TextFieldExamples />,
-  },
-  {
-    name: "Text Area Examples",
-    description: textAreaExamples,
-    children: <TextAreaExamples />,
-  },
-  {
-    name: "Simple Validation",
-    description: simpleValidation,
-    children: <SimpleValidation />,
   },
 ];
 
