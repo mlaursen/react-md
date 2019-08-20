b88895493703259089204f23fde0b7bf98ee6600
Mikkel Laursen <mlaursen03@gmail.com>
2019-08-19 18:48:22 -0600

    Moved loop util to separate file

--- packages/utils/src/wia-aria/__tests__/useKeyboardMovement.ts
+++ packages/utils/src/wia-aria/__tests__/useKeyboardMovement.ts
@@ -1,35 +1,9 @@
 import { cleanup } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 
-import useKeyboardMovement, { loop } from "../useKeyboardMovement";
+import useKeyboardMovement from "../useKeyboardMovement";
 
 afterEach(cleanup);
-
-describe("incrementLoop", () => {
-  it("should increase the number by 1 when the number is less than or equal to the max number", () => {
-    expect(loop(0, 3, true)).toBe(1);
-    expect(loop(0, 1, true)).toBe(1);
-    expect(loop(1, 100, true)).toBe(2);
-  });
-
-  it("should decrease the number by 1 when the number is greater than 0", () => {
-    expect(loop(1, 3, false)).toBe(0);
-    expect(loop(1, 1, false)).toBe(0);
-    expect(loop(100, 100, false)).toBe(99);
-  });
-
-  it("should correctly loop around to 0 when the number is the max value and incrementing", () => {
-    expect(loop(100, 100, true)).toBe(0);
-    expect(loop(1, 1, true)).toBe(0);
-    expect(loop(20, 20, true)).toBe(0);
-  });
-
-  it("should correctly loop around to the max value when the number is 0 and decrementing", () => {
-    expect(loop(0, 100, false)).toBe(100);
-    expect(loop(0, 1, false)).toBe(1);
-    expect(loop(0, 20, false)).toBe(20);
-  });
-});
 
 describe("useKeyboardMovement", () => {
   it("should actually have tests at some point", () => {
--- packages/utils/src/wia-aria/useKeyboardMovement.ts
+++ packages/utils/src/wia-aria/useKeyboardMovement.ts
@@ -1,44 +1,16 @@
 import React, { useCallback } from "react";
 
+import loop from "../loop";
 import { Omit } from "../omit";
 import useRefCache from "../useRefCache";
 
-import { useMemoizedFocusKeys, getKeyboardEventType } from "./useFocusKeys";
 import getFocusableElements from "./getFocusableElements";
+import {
+  getKeyboardEventType,
+  KeyboardFocusKeys,
+  useMemoizedFocusKeys,
+} from "./useFocusKeys";
 import useKeyboardSearch, { KeyboardSearchOptions } from "./useKeyboardSearch";
-
-/**
- * A small util that is used to increment or decrement a number until
- * it reaches the max value or -1. When that happens, it will loop around
- * to 0 or the max value respectively. This does not work for different
- * increment numbers or any values below 0 for now.
- *
- * @param x The number to increment or decrement
- * @param max The max number that can be set
- * @param increment Boolean if it should be incremented or decremented
- * @private
- */
-export function loop(x: number, max: number, increment: boolean): number {
-  let next = x + (increment ? 1 : -1);
-  if (next > max) {
-    next = 0;
-  } else if (next < 0) {
-    next = max;
-  }
-
-  return next;
-}
-
-/**
- * This interface is used to show how keyboard focus can be achieved with different
- * key presses. When any of the values are omitted, an empty list will be used instead.
- */
-export interface KeyboardFocusKeys {
-  incrementKeys?: string[];
-  decrementKeys?: string[];
-  jumpToFirstKeys?: string[];
-  jumpToLastKeys?: string[];
-}
 
 export type KeyboardFocusKeyType =
   | "increment"
