d73cbb301549cc2a342cbfa08250bce3c57184c3
Mikkel Laursen <mlaursen03@gmail.com>
2020-02-03 17:11:02 -0700

    Added Hot Reloading to Guides
    The guides page will now "support" hot reloading in a pretty hacky way
    for dev mode only.
    
    Also found out that nextjs pages with the templated syntax (square
    brackets) causes prettier to fail so had to manually disable the
    pre-commit hook with `--no-verify` to commit this.

--- packages/documentation/src/pages/guides/[id].tsx
+++ packages/documentation/src/pages/guides/[id].tsx
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NextFC } from "next";
 
 import NotFoundPage from "components/NotFoundPage";
 import { MarkdownPage } from "components/Markdown";
+import { qsToString } from "utils/routes";
 
 interface GuidesProps {
+  guideId: string;
   readme: string | null;
 }
 
-const Guides: NextFC<GuidesProps> = ({ readme }) =>
-  readme === null ? <NotFoundPage /> : <MarkdownPage>{readme}</MarkdownPage>;
+const Guides: NextFC<GuidesProps> = ({ readme: propReadme, guideId }) => {
+  let readme = propReadme;
+  if (process.env.NODE_ENV !== "production") {
+    // This is a hacky way to allow hot reloading for the guide in dev mode
+    // since the getInitialProps isn't re-run for hot reloads...
+    /* eslint-disable react-hooks/rules-of-hooks */
+    const [devReadme, setDevReadme] = useState(readme);
+    readme = devReadme;
+
+    useEffect(() => {
+      let cancelled = false;
+      (async function load() {
+        const readme = await import(`../../guides/${guideId}.md`)
+          .then(mod => mod.default)
+          .catch(() => null);
+
+        if (!cancelled) {
+          setDevReadme(readme);
+        }
+      })();
+
+      return () => {
+        cancelled = true;
+      };
+    }, [guideId]);
+  }
+
+  if (readme === null) {
+    return <NotFoundPage />;
+  }
+
+  return <MarkdownPage>{readme}</MarkdownPage>;
+};
 
 Guides.getInitialProps = async ({ query }): Promise<GuidesProps> => {
-  const readme = await import(`../../guides/${query.id}.md`)
+  const guideId = qsToString(query.id);
+  const readme = await import(`../../guides/${guideId}.md`)
     .then(mod => mod.default)
     .catch(() => null);
 
-  return { readme };
+  return { guideId, readme };
 };
 
 export default Guides;
