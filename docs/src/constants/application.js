97b23feb51b5c86eff37ed5a6a83fa81b210951c
Mikkel Laursen <mlaursen03@gmail.com>
2020-04-05 12:54:30 -0600

    Another fix?...

--- docs/src/constants/application.js
+++ docs/src/constants/application.js
@@ -3,7 +3,7 @@ import { bugs } from '../../../package.json';
 export const { ROOT_PATH = '/' } = process.env;
 
 export const GITHUB_URL = bugs.url.replace('/issues', '');
-export const API_ENDPOINT = `${ROOT_PATH}api`;
+export const API_ENDPOINT = '/api';
 export const SEARCH_ENDPOINT = '/search';
 export const DOCGENS_ENDPOINT = '/docgens';
 export const SASSDOCS_ENDPOINT = '/sassdocs';
