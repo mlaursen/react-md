e19379eb4fb85a62449087d6d7bf805f50da3ead
Mikkel Laursen <mlaursen03@gmail.com>
2020-05-08 21:40:45 -0600

    fix(test): Fixed ts-jest after upgrading to 25.5
    I did bad and didn't check the changelog that apparently
    custom .d.ts files have to specifically added to the tsconfig
    to work.

--- jest.config.js
+++ jest.config.js
@@ -3,17 +3,14 @@ const path = require('path');
 
 const packages = fs
   .readdirSync(path.join(process.cwd(), 'packages'))
-  .filter(name => !['dev-utils'].includes(name));
+  .filter((name) => !['dev-utils'].includes(name));
 
 const docsSrc = '<rootDir>/packages/documentation/src';
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  testURL: 'http://localhost',
-  roots: packages.map(name => `<rootDir>/packages/${name}/src`),
-  // fixes not being able to see console.log in tests...
-  verbose: false,
+  roots: packages.map((name) => `<rootDir>/packages/${name}/src`),
   globals: {
     'ts-jest': {
       tsConfig: './tsconfig.test.json',
@@ -22,24 +19,22 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/testSetup/init.js'],
   moduleNameMapper: {
     '\\.scss$': '<rootDir>/testSetup/styleMock.js',
+    '\\.svg$': '<rootDir>/testSetup/inlineSvgTransformer.js',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/testSetup/fileTransformer.js',
     '^constants/(.*)$': `${docsSrc}/constants/$1`,
     '^components/(.*)$': `${docsSrc}/components/$1`,
     '^hooks/(.*)$': `${docsSrc}/hooks/$1`,
     '^icons/(.*)$': `${docsSrc}/icons/$1`,
     '^pages/(.*)$': `${docsSrc}/pages/$1`,
-    '^types/(.*)$': `${docsSrc}/types/$1`,
     '^utils/(.*)$': `${docsSrc}/utils/$1`,
-  },
-  transform: {
-    '\\.svg$': '<rootDir>/testSetup/inlineSvgTransformer.js',
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/testSetup/fileTransformer.js',
   },
   coveragePathIgnorePatterns: [
     '/node_modules/',
     '<rootDir>/packages/documentation/src',
     '<rootDir>/testSetup',
-    ...packages.map(name => `<rootDir>/packages/${name}/es`),
-    ...packages.map(name => `<rootDir>/packages/${name}/lib`),
+    ...packages.map((name) => `<rootDir>/packages/${name}/es`),
+    ...packages.map((name) => `<rootDir>/packages/${name}/lib`),
   ],
 };
+
