5fefeb87d50f7f9c45ca6683872b9c1d15114d82
Mikkel Laursen <mlaursen03@gmail.com>
2020-05-18 22:31:13 -0600

    chore(docs): Updated the menu positioning example

--- packages/documentation/src/components/Demos/Menu/MenuPositioning.tsx
+++ packages/documentation/src/components/Demos/Menu/MenuPositioning.tsx
@@ -1,58 +1,125 @@
-import React, { FC, Fragment } from "react";
+import React, { FC, useState } from "react";
+import { upperFirst } from "lodash";
+import { Checkbox, Select, useChecked } from "@react-md/form";
 import { ArrowDropDownSVGIcon } from "@react-md/material-icons";
 import { DropdownMenu } from "@react-md/menu";
-import { Text } from "@react-md/typography";
-import { HorizontalPosition, VerticalPosition } from "@react-md/utils";
-
-import Code from "components/Code/Code";
+import {
+  ABOVE_CENTER_ANCHOR,
+  ABOVE_INNER_LEFT_ANCHOR,
+  ABOVE_INNER_RIGHT_ANCHOR,
+  ABOVE_LEFT_ANCHOR,
+  ABOVE_RIGHT_ANCHOR,
+  BELOW_CENTER_ANCHOR,
+  BELOW_INNER_LEFT_ANCHOR,
+  BELOW_INNER_RIGHT_ANCHOR,
+  BELOW_LEFT_ANCHOR,
+  BELOW_RIGHT_ANCHOR,
+  BOTTOM_CENTER_ANCHOR,
+  BOTTOM_INNER_LEFT_ANCHOR,
+  BOTTOM_INNER_RIGHT_ANCHOR,
+  BOTTOM_LEFT_ANCHOR,
+  BOTTOM_RIGHT_ANCHOR,
+  CENTER_CENTER_ANCHOR,
+  CENTER_INNER_LEFT_ANCHOR,
+  CENTER_INNER_RIGHT_ANCHOR,
+  CENTER_LEFT_ANCHOR,
+  CENTER_RIGHT_ANCHOR,
+  PositionAnchor,
+  TOP_CENTER_ANCHOR,
+  TOP_INNER_LEFT_ANCHOR,
+  TOP_INNER_RIGHT_ANCHOR,
+  TOP_LEFT_ANCHOR,
+  TOP_RIGHT_ANCHOR,
+} from "@react-md/utils";
 
 import styles from "./MenuPositioning.module.scss";
 
-const items = ["Item 1", "Item 2", "Item 3"];
-
-const verticalPositions: VerticalPosition[] = [
-  "above",
-  "below",
-  "center",
-  "top",
-  "bottom",
+const anchors = [
+  ABOVE_LEFT_ANCHOR,
+  ABOVE_INNER_LEFT_ANCHOR,
+  ABOVE_CENTER_ANCHOR,
+  ABOVE_INNER_RIGHT_ANCHOR,
+  ABOVE_RIGHT_ANCHOR,
+  TOP_LEFT_ANCHOR,
+  TOP_INNER_LEFT_ANCHOR,
+  TOP_CENTER_ANCHOR,
+  TOP_INNER_RIGHT_ANCHOR,
+  TOP_RIGHT_ANCHOR,
+  CENTER_LEFT_ANCHOR,
+  CENTER_INNER_LEFT_ANCHOR,
+  CENTER_CENTER_ANCHOR,
+  CENTER_INNER_RIGHT_ANCHOR,
+  CENTER_RIGHT_ANCHOR,
+  BOTTOM_LEFT_ANCHOR,
+  BOTTOM_INNER_LEFT_ANCHOR,
+  BOTTOM_CENTER_ANCHOR,
+  BOTTOM_INNER_RIGHT_ANCHOR,
+  BOTTOM_RIGHT_ANCHOR,
+  BELOW_LEFT_ANCHOR,
+  BELOW_INNER_LEFT_ANCHOR,
+  BELOW_CENTER_ANCHOR,
+  BELOW_INNER_RIGHT_ANCHOR,
+  BELOW_RIGHT_ANCHOR,
 ];
-const horizontalPositions: HorizontalPosition[] = [
-  "inner-left",
-  "inner-right",
-  "center",
-  "left",
-  "right",
-];
 
-const MenuPositioning: FC = () => (
-  <>
-    {horizontalPositions.map((x, i) => (
-      <Fragment key={x}>
-        <Text type="headline-6" margin={i === 0 ? "none" : "top"}>
-          Horizontal Position: <Code>{x}</Code>
-        </Text>
-        <div className={styles.container}>
-          <div className={styles.group}>
-            {verticalPositions.map((y) => (
-              <DropdownMenu
-                id={`position-${x}-${y}`}
-                key={y}
-                anchor={{ x, y }}
-                items={items}
-                menuLabel="Menu"
-                themeType="outline"
-                dropdownIcon={<ArrowDropDownSVGIcon className={styles.icon} />}
-                className={styles.button}
-              >
-                <span className={styles.text}>{y}</span>
-              </DropdownMenu>
-            ))}
-          </div>
-        </div>
-      </Fragment>
-    ))}
-  </>
-);
+function getLabel({ x, y }: PositionAnchor): string {
+  return `${upperFirst(y)} ${upperFirst(x)} anchor`;
+}
+
+const options = anchors.map((anchor, index) => ({
+  label: getLabel(anchor),
+  value: `${index}`,
+}));
+
+// don't include all items to better show how the position affects the menu
+const items = options.slice(0, 5).map(({ label }) => label);
+
+const MenuPositioning: FC = () => {
+  const [anchorIndex, setAnchorIndex] = useState(() =>
+    anchors.findIndex((a) => a === CENTER_CENTER_ANCHOR)
+  );
+  const [equalWidth, handleChange, setEqualWidth] = useChecked(false);
+  const anchor = anchors[anchorIndex];
+  if (anchor.x !== "center" && equalWidth) {
+    setEqualWidth(false);
+  }
+
+  return (
+    <>
+      <Select
+        id="menu-anchor-select"
+        label="Menu Anchor"
+        options={options}
+        value={`${anchorIndex}`}
+        onChange={(value) => {
+          const i = parseInt(value, 10);
+          if (!Number.isNaN(i) && i >= 0 && i <= options.length) {
+            setAnchorIndex(i);
+          }
+        }}
+      />
+      <Checkbox
+        id="menu-anchor-equal-width"
+        label="Equal Width?"
+        checked={equalWidth}
+        onChange={handleChange}
+        disabled={anchor.x !== "center"}
+      />
+      <DropdownMenu
+        id="menu-anchor"
+        anchor={anchor}
+        items={items}
+        menuLabel="Menu"
+        theme="secondary"
+        themeType="contained"
+        dropdownIcon={<ArrowDropDownSVGIcon className={styles.icon} />}
+        className={styles.button}
+        positionOptions={{ width: equalWidth ? "equal" : "auto" }}
+      >
+        <span className={styles.text}>Menu</span>
+      </DropdownMenu>
+    </>
+  );
+};
 
 export default MenuPositioning;
