24b74343cce1e347a245c172e5df8251e5a8c9a5
Mikkel Laursen <mlaursen03@gmail.com>
2020-06-11 16:35:57 -0600

    chore: Updates to look better in npm
    Created a new `yarn dev-utils rmd-readme` script that will copy the root
    README.md to the `react-md` folder and insert some additional
    information into the README. This is just to help add documentation if
    this package is found through npm instead of GitHub or other sources.
    
    Also updated all the package.json files to link to their specific
    documentation page as the `"homepage"` field.

---
+++ packages/dev-utils/src/rmdReadme/README.md
@@ -0,0 +1,23 @@
+## Installation
+
+react-md has been updated to use
+[scoped packages](https://react-md.dev/guides/scoped-packages) starting with the
+v2 release to help slowly transition to the new components and API. If you do
+not need to slowly transition over this package can be used instead which is a
+convenience package for exporting all the functionality from a single entry
+point.
+
+```sh
+$ npm install --save react-md
+```
+
+or with `yarn`:
+
+```sh
+$ yarn add react-md
+```
+
+For convenience, here is a list of all the included react-md scoped packages and
+a link to their demo documentation page:
+
+#packages
---
+++ packages/dev-utils/src/rmdReadme/index.ts
@@ -0,0 +1,34 @@
+import { join } from "path";
+import { readFile } from "fs-extra";
+
+import { projectRoot, packagesRoot } from "../constants";
+import writeFile from "../utils/writeFile";
+import format from "../utils/format";
+import getPackages from "../utils/getPackages";
+
+const REPLACE_TOKEN = "<!-- rmd-readme-replace -->";
+
+export default async function rmdReadme(): Promise<void> {
+  const rootReadme = await readFile(join(projectRoot, "README.md"), "utf8");
+  if (!rootReadme.includes(REPLACE_TOKEN)) {
+    throw new Error("Unable to find the replace token in the root README.md");
+  }
+
+  const replacement = await readFile(
+    join(packagesRoot, "dev-utils", "src", "rmdReadme", "README.md"),
+    "utf8"
+  );
+  const packages = getPackages()
+    .map(
+      (name) =>
+        `- [@react-md/${name}](https://react-md.dev/packages/${name}/demos)`
+    )
+    .join("\n");
+
+  const markdown = rootReadme
+    .replace(REPLACE_TOKEN, replacement)
+    .replace("#packages", packages);
+
+  const updated = format(markdown, "markdown");
+  return writeFile(join(packagesRoot, "react-md", "README.md"), updated);
+}
