cac5cd16a1452130ba600833c8ad1180d7ec4918
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-08 20:09:50 -0600

    feat: Exported the `useAutoComplete` hook
    This isn't really documented, but might be helpful for different
    situations if the AutoComplete component doesn't meet your needs.

--- packages/autocomplete/src/index.ts
+++ packages/autocomplete/src/index.ts
@@ -3,6 +3,9 @@ export { default as AutoComplete } from "./AutoComplete";
 export { default as HighlightedResult } from "./HighlightedResult";
 export * from "./HighlightedResult";
 
+export { default as useAutoComplete } from "./useAutoComplete";
+export * from "./useAutoComplete";
+
 export {
   isResultOf,
   getResultId as DEFAULT_GET_RESULT_ID,
