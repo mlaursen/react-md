f5d10f5b45339268ca671b190f03ad19138cdc94
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-08 23:37:55 -0600

    chore: Added .versionrc.js

---
+++ .versionrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features' },
+    { type: 'fix', section: 'Bug Fixes' },
+    { type: 'docs', section: 'Documentation', hidden: true },
+    { type: 'chore', section: 'Documentation', hidden: true },
+    { type: 'ci', hidden: true },
+  ],
+};
