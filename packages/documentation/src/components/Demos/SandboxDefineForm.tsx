0f67a4cc1e243452bdeb6882c85d006878f8d57f
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-11 16:00:13 -0600

    docs: Updated sandboxes to always use latest rmd version

--- packages/documentation/src/components/Demos/SandboxDefineForm.tsx
+++ packages/documentation/src/components/Demos/SandboxDefineForm.tsx
@@ -1,7 +1,8 @@
 import React, { FC, useEffect, useState } from "react";
 import { getParameters } from "codesandbox/lib/api/define";
-
 import { IFiles } from "codesandbox-import-utils/lib/api/define";
+
+import { RMD_VERSION } from "constants/github";
 
 const CODE_SANDBOX_DEFINE_API =
   "https://codesandbox.io/api/v1/sandboxes/define";
@@ -25,25 +26,7 @@ const SandboxDefineForm: FC<SandboxDefineFormProps> = ({
         (files, [fileName, data]) => {
           let { content } = data;
           if (typeof content === "string") {
-            content = content.replace(/{{RMD_VERSION}}/g, "latest");
-          } else if (fileName === "package.json") {
-            /* eslint-disable @typescript-eslint/no-explicit-any */
-            // the IFiles type is incorrect since it doesn't include the type for package.json
-            const dependencies = (content as any).dependencies as Record<
-              string,
-              string
-            >;
-
-            const updated = Object.entries(dependencies).reduce(
-              (json, [key, value]) => ({
-                ...json,
-                [key]: value.replace(/{{RMD_VERSION}}/g, "latest"),
-              }),
-              {}
-            );
-
-            content = { ...(content as any), dependencies: updated };
-            /* eslint-enable @typescript-eslint/no-explicit-any */
+            content = content.replace(/{{RMD_VERSION}}/g, RMD_VERSION);
           }
 
           files[fileName] = { ...data, content };
