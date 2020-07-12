7b372926289d0c1cdab76dbea9cb298e7594dfa9
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-11 18:19:46 -0600

    fix: ListItem disabled states
    The ListItem will now correctly apply the disabled states as needed and
    also prevent the item from being clicked while disabled.

--- packages/list/src/__tests__/ListItem.tsx
+++ packages/list/src/__tests__/ListItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import ListItem from "../ListItem";
 
@@ -10,5 +10,23 @@ describe("ListItem", () => {
 
     rerender(<ListItem leftAddon={<span>Left Icon</span>}>Content</ListItem>);
     expect(container).toMatchSnapshot();
+  });
+
+  it('should correctly "polyfill" the disabled behavior by preventing click events when disabled', () => {
+    const onClick = jest.fn();
+    const props = { children: "Content", onClick };
+    const { rerender, getByRole } = render(<ListItem {...props} disabled />);
+
+    const item = getByRole("button");
+    expect(item).toHaveAttribute("aria-disabled", "true");
+    expect(item.className).toContain("rmd-list-item--disabled");
+    fireEvent.click(item);
+    expect(onClick).not.toBeCalled();
+
+    rerender(<ListItem {...props} aria-disabled />);
+    expect(item).toHaveAttribute("aria-disabled", "true");
+    expect(item.className).toContain("rmd-list-item--disabled");
+    fireEvent.click(item);
+    expect(onClick).not.toBeCalled();
   });
 });
--- packages/list/src/__tests__/SimpleListItem.tsx
+++ packages/list/src/__tests__/SimpleListItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 
 import SimpleListItem from "../SimpleListItem";
 
@@ -12,5 +12,31 @@ describe("SimpleListItem", () => {
 
     rerender(<SimpleListItem {...props} leftAddon={<span>Left</span>} />);
     expect(container).toMatchSnapshot();
+  });
+
+  it('should correctly "polyfill" the disabled behavior by preventing click events when disabled', () => {
+    const onClick = jest.fn();
+    const props = { children: "Content", onClick, clickable: true };
+    const { rerender, getByText } = render(
+      <SimpleListItem {...props} disabled />
+    );
+
+    const item = getByText(props.children);
+    expect(item).toHaveAttribute("aria-disabled", "true");
+    expect(item.className).toContain("rmd-list-item--disabled");
+    fireEvent.click(item);
+    expect(onClick).not.toBeCalled();
+
+    rerender(<SimpleListItem {...props} aria-disabled />);
+    expect(item).toHaveAttribute("aria-disabled", "true");
+    expect(item.className).toContain("rmd-list-item--disabled");
+    fireEvent.click(item);
+    expect(onClick).not.toBeCalled();
+
+    rerender(<SimpleListItem {...props} aria-disabled={false} />);
+    expect(item).not.toHaveAttribute("aria-disabled");
+    expect(item.className).not.toContain("rmd-list-item--disabled");
+    fireEvent.click(item);
+    expect(onClick).toBeCalled();
   });
 });
