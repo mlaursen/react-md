d71acb51713c431d5dc374c7a6627fdeb7df938b
Mikkel Laursen <mlaursen03@gmail.com>
2020-07-29 16:41:29 -0600

    chore: docs and chore to no longer be hidden in changelog

--- .versionrc.js
+++ .versionrc.js
@@ -2,8 +2,8 @@ module.exports = {
   types: [
     { type: 'feat', section: 'Features' },
     { type: 'fix', section: 'Bug Fixes' },
-    { type: 'docs', section: 'Documentation', hidden: true },
-    { type: 'chore', section: 'Documentation', hidden: true },
+    { type: 'docs', section: 'Documentation' },
+    { type: 'chore', section: 'Other' },
     { type: 'ci', hidden: true },
   ],
 };
