ce2394f4278a3e5b9da4e450815c65c793ac0c9f
Mikkel Laursen <mlaursen03@gmail.com>
2020-08-01 16:32:48 -0600

    chore(libsize): Updated library size

--- packages/documentation/src/components/Home/LibraryInfo/OtherPros.tsx
+++ packages/documentation/src/components/Home/LibraryInfo/OtherPros.tsx
@@ -1,25 +1,26 @@
+/* this file is automatically updated by `yarn dev-utils libsize` and should not be updated manually */
 import React, { ReactElement } from "react";
 
 import TableCellList from "./TableCellList";
 
-const OtherPros = (): ReactElement | null => (
-  <TableCellList>
-    <li>
-      A fairly small library size (gzipped):
-      <ul>
-        <li>
-          Production UMD Bundle:
-          <br />
-          <b>86.49 KB</b>
-        </li>
-        <li>
-          Default Production CSS Bundles:
-          <br />
-          <b>15.65 KB</b> - <b>15.71 KB</b>
-        </li>
-      </ul>
-    </li>
-  </TableCellList>
-);
-
-export default OtherPros;
+export default function OtherPros(): ReactElement {
+  return (
+    <TableCellList>
+      <li>
+        A fairly small library size (gzipped):
+        <ul>
+          <li>
+            Production UMD Bundle:
+            <br />
+            <b>86.76 KB</b>
+          </li>
+          <li>
+            Default Production CSS Bundles:
+            <br />
+            <b>15.4 KB</b> - <b>15.45 KB</b>
+          </li>
+        </ul>
+      </li>
+    </TableCellList>
+  );
+}
