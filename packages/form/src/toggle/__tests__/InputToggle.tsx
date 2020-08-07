86e19dba17d9a4b0a25bc528c6fd485be7bbec39
Mikkel Laursen <mlaursen03@gmail.com>
2020-08-06 19:45:43 -0600

    feat(form): Added props to style Checkbox and Radio input element

--- packages/form/src/toggle/__tests__/InputToggle.tsx
+++ packages/form/src/toggle/__tests__/InputToggle.tsx
@@ -15,4 +15,34 @@ describe("InputToggle", () => {
     rerender(<InputToggle {...props} type="checkbox" />);
     expect(container).toMatchSnapshot();
   });
+
+  it("should correctly pass the inputStyle and inputClassName to the invisible input element", () => {
+    const props = {
+      id: "toggle",
+      style: { color: "orange" },
+      className: "custom-container",
+      inputStyle: { color: "red" },
+      inputClassName: "custom-input",
+    };
+
+    const { container, rerender, getByRole } = render(
+      <InputToggle type="checkbox" {...props} />
+    );
+
+    const toggleContainer = container.firstElementChild as HTMLDivElement;
+    expect(toggleContainer.style.color).toBe("orange");
+    expect(toggleContainer.className).toContain(props.className);
+    const checkbox = getByRole("checkbox");
+    expect(checkbox.style.color).toBe("red");
+    expect(checkbox.className).toContain(props.inputClassName);
+    expect(container).toMatchSnapshot();
+
+    rerender(<InputToggle type="radio" value="a" {...props} />);
+    const radio = getByRole("radio");
+    expect(toggleContainer.style.color).toBe("orange");
+    expect(toggleContainer.className).toContain(props.className);
+    expect(radio.style.color).toBe("red");
+    expect(radio.className).toContain(props.inputClassName);
+    expect(container).toMatchSnapshot();
+  });
 });
