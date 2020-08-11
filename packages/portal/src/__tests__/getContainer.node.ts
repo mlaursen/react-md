965d08b0ea2d1a9f6349f2654182b980f8e47eb5
Mikkel Laursen <mlaursen03@gmail.com>
2020-08-10 19:12:20 -0600

    chore(portal): additional test coverage

---
+++ packages/portal/src/__tests__/getContainer.node.ts
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment node
+ */
+
+import getContainer from "../getContainer";
+
+describe("getContainer", () => {
+  it("should return null for non-browser environments", () => {
+    expect(typeof document).toBe("undefined");
+    expect(getContainer()).toBe(null);
+  });
+});
