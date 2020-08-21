e467fe51aba0bf707219495a4f8a0cf3b7dc29b2
Mikkel Laursen <mlaursen03@gmail.com>
2020-08-21 15:13:15 -0600

    chore(utils): added a nearest util to round numbers within a range

---
+++ packages/utils/src/__tests__/nearest.ts
@@ -0,0 +1,17 @@
+import { nearest } from "../nearest";
+
+describe("nearest", () => {
+  it("should round correctly", () => {
+    expect(nearest(1, 0, 1, 1)).toBe(1);
+    expect(nearest(0, 0, 100, 100)).toBe(0);
+
+    expect(nearest(28, 0, 100, 10)).toBe(30);
+    expect(nearest(28.7, 0, 100, 10)).toBe(30);
+    expect(nearest(28.3, 0, 100, 100)).toBe(28);
+    expect(nearest(28.7, 0, 100, 100)).toBe(29);
+
+    expect(nearest(2.75, 0, 10, 10)).toBe(3);
+    expect(nearest(5.12, 5, 6, 10)).toBe(5.1);
+    expect(nearest(0.06, 0, 1, 10)).toBe(0.1);
+  });
+});
---
+++ packages/utils/src/nearest.ts
@@ -0,0 +1,26 @@
+/**
+ * Rounds a number to the nearest step within a min/max range.
+ *
+ * @see https://stackoverflow.com/a/13635455
+ * @param value The value to round to the nearest step
+ * @param min The min value allowed
+ * @param max The max value allowed
+ * @param steps The number of steps in the min/max range
+ * @return the value rounded to the nearest step in the min/max range
+ */
+export function nearest(
+  value: number,
+  min: number,
+  max: number,
+  steps: number
+): number {
+  const range = max - min;
+  const rounded = Math.round(((value - min) * steps) / range) / steps;
+  const zeroToOne = Math.min(Math.max(rounded, 0), 1);
+
+  // have to calculate the number of allowed decimal places since decimal
+  // precision gets weird:
+  // 0.28 * 100 === 28.000000000000004
+  const decimals = range % steps;
+  return parseFloat((zeroToOne * range + min).toFixed(decimals));
+}
