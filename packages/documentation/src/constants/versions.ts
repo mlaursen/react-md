0bc495d63175b61b8c8e5f42fa90752e2acdde89
Mikkel Laursen <mlaursen03@gmail.com>
2020-11-12 21:41:24 -0700

    build(docs): reverted back to nect@9.5.2 since css still no work

--- packages/documentation/src/constants/versions.ts
+++ packages/documentation/src/constants/versions.ts
@@ -86,7 +86,7 @@ export const versions: Record<string, string> = {
   marked: "1.2.3",
   "mobile-detect": "1.4.4",
   morgan: "1.10.0",
-  next: "9.5.5",
+  next: "9.5.2",
   prismjs: "1.21.0",
   qs: "6.9.4",
   "react-hook-form": "6.11.0",
