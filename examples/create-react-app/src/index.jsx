be003a9cca1ef7c136fb21c21ab13f9117da5acd
Mikkel Laursen <mlaursen03@gmail.com>
2020-11-13 19:31:08 -0700

    chore(example): updated create-react-app example for v4 of react-scripts

--- examples/create-react-app/src/index.jsx
+++ examples/create-react-app/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <Router>
@@ -12,7 +12,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
