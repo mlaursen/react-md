434dcf688afc8cf5077defd7af09c54cbadb83b1
Mikkel Laursen <mlaursen03@gmail.com>
2020-12-11 19:45:31 -0700

    chore(slider): updated RangeSlider demos for disabled states

--- packages/documentation/src/components/Demos/Form/RangeSliders.tsx
+++ packages/documentation/src/components/Demos/Form/RangeSliders.tsx
@@ -10,6 +10,7 @@ export default function RangeSliders(): ReactElement | null {
   const [_value3, controls3] = useRangeSlider([10, 20], { min: 0, max: 50 });
   const [_value4, controls4] = useRangeSlider([10, 60]);
   const [_value5, controls5] = useRangeSlider([10, 20], { min: 0, max: 50 });
+  const [_value6, controls6] = useRangeSlider([40, 70]);
   return (
     <>
       <SlidersContainer>
@@ -26,6 +27,7 @@ export default function RangeSliders(): ReactElement | null {
           {...controls2}
           thumb1Label="Min"
           thumb2Label="Max"
+          disabled
         />
         <RangeSlider
           baseId={useId()}
@@ -51,6 +53,15 @@ export default function RangeSliders(): ReactElement | null {
           thumb1Label="Min"
           thumb2Label="Max"
           vertical
+        />
+        <RangeSlider
+          baseId={useId()}
+          label="Vertical"
+          {...controls6}
+          thumb1Label="Min"
+          thumb2Label="Max"
+          vertical
+          disabled
         />
       </SlidersContainer>
     </>
