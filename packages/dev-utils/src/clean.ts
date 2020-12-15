2a9f7b5da7e37278deb0222ccadb7bdf75d16d23
Mikkel Laursen <mlaursen03@gmail.com>
2020-12-15 15:04:33 -0700

    chore(dev-utils): fixed clean command to remove old type files

--- packages/dev-utils/src/clean.ts
+++ packages/dev-utils/src/clean.ts
@@ -1,8 +1,8 @@
-import { dist, es, lib } from "./constants";
+import { dist, es, lib, types } from "./constants";
 import { clean as remove, glob } from "./utils";
 
 export async function clean(): Promise<void> {
-  const pattern = `packages/!(dev-utils|documentation)/@(${es}|${lib}|${dist}|*.tsbuildinfo)`;
+  const pattern = `packages/!(dev-utils|documentation)/@(${es}|${lib}|${dist}|${types}|*.tsbuildinfo)`;
   const files = await glob(pattern);
 
   return remove(files);
