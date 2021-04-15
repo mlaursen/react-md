1e0e783bbbf7659a4de4a8803e57366f5681cbd4
Mikkel Laursen <mlaursen03@gmail.com>
2021-04-14 19:31:04 -0600

    chore(docs): Added a new Hover Mode demo

--- packages/documentation/src/components/Demos/Utils/index.tsx
+++ packages/documentation/src/components/Demos/Utils/index.tsx
@@ -23,6 +23,9 @@ import simpleGridList from "./SimpleGridList.md";
 import GridListSize from "./GridListSize";
 import gridListSize from "./GridListSize.md";
 
+import HoverMode from "./HoverMode";
+import hoverMode from "./HoverMode.md";
+
 const demos = [
   {
     name: "App Size Listener Example",
@@ -58,6 +61,11 @@ const demos = [
     name: "Grid List Size",
     description: gridListSize,
     children: <GridListSize />,
+  },
+  {
+    name: "Hover Mode",
+    description: hoverMode,
+    children: <HoverMode />,
   },
 ];
 
---
+++ packages/documentation/src/components/WikipediaPreviewLink/WikipediaPreviewLink.tsx
@@ -0,0 +1,62 @@
+import React, { ReactElement, ReactNode, useRef } from "react";
+import { DialogContent, FixedDialog } from "@react-md/dialog";
+import { Link } from "@react-md/link";
+import { BELOW_CENTER_ANCHOR, useHoverMode } from "@react-md/utils";
+
+import { useId } from "components/IdProvider";
+
+import styles from "./WikipediaPreviewLink.module.scss";
+
+export interface WikipediaPreviewLinkProps {
+  /**
+   * The wikipedia URL
+   */
+  href: string;
+
+  /**
+   * The content to display in the preview window.
+   */
+  preview: ReactNode;
+
+  /**
+   * The link contents.
+   */
+  children: ReactNode;
+}
+
+export default function WikipediaPreviewLink({
+  href,
+  preview,
+  children,
+}: WikipediaPreviewLinkProps): ReactElement {
+  const id = useId("wiki");
+  const { active, handlers, visible, setVisible } = useHoverMode();
+
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  return (
+    <>
+      <Link ref={linkRef} href={href} {...handlers}>
+        {children}
+      </Link>
+      <FixedDialog
+        aria-label="Wikipedia Preview"
+        id={id}
+        visible={visible}
+        onRequestClose={() => setVisible(false)}
+        {...handlers}
+        fixedTo={linkRef.current}
+        onClick={() => {
+          window.location.href = href;
+        }}
+        disableFocusContainer={active}
+        disableScrollLock={!active}
+        anchor={BELOW_CENTER_ANCHOR}
+        options={{ preventOverlap: true }}
+        overlay={active ? false : undefined}
+        className={styles.dialog}
+      >
+        <DialogContent>{preview}</DialogContent>
+      </FixedDialog>
+    </>
+  );
+}
