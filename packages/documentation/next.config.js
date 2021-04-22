8389b68325aff0ec79565d86b2cc35414fb755f3
Mikkel Laursen <mlaursen03@gmail.com>
2021-04-22 16:48:57 -0600

    chore(docs): removed custom nextjs server

--- packages/documentation/next.config.js
+++ packages/documentation/next.config.js
@@ -1,26 +1,34 @@
 const withImages = require('next-images');
 
-const withCustomConfig = (nextConfig = {}) => ({
-  ...nextConfig,
-  future: {
-    webpack5: true,
-    strictPostcssConfiguration: true,
-  },
-  distDir: 'build',
-  poweredByHeader: false,
-  webpack(config, options) {
-    config.module.rules.unshift({
-      test: /\.md$/,
-      use: 'raw-loader',
-      exclude: /node_modules/,
-    });
+const withCustomConfig = (nextConfig = {}) => {
+  /**
+   * @type import('next/dist/next-server/server/config-shared').NextConfig
+   */
+  const config = {
+    ...nextConfig,
+    future: {
+      webpack5: true,
+      strictPostcssConfiguration: true,
+    },
+    distDir: 'build',
+    poweredByHeader: false,
+    compress: false,
+    webpack(config, options) {
+      config.module.rules.unshift({
+        test: /\.md$/,
+        use: 'raw-loader',
+        exclude: /node_modules/,
+      });
 
-    if (typeof nextConfig.webpack === 'function') {
-      return nextConfig.webpack(config, options);
-    }
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options);
+      }
 
-    return config;
-  },
-});
+      return config;
+    },
+  };
+
+  return config;
+};
 
 module.exports = withImages(withCustomConfig());
