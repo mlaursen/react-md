5d9ee71e18494b38e26a7478fd54bf40de49a023
Mikkel Laursen <mlaursen03@gmail.com>
2021-04-23 23:52:12 -0600

    chore(docs): redirect to v1 website

--- packages/documentation/next.config.js
+++ packages/documentation/next.config.js
@@ -26,6 +26,20 @@ const withCustomConfig = (nextConfig = {}) => {
 
       return config;
     },
+    async redirects() {
+      return [
+        {
+          source: '/v1/',
+          destination: 'https://mlaursen.github.io/react-md-v1-docs/#/',
+          permanent: false,
+        },
+        {
+          source: '/v1/:paths*',
+          destination: 'https://mlaursen.github.io/react-md-v1-docs/#/:paths*',
+          permanent: false,
+        },
+      ];
+    },
   };
 
   return config;
