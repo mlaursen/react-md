e78f668d28ad0e1ff99feb9538c6a6b9e765d72c
Mikkel Laursen <mlaursen03@gmail.com>
2021-08-10 16:47:46 -0600

    test: Added additional tests to bump test coverage
    ....

---
+++ packages/typography/src/__tests__/SrOnly.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import { SrOnly } from "../SrOnly";
+
+describe("SrOnly", () => {
+  it("should render correctly", () => {
+    const { container, rerender } = render(<SrOnly>Some Text</SrOnly>);
+    expect(container).toMatchSnapshot();
+
+    rerender(<SrOnly focusable>Some Text</SrOnly>);
+    expect(container).toMatchSnapshot();
+
+    rerender(<SrOnly tabIndex={-1}>Some Text</SrOnly>);
+    expect(container).toMatchSnapshot();
+
+    rerender(
+      <SrOnly tabIndex={-1} focusable>
+        Some Text
+      </SrOnly>
+    );
+    expect(container).toMatchSnapshot();
+
+    rerender(<SrOnly component="div">Some Text</SrOnly>);
+    expect(container).toMatchSnapshot();
+  });
+});
