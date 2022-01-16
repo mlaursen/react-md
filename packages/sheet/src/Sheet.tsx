83fcaacfac29dd867b07bd857fbe17faeea371b2
Mikkel Laursen <mlaursen03@gmail.com>
2022-01-16 00:45:54 -0700

    docs(sheet): Move tsdoc around for easier sharing

--- packages/sheet/src/Sheet.tsx
+++ packages/sheet/src/Sheet.tsx
@@ -10,8 +10,39 @@ type AllowedDialogProps = Omit<
   "role" | "type" | "modal" | "forceContainer"
 >;
 
+/**
+ * The location that the sheet should be located within the viewport.
+ */
 export type SheetPosition = "top" | "right" | "bottom" | "left";
+
+/**
+ * The size to use for sheets that have been positioned left or right. The
+ * default supported values are:
+ *
+ * - none - the size is based on content, but is still limited to the viewport
+ *   width so that the horizontal scrolling will not occur within the page. No
+ *   limits added to sizing.
+ * - touch - the `min-width` is set to be the entire viewport width minus a
+ *   touchable area and `max-width` is set to `20rem` and is normally
+ *   recommended for mobile devices.
+ * - static - the width is set to a static `16rem` and generally used for
+ *   landscape tablets and desktops.
+ * - media - automatically switches between "touch" and "static" based on css
+ *   media queries. (this is the default)
+ */
 export type SheetHorizontalSize = "none" | "media" | "touch" | "static";
+
+/**
+ * The size to use for sheets that have been positioned top or bottom. The
+ * supported sizes are:
+ *
+ * - none - the size is based on content and is limited to the viewport
+ *   height.
+ * - touch - the size is based on content and is limited to the viewport
+ *   height with a touchable area to close the sheet.
+ * - recommended - the material design recommended sizing that forces a
+ *   max-height of 50vh and min-height of 3.5rem
+ */
 export type SheetVerticalSize = "none" | "touch" | "recommended";
 
 export interface BaseSheetProps extends AllowedDialogProps {
@@ -25,39 +56,11 @@ export interface BaseSheetProps extends AllowedDialogProps {
    */
   role?: "dialog" | "menu" | "none";
 
-  /**
-   * The location that the sheet should be located within the viewport.
-   */
+  /** {@inheritDoc SheetPosition} */
   position?: SheetPosition;
-
-  /**
-   * The size to use for sheets that have been positioned left or right. The
-   * default supported values are:
-   *
-   * - none - the size is based on content, but is still limited to the viewport
-   *   width so that the horizontal scrolling will not occur within the page. No
-   *   limits added to sizing.
-   * - touch - the `min-width` is set to be the entire viewport width minus a
-   *   touchable area and `max-width` is set to `20rem` and is normally
-   *   recommended for mobile devices.
-   * - static - the width is set to a static `16rem` and generally used for
-   *   landscape tablets and desktops.
-   * - media - automatically switches between "touch" and "static" based on css
-   *   media queries. (this is the default)
-   */
+  /** {@inheritDoc SheetHorizontalSize} */
   horizontalSize?: SheetHorizontalSize;
-
-  /**
-   * The size to use for sheets that have been positioned top or bottom. The
-   * supported sizes are:
-   *
-   * - none - the size is based on content and is limited to the viewport
-   *   height.
-   * - touch - the size is based on content and is limited to the viewport
-   *   height with a touchable area to close the sheet.
-   * - recommended - the material design recommended sizing that forces a
-   *   max-height of 50vh and min-height of 3.5rem
-   */
+  /** {@inheritDoc SheetVerticalSize} */
   verticalSize?: SheetVerticalSize;
 }
 
