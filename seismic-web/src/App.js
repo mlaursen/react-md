530150c8628ffc09d5ac99986fb8b7ed19e70b86
Yasir <yhossain@gmail.com>
2022-05-28 10:34:42 -0700

    created localUser module

--- seismic-web/src/App.js
+++ seismic-web/src/App.js
@@ -2,12 +2,12 @@ import React, { useRef, useState, useEffect } from 'react';
 
 import './styles/App.scss';
 
+import { getSelf } from './modules/locaUser';
+
 import { auth } from './modules/firebase';
 import firebase from 'firebase/compat/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-import { ANONYMOUS_USER } from './helpers/constants';
 
 import Chat from './components/Chat';
 import VideoPlayer from './components/VideoPlayer';
@@ -15,7 +15,7 @@ import VideoPlayer from './components/VideoPlayer';
 import Button from '@mui/material/Button';
 
 function App() {
-  const [user] = useAuthState(auth);
+  const [user, setUser] = useAuthState(auth);
   const [currentUser, setCurrentUser] = useState(null);
 
   const [isVisible, setIsVisible] = useState(false);
@@ -29,65 +29,12 @@ function App() {
 
   useEffect(() => {
     // TODO: Assign Self to Chat XP
-    getSelf();
+    // getSelf(user);
+    // setCurrentUser = getSelf(user);
+    setCurrentUser(getSelf(user));
     //getInitChatMessages()
     //listenToNewMessages()
   }, [user]);
-
-  /*
-  const createUser = () => {
-    const usersRef = db.collection('users');
-    console.log(usersRef);
-  }
-  */
-
-  const getSelf = async () => {
-    let localUser = localStorage.getItem('seismicChatUser');
-
-    // Returning User Who Has Not Signed In
-    // User info is pulled from local storage
-    if (localUser !== null && user === null) {
-      //console.log("This is a returning user who has not signed in.");
-      setCurrentUser(JSON.parse(localUser));
-    }
-    // Returning User Who Has Signed In
-    // User info is pulled from Firebase
-    else if (localUser !== null && user !== null) {
-      //console.log("This is a returning signed in user.");
-
-      let parsedLocalUser = JSON.parse(localUser);
-      let tempUser = null;
-
-      if (parsedLocalUser.avatarUrl === undefined) {
-        tempUser = {
-          ...user,
-          avatarUrl: ANONYMOUS_USER.avatarUrl,
-          chatName: ANONYMOUS_USER.chatName,
-          devices: ANONYMOUS_USER.devices,
-          role: ANONYMOUS_USER.role,
-          preferences: ANONYMOUS_USER.preferences,
-        };
-      } else {
-        tempUser = {
-          ...user,
-          avatarUrl: parsedLocalUser.avatarUrl,
-          chatName: parsedLocalUser.chatName,
-          devices: parsedLocalUser.devices,
-          role: parsedLocalUser.role,
-          preferences: parsedLocalUser.preferences,
-        };
-      }
-      setCurrentUser(tempUser);
-      localStorage.setItem('seismicChatUser', JSON.stringify(tempUser));
-    }
-    // New User
-    // Anonymous user is created
-    else {
-      setCurrentUser(ANONYMOUS_USER);
-      //console.log("This is a new user");
-      localStorage.setItem('seismicChatUser', JSON.stringify(ANONYMOUS_USER));
-    }
-  };
 
   function SignIn() {
     const signInWithGoogle = () => {
--- seismic-web/src/routes/Admin.js
+++ seismic-web/src/routes/Admin.js
@@ -2,12 +2,12 @@ import React, { useRef, useState, useEffect } from 'react';
 
 import '../styles/Admin.scss';
 
+import { getSelf } from '../modules/locaUser';
+
 import { auth, analytics, db } from '../modules/firebase';
 import firebase from 'firebase/compat/app';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
-
-import { ANONYMOUS_USER } from '../helpers/constants';
 
 import Chat from '../components/Chat';
 import VideoPlayer from '../components/VideoPlayer';
@@ -29,7 +29,7 @@ function Admin() {
 
   useEffect(() => {
     // TODO: Assign Self to Chat XP
-    getSelf();
+    setCurrentUser(getSelf(user));
     //getInitChatMessages()
     //listenToNewMessages()
   }, [user]);
@@ -40,54 +40,6 @@ function Admin() {
     console.log(usersRef);
   }
   */
-
-  const getSelf = async () => {
-    let localUser = localStorage.getItem('seismicChatUser');
-
-    // Returning User Who Has Not Signed In
-    // User info is pulled from local storage
-    if (localUser !== null && user === null) {
-      //console.log("This is a returning user who has not signed in.");
-      setCurrentUser(JSON.parse(localUser));
-    }
-    // Returning User Who Has Signed In
-    // User info is pulled from Firebase
-    else if (localUser !== null && user !== null) {
-      //console.log("This is a returning signed in user.");
-
-      let parsedLocalUser = JSON.parse(localUser);
-      let tempUser = null;
-
-      if (parsedLocalUser.avatarUrl === undefined) {
-        tempUser = {
-          ...user,
-          avatarUrl: ANONYMOUS_USER.avatarUrl,
-          chatName: ANONYMOUS_USER.chatName,
-          devices: ANONYMOUS_USER.devices,
-          role: ANONYMOUS_USER.role,
-          preferences: ANONYMOUS_USER.preferences,
-        };
-      } else {
-        tempUser = {
-          ...user,
-          avatarUrl: parsedLocalUser.avatarUrl,
-          chatName: parsedLocalUser.chatName,
-          devices: parsedLocalUser.devices,
-          role: parsedLocalUser.role,
-          preferences: parsedLocalUser.preferences,
-        };
-      }
-      setCurrentUser(tempUser);
-      localStorage.setItem('seismicChatUser', JSON.stringify(tempUser));
-    }
-    // New User
-    // Anonymous user is created
-    else {
-      setCurrentUser(ANONYMOUS_USER);
-      //console.log("This is a new user");
-      localStorage.setItem('seismicChatUser', JSON.stringify(ANONYMOUS_USER));
-    }
-  };
 
   function SignIn() {
     const signInWithGoogle = () => {
