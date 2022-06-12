771c29b4a44b98f2524dbbf90d858f959e575288
Yasir <yhossain@gmail.com>
2022-06-12 13:22:47 -0400

    connected polls to firebase

--- seismic-web/src/components/Tools/Poll/PollCreate.js
+++ seismic-web/src/components/Tools/Poll/PollCreate.js
@@ -1,7 +1,5 @@
 import React, { setState, useState, useEffect } from 'react';
 
-import Button from '@mui/material/Button';
-
 import cuid from 'cuid';
 
 import { db, analytics } from '../../../modules/firebase';
@@ -13,9 +11,15 @@ import styles from './PollCreate.css';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function PollCreate(props) {
-  const user = props.useer;
+  const { user } = props;
 
   const pollsRef = db.collection('polls');
   const query = pollsRef.orderBy('createdAt').limitToLast(1);
@@ -30,6 +34,8 @@ function PollCreate(props) {
 
   const [showPollOption, setShowPollOption] = useState(false);
   const [viewable, setViewable] = useState(false);
+
+  const [showPollWidget, setShowPollWidget] = useState(false);
 
   useEffect(() => {
     loadPolls();
@@ -56,28 +62,25 @@ function PollCreate(props) {
     console.log(options);
   }
 
-  function pollSubmit(e) {
-    e.preventDefault();
-    let pollData = {};
-
-    console.log(e.target.value);
-
-    const options = {
+  const pollSubmit = async () => {
+    await pollsRef.add({
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       type: 'POLL',
-      id: cuid(),
-      name: 'ADMIN',
-      avatar: user.avatar,
-      title: title,
-      data: options,
-      total: total,
-      viewable: viewable,
-    };
+      id: 'poll-' + cuid(),
+      user: {
+        chatName: user.chatName,
+        uid: user.uid,
+        avatarUrl: user.avatarUrl,
+      },
+      title,
+      options,
+      total,
+      viewable: true,
+    });
 
-    console.log(options);
-
-    // firebaseTools.setCTA(options);
+    firebase.analytics().logEvent('poll_submitted_admin');
     pollReset();
-  }
+  };
 
   function pollReset() {
     setTitle('');
@@ -85,6 +88,7 @@ function PollCreate(props) {
     setTotal(0);
     setShowPollOption(false);
     setViewable(false);
+    setShowPollWidget(false);
   }
 
   function pollStop() {
@@ -100,95 +104,154 @@ function PollCreate(props) {
 
   return (
     <div className="poll">
-      {currentPoll
-        ? [
-            <div className="current-poll">
-              <h3>{currentPoll.title}</h3>
-              <ol className="listed-options" key="current-listed-options">
-                {currentPoll.options.map((option) => {
-                  return (
-                    <li key={option.label}>
-                      <label className="title">{option.label}</label>
-                    </li>
-                  );
-                })}
-              </ol>
-            </div>,
-          ]
-        : [
-            <div className="submit-poll" key="submit-poll">
-              <h3>Submit a Poll</h3>
-              <Box
-                autoComplete="off"
-                component="form"
-                onSubmit={submitPollTitle}
-                className="poll-title"
-                key="poll-title"
-              >
-                {!showPollOption
-                  ? [
-                      <TextField
-                        id="poll-title"
-                        key="poll-title-input"
-                        label="Poll Question"
-                        variant="standard"
-                        onChange={(e) => setTitle(e.target.value)}
-                      />,
-                    ]
-                  : [<h4>{title}</h4>]}
-              </Box>
+      <Card sx={{ maxWidth: 345 }} key="poll-card">
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://firebasestorage.googleapis.com/v0/b/showintel-8dcf8.appspot.com/o/trivia.jpg?alt=media&token=baae0177-197e-4b41-b2f3-9e80ab403e60"
+          alt="Create a Poll"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Play Trivia
+          </Typography>
+          {!showPollWidget
+            ? [
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  key="poll-card"
+                >
+                  Poll your audience all in real-time! Give your community a way
+                  to score high on the leaderboard all while you gain valuable
+                  insights.
+                </Typography>,
+              ]
+            : [
+                <div>
+                  {currentPoll
+                    ? [
+                        <div className="current-poll" key="poll-create">
+                          <h3>{currentPoll.title}</h3>
+                          <ol
+                            className="listed-options"
+                            key="current-listed-options"
+                          >
+                            {currentPoll.options.map((option) => {
+                              return (
+                                <li key={option.label}>
+                                  <label className="title">
+                                    {option.label}
+                                  </label>
+                                </li>
+                              );
+                            })}
+                          </ol>
+                        </div>,
+                      ]
+                    : [
+                        <div className="submit-poll" key="submit-poll">
+                          <h3>Submit a Poll</h3>
+                          <Box
+                            autoComplete="off"
+                            component="form"
+                            onSubmit={submitPollTitle}
+                            className="poll-title"
+                            key="poll-title"
+                          >
+                            {!showPollOption
+                              ? [
+                                  <TextField
+                                    id="poll-title"
+                                    key="poll-title-input"
+                                    label="Poll Question"
+                                    variant="standard"
+                                    onChange={(e) => setTitle(e.target.value)}
+                                  />,
+                                ]
+                              : [<h4>{title}</h4>]}
+                          </Box>
 
-              <ol className="listed-options" key="listed-options">
-                {options.map((option) => {
-                  return (
-                    <li key={option.label}>
-                      <label className="title">{option.label}</label>
-                    </li>
-                  );
-                })}
-              </ol>
+                          <ol className="listed-options" key="listed-options">
+                            {options.map((option) => {
+                              return (
+                                <li key={option.label}>
+                                  <label className="title">
+                                    {option.label}
+                                  </label>
+                                </li>
+                              );
+                            })}
+                          </ol>
 
-              {showPollOption
-                ? [
-                    <Box
-                      autoComplete="off"
-                      component="form"
-                      id="formPollOption"
-                      key="form-option"
-                      onSubmit={pollOptionSubmit}
-                    >
-                      <TextField
-                        id="txtPollOption"
-                        key="poll-option-input"
-                        label="Enter an Option"
-                        variant="standard"
-                        onChange={(e) => setCurrentPollOption(e.target.value)}
-                      />
-                    </Box>,
-                  ]
-                : null}
+                          {showPollOption
+                            ? [
+                                <Box
+                                  autoComplete="off"
+                                  component="form"
+                                  id="formPollOption"
+                                  key="form-option"
+                                  onSubmit={pollOptionSubmit}
+                                >
+                                  <TextField
+                                    id="txtPollOption"
+                                    key="poll-option-input"
+                                    label="Enter an Option"
+                                    variant="standard"
+                                    onChange={(e) =>
+                                      setCurrentPollOption(e.target.value)
+                                    }
+                                  />
+                                </Box>,
+                              ]
+                            : null}
 
-              <div className="button-container" key="button-container">
-                {title ? (
-                  <Button onClick={pollReset} className="default">
-                    Cancel
-                  </Button>
-                ) : null}
-                {options.length > 1 ? (
-                  <div>
-                    {viewable ? (
-                      <Button onClick={pollStop} className="secondary">
-                        Stop Poll
-                      </Button>
-                    ) : null}
-                    <Button onClick={(e) => pollSubmit()} className="primary">
-                      Post Poll
-                    </Button>
-                  </div>
-                ) : null}
-              </div>
-            </div>,
-          ]}
+                          <div
+                            className="button-container"
+                            key="button-container"
+                          >
+                            {title ? (
+                              <Button onClick={pollReset} className="default">
+                                Cancel
+                              </Button>
+                            ) : null}
+                            {options.length > 1 ? (
+                              <div>
+                                {viewable ? (
+                                  <Button
+                                    onClick={pollStop}
+                                    className="secondary"
+                                  >
+                                    Stop Poll
+                                  </Button>
+                                ) : null}
+                                <Button
+                                  onClick={(e) => pollSubmit(e)}
+                                  className="primary"
+                                >
+                                  Post Poll
+                                </Button>
+                              </div>
+                            ) : null}
+                          </div>
+                        </div>,
+                      ]}
+                </div>,
+              ]}
+        </CardContent>
+        <CardActions>
+          {showPollWidget
+            ? [
+                <Button size="small" onClick={(e) => pollReset(e)}>
+                  Cancel
+                </Button>,
+              ]
+            : null}
+          <Button size="small" onClick={() => setShowPollWidget(true)}>
+            Create
+          </Button>
+        </CardActions>
+      </Card>
     </div>
   );
 }
