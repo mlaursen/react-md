0849329e675814951f2d28d192054ecc2b635e79
Mikkel Laursen <mlaursen03@gmail.com>
2022-06-26 12:05:39 -0600

    chore: update icon rotator styles, define IconProvider, and fix list types

--- packages/list/src/types.ts
+++ packages/list/src/types.ts
@@ -21,50 +21,8 @@ export type ListItemHeight =
 export type ListItemAddonPosition = "top" | "middle" | "bottom";
 export type ListItemAddonType = "icon" | "avatar" | "media" | "large-media";
 
-export interface ListItemChildrenProps {
-  /**
-   * The main content to display. When the `textChildren` prop is enabled and
-   * there is child content, it will be treated as primary text and update the
-   * styles automatically.
-   */
-  children?: ReactNode;
-
-  /**
-   * An optional className to apply to the `<span>` that surrounds the
-   * `primaryText` and optionally `secondaryText` within the list item.
-   */
-  textClassName?: string;
-
-  /**
-   * An optional className to apply to the `<span>` that surrounds the
-   * `secondaryText` within the list item.
-   */
-  secondaryTextClassName?: string;
-
-  /**
-   *
-   * @remarks \@since 6.0.0 Renamed from `textChildren` since it was defaulted on.
-   * @defaultValue `false`
-   */
-  disableTextChildren?: boolean;
-
-  /**
-   * An optional element that should be rendered as the `primaryText` within the
-   * list item. It is most likely easier to use the `children` prop instead, but
-   * this allows you to create more complex components with the `ListItem` since
-   * you can provided `children` and have the styles for the `primaryText` still
-   * applied. By default, this will only allow one line of text and add ellipsis
-   * for any text overflow.
-   */
-  primaryText?: ReactNode;
-
-  /**
-   * An optional element that should be rendered as the `secondaryText` within
-   * the list item. By default, this will only span one line and add ellipsis
-   * for overflow.
-   */
-  secondaryText?: ReactNode;
-
+/** @remarks \@since 6.0.0 */
+export interface ListItemChildrenAddonProps {
   /**
    * An optional addon to display to the left of the `primaryText` or
    * `children` and should be used with the `leftAddonType` prop to adjust
@@ -117,6 +75,51 @@ export interface ListItemChildrenProps {
    * clone it into the provided icon element.
    */
   forceAddonWrap?: boolean;
+}
+
+export interface ListItemChildrenProps extends ListItemChildrenAddonProps {
+  /**
+   * The main content to display. When the `textChildren` prop is enabled and
+   * there is child content, it will be treated as primary text and update the
+   * styles automatically.
+   */
+  children?: ReactNode;
+
+  /**
+   * An optional className to apply to the `<span>` that surrounds the
+   * `primaryText` and optionally `secondaryText` within the list item.
+   */
+  textClassName?: string;
+
+  /**
+   * An optional className to apply to the `<span>` that surrounds the
+   * `secondaryText` within the list item.
+   */
+  secondaryTextClassName?: string;
+
+  /**
+   *
+   * @remarks \@since 6.0.0 Renamed from `textChildren` since it was defaulted on.
+   * @defaultValue `false`
+   */
+  disableTextChildren?: boolean;
+
+  /**
+   * An optional element that should be rendered as the `primaryText` within the
+   * list item. It is most likely easier to use the `children` prop instead, but
+   * this allows you to create more complex components with the `ListItem` since
+   * you can provided `children` and have the styles for the `primaryText` still
+   * applied. By default, this will only allow one line of text and add ellipsis
+   * for any text overflow.
+   */
+  primaryText?: ReactNode;
+
+  /**
+   * An optional element that should be rendered as the `secondaryText` within
+   * the list item. By default, this will only span one line and add ellipsis
+   * for overflow.
+   */
+  secondaryText?: ReactNode;
 
   /**
    * @defaultValue `false`
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,116 @@
-import "./app.scss";
-
 import { CoreProviders, ThemeProvider } from "@react-md/core";
+import type { ConfiguredIcons } from "@react-md/icon";
 import { IconProvider } from "@react-md/icon";
+import { ErrorOutlineIcon } from "@react-md/material-icons/filled/alert/ErrorOutlineIcon";
+import { FileUploadIcon } from "@react-md/material-icons/filled/file/FileUploadIcon";
+import { KeyboardArrowLeftIcon } from "@react-md/material-icons/filled/hardware/KeyboardArrowLeftIcon";
+import { KeyboardArrowRightIcon } from "@react-md/material-icons/filled/hardware/KeyboardArrowRightIcon";
+import { RemoveRedEyeIcon } from "@react-md/material-icons/filled/image/RemoveRedEyeIcon";
+import { ArrowDropDownIcon } from "@react-md/material-icons/filled/navigation/ArrowDropDownIcon";
+import { ArrowUpwardIcon } from "@react-md/material-icons/filled/navigation/ArrowUpwardIcon";
+import { CheckIcon } from "@react-md/material-icons/filled/navigation/CheckIcon";
+import { MenuIcon } from "@react-md/material-icons/filled/navigation/MenuIcon";
+import { NotificationsIcon } from "@react-md/material-icons/filled/social/NotificationsIcon";
+import { CheckBoxIcon } from "@react-md/material-icons/filled/toggle/CheckBoxIcon";
+import { RadioButtonCheckedIcon } from "@react-md/material-icons/filled/toggle/RadioButtonCheckedIcon";
+import { upperFirst } from "lodash";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import type { ReactElement } from "react";
-
 import { Header } from "src/components/Header";
+import Navigation from "src/components/Navigation";
 import { LoadThemeStyles } from "src/components/Theme/LoadThemeStyles";
 import {
   defaultColorSchemeMode,
   defaultDisableHighContrastMode,
   defaultElementInteractionMode,
 } from "src/constants/rmdConfig";
-import Navigation from "src/components/Navigation";
+import "./app.scss";
+
+const icons: ConfiguredIcons = {
+  back: <KeyboardArrowLeftIcon />,
+  checkbox: <CheckBoxIcon />,
+  dropdown: <ArrowDropDownIcon />,
+  error: <ErrorOutlineIcon />,
+  expander: <KeyboardArrowLeftIcon />,
+  forward: <KeyboardArrowRightIcon />,
+  menu: <MenuIcon />,
+  notification: <NotificationsIcon />,
+  password: <RemoveRedEyeIcon />,
+  radio: <RadioButtonCheckedIcon />,
+  selected: <CheckIcon />,
+  sort: <ArrowUpwardIcon />,
+  upload: <FileUploadIcon />,
+};
+
+const toTitle = (s: string): string => {
+  if (/autocomplete/i.test(s)) {
+    return "AutoComplete";
+  }
+
+  if (/^api$/i.test(s)) {
+    return "API";
+  }
+
+  if (/sassdoc/i.test(s)) {
+    return "SassDoc";
+  }
+
+  if (s === "[id]") {
+    // TODO: Fix to use query params instead
+    return "Layout";
+  }
+
+  return s
+    .split("-")
+    .map((part) => {
+      if (/^((v\d+)|(to))$/.test(part)) {
+        return part;
+      }
+
+      if (/^api$/i.test(part)) {
+        return "API";
+      }
+
+      if (/^cdn$/i.test(part)) {
+        return "CDN";
+      }
+
+      return upperFirst(part);
+    })
+    .join(" ");
+};
+
+const toBreadcrumbPageTitle = (
+  pathname: string,
+  statusCode?: number
+): string => {
+  let title = "";
+  if (statusCode) {
+    switch (statusCode) {
+      case 404:
+        title = "Not Found";
+        break;
+      default:
+        title = "Server error";
+    }
+  } else if (/v\d+-to-v\d+$/.test(pathname)) {
+    const [migration] = pathname.split("/").reverse();
+    title = `Migration Guides - ${migration.replace(/-/g, " ")}`;
+  } else {
+    const parts = pathname.split("/").filter((p) => !!p && !/packages/.test(p));
+    title = parts.map((p) => toTitle(p)).join(" - ");
+  }
+
+  if (title) {
+    return title;
+  }
+
+  return "react-md";
+};
 
 export default function App(props: AppProps): ReactElement {
-  const { Component, pageProps } = props;
+  const { Component, pageProps, router } = props;
 
   return (
     <CoreProviders
@@ -23,8 +118,11 @@ export default function App(props: AppProps): ReactElement {
       elementInteractionMode={defaultElementInteractionMode}
       disableHigherContrast={defaultDisableHighContrastMode}
     >
+      <Head>
+        <title>{toBreadcrumbPageTitle(router.pathname)}</title>
+      </Head>
       <ThemeProvider>
-        <IconProvider>
+        <IconProvider {...icons}>
           <Header />
           <Navigation />
           <Component {...pageProps} />
