0e5c9f3e15c550deaa6df6327e8756196c47c902
Mikkel Laursen <mlaursen03@gmail.com>
2022-10-28 20:38:50 -0600

    chore: stop precommit hook for fixing type imports

--- src/pages/textfield.tsx
+++ src/pages/textfield.tsx
@@ -18,8 +18,26 @@ function TextFieldHook(): ReactElement {
   // - should I create ValidatedTextField/ValidatedTextArea componets that implement this hook?
   //   - this would allow for some internal optimizations around persisting error states when needed
   //   - alos around the value? This hook is only useful if you need the value immmediately
-  const { fieldProps } = useTextField({
-    name: "field",
+  const { fieldProps: fieldProps1 } = useTextField({
+    name: "field1",
+    required: true,
+    counter: true,
+    maxLength: 40,
+    minLength: 4,
+    disableMessage: true,
+    disableMaxLength: true,
+  });
+  const { fieldProps: fieldProps2 } = useTextField({
+    name: "field2",
+    required: true,
+    counter: true,
+    maxLength: 40,
+    minLength: 4,
+    disableMessage: true,
+    disableMaxLength: true,
+  });
+  const { fieldProps: fieldProps3 } = useTextField({
+    name: "field3",
     required: true,
     counter: true,
     maxLength: 40,
@@ -30,7 +48,9 @@ function TextFieldHook(): ReactElement {
 
   return (
     <Form>
-      <TextField {...fieldProps} label="Label" />
+      <TextField {...fieldProps1} label="Label" />
+      <TextField {...fieldProps2} label="Label" />
+      <TextField {...fieldProps3} label="Label" />
       <Button type="submit">Submit</Button>
     </Form>
   );
