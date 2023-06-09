3a0aba4df6764afb028209b52e633e3862f60d2e
Mikkel Laursen <mlaursen03@gmail.com>
2023-06-08 20:20:18 -0400

    test(core): add some useDropzone tests

---
+++ packages/core/src/__tests__/useDropzone.tsx
@@ -0,0 +1,131 @@
+import type { ReactElement, ReactNode } from "react";
+import { cnb } from "cnbuilder";
+import { fireEvent, render } from "@testing-library/react";
+
+import type { DropzoneOptions } from "../useDropzone";
+import { useDropzone } from "../useDropzone";
+
+function Test({
+  children,
+  ...options
+}: DropzoneOptions & { children?: ReactNode }): ReactElement {
+  const { isOver, dropzoneHandlers } = useDropzone(options);
+
+  return (
+    <div
+      data-testid="dropzone"
+      {...dropzoneHandlers}
+      className={cnb(isOver && "over")}
+    >
+      {children}
+    </div>
+  );
+}
+
+describe("useDropzone", () => {
+  it("should work correctly", () => {
+    const onDragOver = jest.fn();
+    const onDragEnter = jest.fn();
+    const onDragLeave = jest.fn();
+    const onDrop = jest.fn();
+
+    const { getByTestId } = render(
+      <Test
+        onDragLeave={onDragLeave}
+        onDragEnter={onDragEnter}
+        onDragOver={onDragOver}
+        onDrop={onDrop}
+      />
+    );
+    const dropzone = getByTestId("dropzone");
+    expect(dropzone).not.toHaveClass("over");
+    expect(onDragOver).not.toHaveBeenCalled();
+    expect(onDragEnter).not.toHaveBeenCalled();
+    expect(onDragLeave).not.toHaveBeenCalled();
+    expect(onDrop).not.toHaveBeenCalled();
+
+    fireEvent.dragEnter(dropzone);
+    expect(dropzone).toHaveClass("over");
+    expect(onDragOver).not.toHaveBeenCalled();
+    expect(onDragEnter).toHaveBeenCalled();
+    expect(onDragLeave).not.toHaveBeenCalled();
+    expect(onDrop).not.toHaveBeenCalled();
+
+    fireEvent.dragLeave(dropzone);
+    expect(dropzone).not.toHaveClass("over");
+    expect(onDragOver).not.toHaveBeenCalled();
+    expect(onDragEnter).toHaveBeenCalled();
+    expect(onDragLeave).toHaveBeenCalled();
+    expect(onDrop).not.toHaveBeenCalled();
+
+    fireEvent.dragOver(dropzone);
+    expect(dropzone).toHaveClass("over");
+    expect(onDragOver).toHaveBeenCalled();
+    expect(onDragEnter).toHaveBeenCalled();
+    expect(onDragLeave).toHaveBeenCalled();
+    expect(onDrop).not.toHaveBeenCalled();
+
+    fireEvent.drop(dropzone);
+    expect(dropzone).not.toHaveClass("over");
+    expect(onDragOver).toHaveBeenCalled();
+    expect(onDragEnter).toHaveBeenCalled();
+    expect(onDragLeave).toHaveBeenCalled();
+    expect(onDrop).toHaveBeenCalled();
+  });
+
+  it("should prevent default and stop propagation", () => {
+    const onDragOver = jest.fn();
+    const onDragEnter = jest.fn();
+    const onDragLeave = jest.fn();
+    const onDrop = jest.fn();
+    const { getByTestId } = render(
+      <div
+        data-testid="container"
+        onDragLeave={onDragLeave}
+        onDragEnter={onDragEnter}
+        onDragOver={onDragOver}
+        onDrop={onDrop}
+      >
+        <Test onDrop={() => {}} />
+      </div>
+    );
+
+    const dropzone = getByTestId("dropzone");
+    fireEvent.dragEnter(dropzone);
+    fireEvent.dragLeave(dropzone);
+    fireEvent.dragOver(dropzone);
+    fireEvent.drop(dropzone);
+
+    expect(onDragOver).not.toHaveBeenCalled();
+    expect(onDragEnter).not.toHaveBeenCalled();
+    expect(onDragLeave).not.toHaveBeenCalled();
+    expect(onDrop).not.toHaveBeenCalled();
+  });
+
+  it("should not disable the isOver state if a dragleave event is called on a child element", () => {
+    const { getByTestId } = render(
+      <Test onDrop={() => {}}>
+        <div data-testid="child1" />
+        <div data-testid="child2" />
+      </Test>
+    );
+
+    const dropzone = getByTestId("dropzone");
+    const child1 = getByTestId("child1");
+    const child2 = getByTestId("child2");
+    expect(dropzone).not.toHaveClass("over");
+
+    fireEvent.dragEnter(dropzone);
+    expect(dropzone).toHaveClass("over");
+
+    fireEvent.dragOver(child1);
+    expect(dropzone).toHaveClass("over");
+
+    fireEvent.dragLeave(child1);
+    fireEvent.dragOver(child2);
+    expect(dropzone).toHaveClass("over");
+
+    fireEvent.dragLeave(dropzone);
+    expect(dropzone).not.toHaveClass("over");
+  });
+});
