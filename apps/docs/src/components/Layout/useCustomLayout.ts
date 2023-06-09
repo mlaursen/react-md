380a993ce044f0e87e6013f2c0fa53dd5c2e03aa
Mikkel Laursen <mlaursen03@gmail.com>
2023-06-08 20:35:23 -0400

    chore(docs): fix layouts after local storage changes

--- apps/docs/src/components/Layout/useCustomLayout.ts
+++ apps/docs/src/components/Layout/useCustomLayout.ts
@@ -12,6 +12,26 @@ export type CustomLayout = UseStateObject<"phoneLayout", SupportedPhoneLayout> &
   UseStateObject<"largeDesktopLayout", SupportedWideLayout> &
   UseStateObject<"landscapeTabletLayout", SupportedTabletLayout>;
 
+const PHONE_LAYOUTS: SupportedPhoneLayout[] = ["temporary", "temporary-mini"];
+const TABLET_LAYOUTS: SupportedTabletLayout[] = [
+  ...PHONE_LAYOUTS,
+  "toggleable",
+  "toggleable-mini",
+];
+const WIDE_LAYOUTS: SupportedWideLayout[] = [
+  ...TABLET_LAYOUTS,
+  "floating",
+  "clipped",
+  "full-height",
+];
+
+const isPhoneLayout = (value: unknown): value is SupportedPhoneLayout =>
+  PHONE_LAYOUTS.includes(value as SupportedPhoneLayout);
+const isTabletLayout = (value: unknown): value is SupportedTabletLayout =>
+  TABLET_LAYOUTS.includes(value as SupportedTabletLayout);
+const isWideLayout = (value: unknown): value is SupportedWideLayout =>
+  WIDE_LAYOUTS.includes(value as SupportedWideLayout);
+
 /**
  * Note: I'd like to be able to say that large layouts are toggleable by
  * default, but it appears to cause SSR issues that way on mobile
@@ -21,26 +41,31 @@ export function useCustomLayout(): CustomLayout {
     useLocalStorage<SupportedPhoneLayout>({
       key: "phoneLayout",
       defaultValue: "temporary",
+      deserializer: (item) => (isPhoneLayout(item) ? item : PHONE_LAYOUTS[0]),
     });
   const { value: tabletLayout, setValue: setTabletLayout } =
     useLocalStorage<SupportedTabletLayout>({
       key: "tabletLayout",
       defaultValue: "temporary",
+      deserializer: (item) => (isTabletLayout(item) ? item : TABLET_LAYOUTS[0]),
     });
   const { value: desktopLayout, setValue: setDesktopLayout } =
     useLocalStorage<SupportedWideLayout>({
       key: "desktopLayout",
       defaultValue: "temporary",
+      deserializer: (item) => (isWideLayout(item) ? item : WIDE_LAYOUTS[0]),
     });
   const { value: largeDesktopLayout, setValue: setLargeDesktopLayout } =
     useLocalStorage<SupportedWideLayout>({
       key: "largeDesktopLayout",
       defaultValue: "temporary",
+      deserializer: (item) => (isWideLayout(item) ? item : WIDE_LAYOUTS[0]),
     });
   const { value: landscapeTabletLayout, setValue: setLandscapeTabletLayout } =
     useLocalStorage<SupportedTabletLayout>({
       key: "landscapeTabletLayout",
       defaultValue: "temporary",
+      deserializer: (item) => (isTabletLayout(item) ? item : TABLET_LAYOUTS[0]),
     });
 
   return {
