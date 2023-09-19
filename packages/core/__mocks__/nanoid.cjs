fd6b2c36509047dbb42b0445e4122465f20c83ec
Mikkel Laursen <mlaursen03@gmail.com>
2023-09-18 20:12:59 -0400

    build(core): bump nanoid to v5

--- packages/core/__mocks__/nanoid.cjs
+++
@@ -1,7 +0,0 @@
-let index = 0;
-
-// this was added because https://github.com/microsoft/accessibility-insights-web/pull/5421#issuecomment-1109168149
-// is not working at this time
-module.exports = {
-  nanoid: () => `${++index}`,
-};
