8eaf4e9311bca54d3d17f83ea22a4f01140f1fd1
Mikkel Laursen <mlaursen03@gmail.com>
2023-09-18 20:11:42 -0400

    build(core): bump most dependencies to latest

--- packages/core/jest.setup.ts
+++ packages/core/jest.setup.ts
@@ -1,2 +1,2 @@
-import "@testing-library/jest-dom/jest-globals.js";
+import "@testing-library/jest-dom/jest-globals";
 import "./src/test-utils/polyfills/index.js";
--- packages/core/src/test-utils/index.ts
+++ packages/core/src/test-utils/index.ts
@@ -1,10 +1,3 @@
-import userEvent_ from "@testing-library/user-event";
-
-// const userEvent_ = userEvent.default;
-
-const userEvent = userEvent_ as unknown as typeof userEvent_.default;
-
-export { userEvent };
 export * from "@testing-library/react";
 export * from "@testing-library/user-event";
 
