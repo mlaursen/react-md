c001a6b47a4738e018eac7d70c4c2b0c19c3e86d
Mikkel Laursen <mlaursen03@gmail.com>
2023-10-01 14:12:51 -0400

    chore(docs): update search to use responsive button

--- apps/docs/src/components/RootLayout/Search.tsx
+++ apps/docs/src/components/RootLayout/Search.tsx
@@ -1,7 +1,5 @@
-"use client";
-import { Button, Typography, cssUtils, useAppSize } from "@react-md/core";
+import { Button, SrOnly, Typography } from "@react-md/core";
 import SearchIcon from "@react-md/material-icons/SearchIcon";
-import { cnb } from "cnbuilder";
 import { type ReactElement } from "react";
 import styles from "./Search.module.scss";
 
@@ -11,32 +9,25 @@ export interface SearchProps {
 
 export function Search(props: SearchProps): ReactElement {
   const { isMac } = props;
-  const { isPhone } = useAppSize();
 
   return (
     <>
-      <Button
-        themeType={isPhone ? "flat" : "outline"}
-        buttonType={isPhone ? "icon" : "text"}
-        className={styles.button}
-      >
+      <Button responsive className={styles.button}>
         <SearchIcon />
-        <Typography
-          as="span"
-          className={cnb(styles.search, cssUtils({ srOnly: isPhone }))}
-        >
-          Search...
-        </Typography>
-        <Typography
-          type="body-2"
-          as="span"
-          className={styles.shortcut}
-          margin="none"
-          hidden={isPhone}
-        >
-          {isMac ? "⌘" : "Ctrl"}
-          {" + K"}
-        </Typography>
+        <SrOnly phoneOnly>
+          <Typography as="span" className={styles.search}>
+            Search...
+          </Typography>
+          <Typography
+            type="body-2"
+            as="span"
+            className={styles.shortcut}
+            margin="none"
+          >
+            {isMac ? "⌘" : "Ctrl"}
+            {" + K"}
+          </Typography>
+        </SrOnly>
       </Button>
     </>
   );
