fb25e9576b227c6b3e98036cf4586ca3e802ec5a
Mikkel Laursen <mlaursen03@gmail.com>
2023-10-07 16:54:14 -0400

    chore(docs): update TableOfContents to support large pages

--- apps/docs/src/components/TableOfContents/TableOfContents.tsx
+++ apps/docs/src/components/TableOfContents/TableOfContents.tsx
@@ -27,6 +27,22 @@ function getHeadings(items: readonly TableOfContentsItem[]): HTMLElement[] {
   return headings;
 }
 
+function getLastHeadingId(items: readonly TableOfContentsItem[]): string {
+  const last = items.at(-1);
+  if (!last) {
+    return "";
+  }
+
+  if (last.items) {
+    return getLastHeadingId(last.items);
+  }
+
+  return last.id;
+}
+
+const isScrolledNearPageBottom = (): boolean =>
+  window.scrollY >= document.documentElement.scrollHeight * 0.8;
+
 const threshold = [0.0, 1.0];
 const getRootMargin = (): string => {
   const headerHeightVar = window
@@ -49,8 +65,9 @@ export function TableOfContents(
   const { toc } = props;
 
   // heavily inspired by https://github.com/mdn/yari/blob/231d6aab8f1c8efe159d268c261446c5b7ae12d9/client/src/document/hooks.ts#L171
-  const [activeId, setActiveId] = useState("");
+  const [activeId, setActiveId] = useState(toc[0]?.id ?? "");
   const elements = useRef<Map<string, boolean>>();
+  const isFirstRender = useRef(true);
   useIntersectionObserver({
     threshold,
     getRootMargin,
@@ -64,27 +81,35 @@ export function TableOfContents(
 
       return headings;
     }, [toc]),
-    onUpdate: useCallback((entries) => {
-      const lookup = elements.current;
-      if (!lookup) {
-        return;
-      }
+    onUpdate: useCallback(
+      (entries) => {
+        const lookup = elements.current;
+        if (!lookup) {
+          return;
+        }
 
-      entries.forEach((entry) => {
-        lookup.set(entry.target.id, entry.isIntersecting);
-      });
+        entries.forEach((entry) => {
+          lookup.set(entry.target.id, entry.isIntersecting);
+        });
 
-      // get the first visible/intersecting item and set it
-      const foundId = [...lookup.entries()].find(
-        ([_id, isIntersecting]) => isIntersecting
-      )?.[0];
+        // get the first visible/intersecting item and set it
+        let foundId = [...lookup.entries()].find(
+          ([_id, isIntersecting]) => isIntersecting
+        )?.[0];
+        if (!foundId && isFirstRender.current && isScrolledNearPageBottom()) {
+          foundId = getLastHeadingId(toc);
+        }
 
-      // if there isn't a found id, it might be a really large section where
-      // another heading isn't visible, so maintain the previous one
-      if (foundId) {
-        setActiveId(foundId);
-      }
-    }, []),
+        isFirstRender.current = false;
+
+        // if there isn't a found id, it might be a really large section where
+        // another heading isn't visible, so maintain the previous one
+        if (foundId) {
+          setActiveId(foundId);
+        }
+      },
+      [toc]
+    ),
   });
 
   const headingId = useId();
