cd98f408d77c390638d1cf6193df5bbaf1e1eaa9
Mikkel Laursen <mlaursen03@gmail.com>
2023-12-02 16:37:30 -0500

    chore(docs): fix missing errors for DangerouslyRunCode in StrictMode

--- apps/docs/src/components/DangerouslyRunCode/useDangerouslyRunnableCode.tsx
+++ apps/docs/src/components/DangerouslyRunCode/useDangerouslyRunnableCode.tsx
@@ -1,10 +1,12 @@
 // https://github.com/nihgwu/react-runner/tree/974ebc932db7b7c7d59f1b50a79aed705efbf75a
 // This is pretty much everything from there except using the new JSX transform
 // and I wanted to understand why things were implemented the way they were
+// Note: Also switched from useEffect to useMemo since useEffect doesn't report
+// errors correctly in strict mode. useMemo is pretty much the same thing as
+// useRef + useEffect anyways?
 "use client";
 import {
-  useEffect,
-  useRef,
+  useMemo,
   useState,
   type MutableRefObject,
   type ReactElement,
@@ -15,14 +17,6 @@ import {
 } from "./DangerouslyRunCode.jsx";
 import { type DangerouslyRunCodeOptions } from "./utils.jsx";
 
-const checkDefaultExport = (code: string): Error | null => {
-  if (!code.includes("export default")) {
-    return new Error("Missing default export for the demo");
-  }
-
-  return null;
-};
-
 export interface UpdateOptions extends DangerouslyRunCodeOptions {
   setState(nextState: DangerouslyRunCodeResult): void;
   elementRef: MutableRefObject<ReactElement | null>;
@@ -32,56 +26,20 @@ export function useDangerouslyRunnableCode(
   options: DangerouslyRunCodeOptions
 ): DangerouslyRunCodeResult {
   const { code, scope } = options;
-  const elementRef = useRef<ReactElement | null>(null);
 
-  const [state, setState] = useState<DangerouslyRunCodeResult>(() => {
-    const element = (
+  const [error, setError] = useState<Error | null>(null);
+  const element = useMemo(
+    () => (
       <DangerouslyRunCode
         code={code}
         scope={scope}
         onRendered={(error) => {
-          if (error) {
-            setState({
-              error,
-              element: elementRef.current,
-            });
-          } else {
-            elementRef.current = element;
-          }
+          setError(error);
         }}
       />
-    );
+    ),
+    [code, scope]
+  );
 
-    return { element, error: checkDefaultExport(code) };
-  });
-
-  const firstRender = useRef(true);
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      return;
-    }
-
-    const element = (
-      <DangerouslyRunCode
-        code={code}
-        scope={scope}
-        onRendered={(error) => {
-          if (error) {
-            setState({
-              error,
-              element: elementRef.current,
-            });
-          } else {
-            elementRef.current = element;
-          }
-        }}
-      />
-    );
-    setState({
-      error: checkDefaultExport(code),
-      element,
-    });
-  }, [code, scope]);
-  return state;
+  return { element, error };
 }
