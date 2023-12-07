5d8ce20ff7bb77ebf57b358b3653b05ffaa131f2
Mikkel Laursen <mlaursen03@gmail.com>
2023-12-07 17:04:37 -0500

    docs(sheet): update position docs

--- packages/core/src/sheet/styles.ts
+++ packages/core/src/sheet/styles.ts
@@ -12,16 +12,15 @@ export type SheetPosition = "top" | "right" | "bottom" | "left";
  * The size to use for sheets that have been positioned left or right. The
  * default supported values are:
  *
- * - none - the size is based on content, but is still limited to the viewport
- *   width so that the horizontal scrolling will not occur within the page. No
- *   limits added to sizing.
- * - touch - the `min-width` is set to be the entire viewport width minus a
- *   touchable area and `max-width` is set to `20rem` and is normally
- *   recommended for mobile devices.
- * - static - the width is set to a static `16rem` and generally used for
+ * - `"none"` - the size is based on content, but is still limited to the
+ *   viewport width so that the horizontal scrolling will not occur within the
+ *   page. No limits added to sizing.
+ * - `"touch"` - the `width` is set to be the entire viewport width minus a
+ *   touchable area and is normally recommended for mobile devices.
+ * - `"static"` - the `width` is set to a static `16rem` and generally used for
  *   landscape tablets and desktops.
- * - media - automatically switches between "touch" and "static" based on css
- *   media queries. (this is the default)
+ * - `"media"` - automatically switches between "touch" and "static" based on
+ *   css media queries. (this is the default)
  */
 export type SheetHorizontalSize = "none" | "media" | "touch" | "static";
 
@@ -29,12 +28,12 @@ export type SheetHorizontalSize = "none" | "media" | "touch" | "static";
  * The size to use for sheets that have been positioned top or bottom. The
  * supported sizes are:
  *
- * - none - the size is based on content and is limited to the viewport
- *   height.
- * - touch - the size is based on content and is limited to the viewport
- *   height with a touchable area to close the sheet.
- * - recommended - the material design recommended sizing that forces a
- *   max-height of 50vh and min-height of 3.5rem
+ * - `"none"` - the size is based on content and is limited to the viewport
+ *   height
+ * - `"touch"` - the size is based on content and is limited to the viewport
+ *   height minus a small touchable area
+ * - `"recommended"` - the material design recommended sizing that forces a
+ *   `max-height` of 50vh and `min-height` of 3.5rem
  */
 export type SheetVerticalSize = "none" | "touch" | "recommended";
 
