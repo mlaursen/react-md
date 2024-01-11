055d5a07cc4cfbebf77e0e23ad8fe7fc2ed0c7fc
Mikkel Laursen <mlaursen03@gmail.com>
2024-01-11 15:10:55 -0500

    test(core): added suspense tests

---
+++ packages/core/src/suspense/__tests__/CircularProgressSuspense.tsx
@@ -0,0 +1,90 @@
+import { describe, it, expect } from "@jest/globals";
+import { act } from "react-dom/test-utils";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "../../test-utils/index.js";
+import {
+  CircularProgressSuspense,
+  type CircularProgressSuspenseProps,
+} from "../CircularProgressSuspense.js";
+import { type FC, lazy, type ReactElement } from "react";
+import { Typography } from "../../typography/Typography.js";
+
+const setup = (props?: Omit<CircularProgressSuspenseProps, "children">) => {
+  function Content(): ReactElement {
+    return <Typography>Hello, world!</Typography>;
+  }
+
+  // this is how you can manually resolve a promise using events
+  const instance = new EventTarget();
+  const FakeLazyComponent = lazy(
+    () =>
+      new Promise<{ default: FC }>((resolve) => {
+        instance.addEventListener("resolve-promise", () =>
+          resolve({ default: Content })
+        );
+      })
+  );
+
+  function Test(): ReactElement {
+    return (
+      <div data-testid="container">
+        <CircularProgressSuspense {...props}>
+          <FakeLazyComponent />
+        </CircularProgressSuspense>
+      </div>
+    );
+  }
+
+  render(<Test />);
+
+  return () => {
+    act(() => {
+      // resolve the promise
+      instance.dispatchEvent(new Event("resolve-promise"));
+    });
+  };
+};
+
+describe("CircularProgressSuspense", () => {
+  it("should render a CircularProgress as the fallback value", async () => {
+    const resolve = setup();
+
+    const progress = await screen.findByRole("progressbar", {
+      name: "Loading",
+    });
+    expect(() => screen.getByText("Hello, world!")).toThrow();
+
+    resolve();
+    await waitForElementToBeRemoved(progress);
+    expect(() => screen.getByText("Hello, world!")).not.toThrow();
+  });
+
+  it("should not provide the loading aria-label if the aria-labelledby prop was provided", async () => {
+    const resolve = setup({ "aria-labelledby": "fake-id" });
+    const progress = await screen.findByRole("progressbar");
+    expect(progress).not.toHaveAttribute("aria-label");
+    expect(progress).toHaveAttribute("aria-labelledby", "fake-id");
+
+    // just resolve promises in case it causes issues
+    resolve();
+    await waitForElementToBeRemoved(progress);
+  });
+
+  it("should pass props to the CircularProgress component", async () => {
+    const resolve = setup({
+      "aria-label": "Custom",
+      dense: true,
+      className: "custom-class-name",
+    });
+    const progress = await screen.findByRole("progressbar", { name: "Custom" });
+
+    expect(progress).toMatchSnapshot();
+
+    // just resolve promises in case it causes issues
+    resolve();
+    await waitForElementToBeRemoved(progress);
+  });
+});
---
+++ packages/core/src/suspense/__tests__/NullSuspense.tsx
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "@jest/globals";
+import { lazy, type FC, type ReactElement } from "react";
+import { act, render, screen, waitFor } from "../../test-utils/index.js";
+import { Typography } from "../../typography/Typography.js";
+import { NullSuspense } from "../NullSuspense.js";
+
+describe("NullSuspense", () => {
+  it("should render nothing as the fallback value", async () => {
+    function Content(): ReactElement {
+      return <Typography>Hello, world!</Typography>;
+    }
+
+    // this is how you can manually resolve a promise using events
+    const instance = new EventTarget();
+    const FakeLazyComponent = lazy(
+      () =>
+        new Promise<{ default: FC }>((resolve) => {
+          instance.addEventListener("resolve-promise", () =>
+            resolve({ default: Content })
+          );
+        })
+    );
+
+    function Test(): ReactElement {
+      return (
+        <div data-testid="container">
+          <NullSuspense>
+            <FakeLazyComponent />
+          </NullSuspense>
+        </div>
+      );
+    }
+
+    render(<Test />);
+    const container = screen.getByTestId("container");
+    expect(container).toBeEmptyDOMElement();
+
+    act(() => {
+      // resolve the promise
+      instance.dispatchEvent(new Event("resolve-promise"));
+    });
+    await waitFor(() => {
+      expect(() => screen.getByText("Hello, world!")).not.toThrow();
+    });
+  });
+});
