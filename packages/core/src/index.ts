4598299079f937e721ef4134f0ba585aa7e2417c
Mikkel Laursen <mlaursen03@gmail.com>
2024-01-30 15:43:27 -0500

    fix(core): missing tooltip styling util export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -254,6 +254,7 @@ export * from "./theme/utils.js";
 export * from "./tooltip/Tooltip.js";
 export * from "./tooltip/TooltipHoverModeProvider.js";
 export * from "./tooltip/constants.js";
+export * from "./tooltip/tooltipStyles.js";
 export * from "./tooltip/useTooltip.js";
 export * from "./transition/CSSTransition.js";
 export * from "./transition/Collapse.js";
