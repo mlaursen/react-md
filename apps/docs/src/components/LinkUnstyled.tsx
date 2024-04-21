7f4edd2aebaf11d19907896540fef08915c5499b
Mikkel Laursen <mlaursen03@gmail.com>
2024-04-21 18:28:23 -0400

    chore(docs): simplify LinkUnstyled

--- apps/docs/src/components/LinkUnstyled.tsx
+++ apps/docs/src/components/LinkUnstyled.tsx
@@ -1,45 +1,12 @@
 import Link, { type LinkProps } from "next/link.js";
-import { forwardRef, type AnchorHTMLAttributes } from "react";
+import { forwardRef } from "react";
 
-export interface LinkUnstyledProps
-  extends Omit<
-      LinkProps,
-      | "children"
-      | "passHref"
-      | "onError"
-      | "as"
-      | "onMouseEnter"
-      | "onClick"
-      | "onTouchStart"
-      | "href"
-    >,
-    Omit<AnchorHTMLAttributes<HTMLAnchorElement>, "onError"> {
+export interface LinkUnstyledProps extends LinkProps {
   href: string;
 }
 
 export const LinkUnstyled = forwardRef<HTMLAnchorElement, LinkUnstyledProps>(
   function LinkUnstyled(props, ref) {
-    const { shallow, scroll, replace, href, children, ...remaining } = props;
-
-    if (href.startsWith("http")) {
-      return (
-        <a {...remaining} ref={ref} href={href} rel="noopener noreferrer">
-          {children}
-        </a>
-      );
-    }
-
-    return (
-      <Link
-        {...remaining}
-        ref={ref}
-        shallow={shallow}
-        scroll={scroll}
-        replace={replace}
-        href={href}
-      >
-        {children}
-      </Link>
-    );
+    return <Link {...props} ref={ref} />;
   }
 );
