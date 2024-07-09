e52a7c736ddb158a801da69af84865973c6bf1a1
Mikkel Laursen <mlaursen03@gmail.com>
2024-07-08 22:30:07 -0400

    chore(codemod): add some missing committed files

--- packages/codemod/transforms/types.ts
+++ packages/codemod/transforms/types.ts
@@ -1,10 +1,16 @@
 import {
+  type ArrowFunctionExpression,
+  type BlockStatement,
+  type CallExpression,
+  type FunctionDeclaration,
+  type FunctionExpression,
   type ImportDefaultSpecifier,
   type ImportNamespaceSpecifier,
   type ImportSpecifier,
   type JSCodeshift,
   type JSXAttribute,
   type JSXSpreadAttribute,
+  type ObjectExpression,
 } from "jscodeshift";
 
 export type AnyImportSpecifier =
@@ -27,3 +33,12 @@ export type AnyJSXAttribute = JSXAttribute | JSXSpreadAttribute;
 export type JSXAttributes = AnyJSXAttribute[];
 export type JSXReactNode = Parameters<JSCodeshift["jsxElement"]>[2];
 export type JSXReactNodeItem = NonNullable<JSXReactNode>[number];
+
+export type FunctionArgument = CallExpression["arguments"][number];
+export type ObjectPropertyKind = ObjectExpression["properties"][number];
+
+export type ComponentDefinition = (
+  | ArrowFunctionExpression
+  | FunctionDeclaration
+  | FunctionExpression
+) & { body: BlockStatement };
