fa48620497a21e63af700db91d7ac79c0113a514
Mikkel Laursen <mlaursen03@gmail.com>
2024-07-17 15:49:00 -0400

    chore(docs): scroll active toc item into view

--- apps/docs/src/components/TableOfContents/RenderTableOfContentsItem.tsx
+++ apps/docs/src/components/TableOfContents/RenderTableOfContentsItem.tsx
@@ -1,6 +1,6 @@
 import { type RenderRecursiveItemsProps } from "@react-md/core/utils/RenderRecursively";
 import { type TOCItem } from "docs-generator/rehype-toc";
-import { type ReactElement } from "react";
+import { useEffect, useRef, type ReactElement } from "react";
 import styles from "./RenderTableOfContentsItem.module.scss";
 import { TableOfContentsGroup } from "./TableOfContentsGroup.jsx";
 
@@ -9,10 +9,23 @@ export function RenderTableOfContentsItem(
 ): ReactElement {
   const { item, data: activeHeadingId, children } = props;
   const { id, value, depth } = item;
+  const active = id === activeHeadingId;
+  const ref = useRef<HTMLLIElement>(null);
+  useEffect(() => {
+    if (!active || !ref.current) {
+      return;
+    }
+
+    ref.current.scrollIntoView({ block: "nearest" });
+  }, [active]);
   return (
-    <li style={{ "--rmd-tree-depth": depth - 1 }} className={styles.item}>
+    <li
+      ref={ref}
+      style={{ "--rmd-tree-depth": depth - 1 }}
+      className={styles.item}
+    >
       <a
-        aria-current={id === activeHeadingId || undefined}
+        aria-current={active || undefined}
         href={`#${id}`}
         className={styles.link}
       >
