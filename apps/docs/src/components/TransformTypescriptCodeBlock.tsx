d8a1b00f80434475bdb094653bfb386c134c9759
Mikkel Laursen <mlaursen03@gmail.com>
2025-01-12 16:24:59 -0500

    chore(docs): allow transforming typescript without mdx

---
+++ apps/docs/src/components/TransformTypescriptCodeBlock.tsx
@@ -0,0 +1,38 @@
+import { type HighlightedCodeBlockWithAppBarAdditionalProps } from "@react-md/code/HighlightedCodeBlockWithAppBar";
+import { transformTsToJs } from "docs-generator/utils/transformTsToJs";
+import { type ReactElement } from "react";
+import { MarkdownCode } from "./MarkdownCode.jsx";
+import { TypescriptCodeBlock } from "./TypescriptCodeBlock.jsx";
+
+export interface TransformTypescriptCodeBlockProps
+  extends HighlightedCodeBlockWithAppBarAdditionalProps {
+  isTsx: boolean;
+  code: string;
+}
+
+export async function TransformTypescriptCodeBlock(
+  props: TransformTypescriptCodeBlockProps
+): Promise<ReactElement> {
+  const { isTsx, code, ...remaining } = props;
+  const jsCode = await transformTsToJs(code, "");
+  if (jsCode === code) {
+    return (
+      <MarkdownCode
+        {...remaining}
+        language={`ts${isTsx ? "x" : ""}`}
+        disableMarginTop
+      >
+        {code}
+      </MarkdownCode>
+    );
+  }
+
+  return (
+    <TypescriptCodeBlock
+      {...remaining}
+      isTsx={isTsx}
+      jsCode={jsCode}
+      tsCode={code}
+    />
+  );
+}
--- packages/docs-generator/src/utils/transformNpmCode.ts
+++ packages/docs-generator/src/utils/transformNpmCode.ts
@@ -1,8 +1,25 @@
+import { type PackageManager } from "@react-md/code/PackageManagerProvider";
 import { createJsxNode } from "./createJsxNode.js";
 import {
   replacePreElement,
   type ReplacePreElementWithJsxNodeOptions,
 } from "./replacePreElement.js";
+
+export function getTransformedNpmCode(
+  code: string
+): Record<PackageManager, string> {
+  const yarnCode = code
+    .replace(/npm/g, "yarn")
+    .replace(/uninstall/g, "remove")
+    .replace(/install/g, "add");
+  const pnpmCode = yarnCode.replace(/yarn/g, "pnpm");
+
+  return {
+    npm: code,
+    pnpm: pnpmCode,
+    yarn: yarnCode,
+  };
+}
 
 export interface TransformNpmCodeOptions
   extends ReplacePreElementWithJsxNodeOptions {
@@ -13,21 +30,11 @@ export interface TransformNpmCodeOptions
 export function transformNpmCode(options: TransformNpmCodeOptions): void {
   const { as, code, meta, preElement, preElementParent } = options;
 
-  const yarnCode = code
-    .replace(/npm/g, "yarn")
-    .replace(/uninstall/g, "remove")
-    .replace(/install/g, "add");
-  const pnpmCode = yarnCode.replace(/yarn/g, "pnpm");
-
   const codeBlock = createJsxNode({
     as,
     meta,
     props: {
-      managers: {
-        npm: code,
-        pnpm: pnpmCode,
-        yarn: yarnCode,
-      },
+      managers: getTransformedNpmCode(code),
     },
   });
   replacePreElement({
