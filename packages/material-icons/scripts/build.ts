4bfb6186603f38d34a7672de3dcc3cf4ac14b220
Mikkel Laursen <mlaursen03@gmail.com>
2025-02-20 17:25:36 -0500

    feat(material-icons): reduce installation size

--- packages/material-icons/scripts/build.ts
+++ packages/material-icons/scripts/build.ts
@@ -3,12 +3,6 @@ import { glob } from "glob";
 import { rm, writeFile } from "node:fs/promises";
 import { join } from "node:path";
 
-const TYPE_DEFINITION = `/// <reference types="react" />
-import { SVGIconProps } from "@react-md/core/icon/SVGIcon";
-declare const _default: import("react").ForwardRefExoticComponent<SVGIconProps & import("react").RefAttributes<SVGSVGElement>>;
-export default _default;
-`;
-
 const SRC_DIR = "src";
 
 // clean -- about the same as `rm -f *Icon.d.ts *Icon.js`
@@ -42,9 +36,6 @@ const promises = components.map(async (name) => {
   });
 
   await writeFile(name.replace(".tsx", ".js"), code);
-  // I should be able to create a single .d.ts file in the future once esm is
-  // the standard
-  await writeFile(name.replace(".tsx", ".d.ts"), TYPE_DEFINITION);
 });
 
 await Promise.all(promises);
---
+++ packages/material-icons/types.d.ts
@@ -0,0 +1,3 @@
+import { SVGIconProps } from "@react-md/core/icon/SVGIcon";
+declare const _default: import("react").ForwardRefExoticComponent<SVGIconProps & import("react").RefAttributes<SVGSVGElement>>;
+export default _default;
