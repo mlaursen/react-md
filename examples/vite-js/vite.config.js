9da18a9262428ab9a670d8972ecece402fa2f181
Mikkel Laursen <mlaursen03@gmail.com>
2025-03-29 22:24:00 -0400

    chore(examples): allow everything to be imported without relative paths
    This is one of the few changes I will include in these base templates.

--- examples/vite-js/vite.config.js
+++ examples/vite-js/vite.config.js
@@ -1,7 +1,13 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      everything: resolve(import.meta.dirname, 'src/_everything.scss'),
+    },
+  },
 })
