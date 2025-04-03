1b6cd056609fd7f25f1348e1926c469cff718ee5
Mikkel Laursen <mlaursen03@gmail.com>
2025-04-03 19:49:08 -0400

    chore: hopefully allow algolia web crawler

--- apps/docs/src/app/robots.ts
+++ apps/docs/src/app/robots.ts
@@ -4,12 +4,20 @@ import { BASE_URL } from "@/constants/env.js";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      // I don't want them to crawl this preview website
-      disallow: "/",
-      // allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        // I don't want them to crawl this preview website
+        disallow: "/",
+        // allow: "/",
+      },
+      {
+        userAgent: "Algolia Crawler",
+        allow: "/",
+        // there's no reason to crawl the typedoc since it's a separate search
+        disallow: "/typedoc",
+      },
+    ],
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
 }
