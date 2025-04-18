8ae0e7802c57fc8c4142c8c638ce7a8a5f564923
Mikkel Laursen <mlaursen03@gmail.com>
2025-04-18 09:04:54 -0400

    chore(examples): update examples to be runnable in web containers
    sass-embedded doesn't appear to work.

--- examples/nextjs-js/next.config.mjs
+++ examples/nextjs-js/next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    implementation: "sass-embedded",
-  },
+
+  // uncomment if you want to use sass-embedded. It will also require running:
+  // ```sh
+  // npm uninstall sass
+  // npm install sass-embedded
+  // ```
+  //
+  // sassOptions: {
+  //   implementation: "sass-embedded",
+  // },
 };
 
 export default nextConfig;
--- examples/nextjs-ts/next.config.ts
+++ examples/nextjs-ts/next.config.ts
@@ -2,9 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    implementation: "sass-embedded",
-  },
+
+  // uncomment if you want to use sass-embedded. It will also require running:
+  // ```sh
+  // npm uninstall sass
+  // npm install sass-embedded
+  // ```
+  //
+  // sassOptions: {
+  //   implementation: "sass-embedded",
+  // },
 };
 
 export default nextConfig;
