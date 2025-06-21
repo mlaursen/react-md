5822d37784ebfab52bc9e204a38d32c5888644b6
Mikkel Laursen <mlaursen03@gmail.com>
2025-06-20 21:34:14 -0400

    feat(movement): split movement for jumping

--- packages/core/src/movement/constants.ts
+++ packages/core/src/movement/constants.ts
@@ -14,23 +14,45 @@ export const DEFAULT_KEYBOARD_MOVEMENT: Readonly<KeyboardMovementConfig> = {
 };
 
 /**
+ * @since 6.3.0
+ * @internal
+ */
+export const DEFAULT_LTR_KEYBOARD_MOVEMENT_WITHOUT_JUMP: Readonly<KeyboardMovementConfig> =
+  {
+    incrementKeys: ["ArrowRight"],
+    decrementKeys: ["ArrowLeft"],
+    jumpToFirstKeys: [],
+    jumpToLastKeys: [],
+  };
+
+/**
  * @since 5.1.2
  * @internal
  */
 export const DEFAULT_LTR_KEYBOARD_MOVEMENT: Readonly<KeyboardMovementConfig> = {
-  incrementKeys: ["ArrowRight"],
-  decrementKeys: ["ArrowLeft"],
+  ...DEFAULT_LTR_KEYBOARD_MOVEMENT_WITHOUT_JUMP,
   jumpToFirstKeys: ["Home"],
   jumpToLastKeys: ["End"],
 };
+
+/**
+ * @since 6.3.0
+ * @internal
+ */
+export const DEFAULT_RTL_KEYBOARD_MOVEMENT_WITHOUT_JUMP: Readonly<KeyboardMovementConfig> =
+  {
+    incrementKeys: ["ArrowLeft"],
+    decrementKeys: ["ArrowRight"],
+    jumpToFirstKeys: [],
+    jumpToLastKeys: [],
+  };
 
 /**
  * @since 5.1.2
  * @internal
  */
 export const DEFAULT_RTL_KEYBOARD_MOVEMENT: Readonly<KeyboardMovementConfig> = {
-  incrementKeys: ["ArrowLeft"],
-  decrementKeys: ["ArrowRight"],
+  ...DEFAULT_RTL_KEYBOARD_MOVEMENT_WITHOUT_JUMP,
   jumpToFirstKeys: ["Home"],
   jumpToLastKeys: ["End"],
 };
