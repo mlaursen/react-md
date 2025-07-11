1c08e01bc2caa1f8904f6c12afb07690479412f0
Mikkel Laursen <mlaursen03@gmail.com>
2025-07-11 17:14:11 -0400

    chore(examples): update examples to latest

--- examples/mlaursen-vite/vite.config.ts
+++ examples/mlaursen-vite/vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import tsconigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tsconigPaths(), react()],
+  resolve: {
+    alias: {
+      everything: resolve(import.meta.dirname, "src/_everything.scss"),
+    },
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./src/testSetup.ts",
--- examples/react-router-framework-mode/vite.config.ts
+++ examples/react-router-framework-mode/vite.config.ts
@@ -1,7 +1,13 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      everything: resolve(import.meta.dirname, "_everything.scss"),
+    },
+  },
 });
