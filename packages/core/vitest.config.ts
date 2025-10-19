43d55ae01e2bfb1069628d8b09c912cbbd39d3a3
Mikkel Laursen <mlaursen03@gmail.com>
2025-10-19 18:21:14 -0400

    test: fix vitest test coverage

--- packages/core/vitest.config.ts
+++ packages/core/vitest.config.ts
@@ -1,5 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +8,27 @@ export default defineConfig({
     setupFiles: "./vitest.setup.ts",
     // I do not like having tests in the same folders as the rest of the files
     include: ["**/__tests__/**.{ts,tsx,js,jsx}"],
+    coverage: {
+      include: ["src/**"],
+      exclude: [
+        "**/index.ts",
+        "**/types.ts",
+        "**/test-utils/data-testid.ts",
+
+        // generated file that doesn't really matter
+        "**/icon/material.ts",
+
+        // it's hard to verify these since they probably only occur when trying to
+        // use react-dom/server in jsdom?
+        "**/test-utils/polyfills/TextEncoder.ts",
+        "**/test-utils/polyfills/TextDecoder.ts",
+        // can't really test jest with vitest
+        "**/test-utils/jest-globals/**",
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
+
+    // increase test timeout for tooltips since they are funky
+    testTimeout: 10000,
   },
 });
