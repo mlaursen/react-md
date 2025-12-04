2054ba2a082ece8efdf4b4f785daf08ece39829c
Mikkel Laursen <mlaursen03@gmail.com>
2025-12-03 19:16:41 -0500

    build(deps): bump safe deps to latest

--- packages/core/src/test-utils/polyfills/TextEncoder.ts
+++ packages/core/src/test-utils/polyfills/TextEncoder.ts
@@ -1,6 +1,5 @@
 import { TextEncoder } from "node:util";
 
 if (typeof global.TextEncoder === "undefined") {
-  // @ts-expect-error `node:util` Type 'Uint8Array<ArrayBufferLike>' is not assignable to type 'Uint8Array<ArrayBuffer>'
   global.TextEncoder = TextEncoder;
 }
