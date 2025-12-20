d5e35010dd16644e78770ed5826200ccb8cabda7
Mikkel Laursen <mlaursen03@gmail.com>
2025-12-20 10:15:39 -0500

    build(deps): bump deps to latest

--- scripts/release.ts
+++ scripts/release.ts
@@ -4,4 +4,11 @@ await release({
   repo: "react-md",
   cleanCommand: "clean-release-dist",
   buildCommand: "build-release-packages",
+  packagePaths: {
+    "@react-md/code": "packages/code",
+    "@react-md/codemod": "packages/codemod",
+    "@react-md/core": "packages/core",
+    "@react-md/material-icons": "packages/material-icons",
+    "react-md": "packages/react-md",
+  },
 });
