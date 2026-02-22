e29313a720bd0062f18e59288740e6d1c730a19f
Mikkel Laursen <mlaursen03@gmail.com>
2026-02-22 15:48:27 -0500

    setup initial material-you package

---
+++ packages/you/eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { configs, gitignore } from "@react-md/eslint-config";
+import { defineConfig } from "eslint/config";
+import { join } from "node:path";
+
+export default defineConfig([
+  gitignore(join(import.meta.url, "..", "..")),
+  ...configs.recommendedFrontend({
+    testFramework: "vitest",
+    tsconfigRootDir: import.meta.dirname,
+    strictTypeChecked: process.env.STRICT_TYPING === "true",
+    reactCompiler: true,
+  }),
+  {
+    rules: {
+      "unicorn/prefer-query-selector": "off",
+
+      "unicorn/no-negated-condition": "off",
+
+      "@typescript-eslint/no-empty-object-type": [
+        "error",
+        {
+          allowInterfaces: "never",
+          allowObjectTypes: "never",
+          allowWithName: ".+Overrides$",
+        },
+      ],
+      "react-hooks/exhaustive-deps": [
+        "error",
+        {
+          additionalHooks: "(useIsomorphicLayoutEffect|useDevEffect)",
+        },
+      ],
+    },
+  },
+]);
