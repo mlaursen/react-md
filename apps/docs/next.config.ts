0e352be0dc0d758cef8d9f4c03807ed87d7aa59c
Mikkel Laursen <mlaursen03@gmail.com>
2026-04-22 21:05:11 -0400

    build(docs): try enabling react compiler

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -3,6 +3,7 @@ import withMDX from "@next/mdx";
 import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactCompiler: true,
   reactStrictMode: true,
   poweredByHeader: false,
   pageExtensions: ["ts", "tsx", "mdx"],
